a0a98a851aeead8bf9c0eea163a91376790075bf
宋星伟 <songxingwei@songxingweideMacBook-Pro.local>
2018-09-29 17:18:45 +0800

    1

--- api/crontab/crontab_move_data.php
+++ api/crontab/crontab_move_data.php
@@ -1,15 +1,11 @@
 <?php
 require_once "../inc/common.php";
-$db = new DB_COM();
-$s = "update com_option_config set option_value=1 WHERE option_name='user_lock'";
-echo $s;
-$db -> query($s);
 
 if (time()>1538323200){
 
-//    $db = new DB_COM();
-//    $s = "update com_option_config set option_value=1 WHERE option_name=user_lock";
-//    $db -> query($s);
+    $db = new DB_COM();
+    $s = $s = "update com_option_config set option_value=1 WHERE option_name='user_lock'";
+    $db -> query($s);
 
     echo "不运行";
     die();
