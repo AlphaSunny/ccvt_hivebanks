4e945a280b63e9edf7c8cfc80eb4e1d99b4b48d9
宋星伟 <songxingwei@songxingweideMacBook-Pro.local>
2018-10-08 16:44:43 +0800

    1

--- api/bot/1.php
+++ api/bot/1.php
@@ -1,24 +1,22 @@
 <?php
-//http://www.lampol-blog.com/detail/aid/ZDk5MmFNZ2pJL1pROW5QZU9KZ2FWdVlFTDVHRnRmZm4rNDMzSFlHNg%3D%3D 各种秘钥生成的教程
-//生成密钥
-#$opensslConfigPath = "D:\phpstudy\PHPTutorial\Apache\conf\openssl.cnf";
 $config = array(
-    "digest_alg" => "sha256",
-    "private_key_bits" =>512,
+    //"digest_alg" => "sha512",
+    "private_key_bits" => 512,
     "private_key_type" => OPENSSL_KEYTYPE_RSA,
-    #'config'=> $opensslConfigPath
 );
 
-
-//创建密钥对
+//1.创建公钥和私钥  返回资源
 $res = openssl_pkey_new($config);
-//生成私钥
-openssl_pkey_export($res, $privkey, null, $config);
-//生成公钥
-$pubKey = openssl_pkey_get_details($res)['key'];
-print_r($privkey);
-echo '<div style="height:100px;background:#ffe583">1</div>';
-print_r(base64_encode($pubKey));
-file_put_contents('private.key',$privkey);
-file_put_contents('public.key',$pubKey);
+
+//从得到的资源中获取私钥  并把私钥赋给$privKey
+openssl_pkey_export($res, $privKey);
+
+//从得到的资源中获取私钥  并把私钥赋给$pubKey
+$pubKey = openssl_pkey_get_details($res);
+
+$pubKey = $pubKey["key"];
+var_dump(array('privKey'=>$privKey,'pubKey'=>$pubKey));
+die;
+
+
 ?>
