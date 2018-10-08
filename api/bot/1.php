7c5e2767ff4a4a0ba9e1636d2d4261be152491c7
宋星伟 <songxingwei@songxingweideMacBook-Pro.local>
2018-10-08 16:27:42 +0800

    1

--- api/bot/1.php
+++ api/bot/1.php
@@ -1,12 +1,12 @@
 <?php
 //http://www.lampol-blog.com/detail/aid/ZDk5MmFNZ2pJL1pROW5QZU9KZ2FWdVlFTDVHRnRmZm4rNDMzSFlHNg%3D%3D 各种秘钥生成的教程
 //生成密钥
-//$opensslConfigPath = "D:\phpstudy\PHPTutorial\Apache\conf\openssl.cnf";
+#$opensslConfigPath = "D:\phpstudy\PHPTutorial\Apache\conf\openssl.cnf";
 $config = array(
     "digest_alg" => "sha512",
     "private_key_bits" =>2048,
     "private_key_type" => OPENSSL_KEYTYPE_RSA,
-    //'config'=> $opensslConfigPath
+    #'config'=> $opensslConfigPath
 );
 
 
@@ -17,7 +17,7 @@ openssl_pkey_export($res, $privkey, null, $config);
 //生成公钥
 $pubKey = openssl_pkey_get_details($res)['key'];
 print_r($privkey);
-
+echo '<div style="height:100px;background:#ffe583">1</div>';
 print_r($pubKey);
 file_put_contents('private.key',$privkey);
 file_put_contents('public.key',$pubKey);
