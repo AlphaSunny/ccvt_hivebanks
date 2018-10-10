9e285a259ea37b0e1193bd0a3871f4577b2d1002
宋星伟 <songxingwei@songxingweideMacBook-Pro.local>
2018-10-10 13:51:18 +0800

    1

--- api/bot/collect_message.php
+++ api/bot/collect_message.php
@@ -27,7 +27,7 @@ chk_empty_args('GET', $args);
 //信息唯一值
 $data['bot_message_id'] = get_guid();
 $data['bot_nickname'] = get_arg_str('GET','nickname');
-$data['bot_content'] = get_arg_str('GET', 'content');
+$data['bot_content'] = $_REQUEST['content'];
 echo strlen($data['bot_content']);
 echo $data['bot_content'];die;
 $data['bot_send_time'] = get_arg_str('GET', 'send_time');
