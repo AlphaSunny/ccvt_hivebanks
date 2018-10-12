7fbc27969c85b0f96cf155502278efa4f063aade
宋星伟 <songxingwei@songxingweideMacBook-Pro.local>
2018-10-12 14:15:05 +0800

    1

--- api/ba/log_balance.php
+++ api/ba/log_balance.php
@@ -54,6 +54,5 @@ $rtn_ary = array();
 $rtn_ary['errcode'] = '0';
 $rtn_ary['errmsg'] = '';
 $rtn_ary['rows'] = $newrows ? $newrows : [];
-$rtn_ary['total'] = $total;
 $rtn_str = json_encode($rtn_ary);
 php_end($rtn_str);
