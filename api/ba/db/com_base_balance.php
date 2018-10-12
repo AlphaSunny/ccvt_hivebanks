5637facafd15198393fd3f73ba1c60c88dcc4925
宋星伟 <songxingwei@songxingweideMacBook-Pro.local>
2018-10-12 14:07:14 +0800

    1

--- api/ba/db/com_base_balance.php
+++ api/ba/db/com_base_balance.php
@@ -1,4 +1,17 @@
 <?php
+//======================================
+// 函数: 获取账户变动录记录总数
+// 参数: us_id        用户id
+// 返回: count        记录总数
+//======================================
+function  get_log_balance_total($us_id)
+{
+    $db = new DB_COM();
+    $sql = "SELECT hash_id FROM com_base_balance WHERE credit_id = '{$us_id}'";//fzg:hash_id=>credit_id
+    $db -> query($sql);
+    $count = $db -> affectedRows();
+    return $count;
+}
 
 //======================================
 // 函数: 获取com_base_balance基本信息
