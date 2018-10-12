7861f31f20aa08b0c3a021fa49af88df78abadd3
宋星伟 <songxingwei@songxingweideMacBook-Pro.local>
2018-10-12 10:28:37 +0800

    1

--- h5/assets/language/language.js
+++ h5/assets/language/language.js
@@ -1,7 +1,7 @@
 /**
  * Cookie operation
  */
-var getCookie = function (name, value, options) {
+var getCookie = function(name, value, options) {
     if (typeof value != 'undefined') { // name and value given, set cookie
         options = options || {};
         if (value === null) {
@@ -61,28 +61,27 @@ var getCookie = function (name, value, options) {
 var i18nLanguage = "";
 
 /*
-Set the language types supported by the website
+ Set the language types supported by the website
  */
-var webLanguage = ['en', 'zh-CN', 'ja'];
+var webLanguage = ['en','zh-CN', 'ja'];
 
 /**
  * Execute page i18n method
  * @return
  */
-var execI18n = function () {
+var execI18n = function(){
     /*
-    Get the resource file name
+     Get the resource file name
      */
     var optionEle = $("#i18n_pagename");
     if (optionEle.length < 1) {
         // console.log("未找到页面名称元素，请在页面写入\n <meta id=\"i18n_pagename\" content=\"页面名(对应语言包的语言文件名)\">");
         return false;
-    }
-
+    };
     var sourceName = optionEle.attr('content');
     sourceName = sourceName.split('-');
     /*
-    First get the language type selected before the user's browser device
+     First get the language type selected before the user's browser device
      */
     if (getCookie("userLanguage")) {
         i18nLanguage = getCookie("userLanguage");
@@ -106,22 +105,22 @@ var execI18n = function () {
     if ($.i18n == undefined) {
         // console.log("请引入i18n js 文件")
         return false;
-    }
+    };
 
     /*
-    Here you need to translate i18n
+     Here you need to translate i18n
      */
     jQuery.i18n.properties({
-        name: sourceName, //Resource file name
-        path: 'assets/language/i18n/' + i18nLanguage + '/', //Resource file path
-        mode: 'map', //Use the value in the resource file as a Map
-        language: i18nLanguage,
-        checkAvailableLanguages: true,
-        async: true,
-        callback: function () {//Set the display content after loading successfully
+        name : sourceName, //Resource file name
+        path : 'language/i18n/' + i18nLanguage +'/', //Resource file path
+        mode : 'map', //Use the value in the resource file as a Map
+        language : i18nLanguage,
+        checkAvailableLanguages:true,
+        async:true,
+        callback : function() {//Set the display content after loading successfully
             var insertEle = $(".i18n");
             // console.log(".i18n 写入中...");
-            insertEle.each(function () {
+            insertEle.each(function() {
                 // Get content write based on the name of the i18n element
                 $(this).html($.i18n.prop($(this).attr('name')));
             });
@@ -129,12 +128,11 @@ var execI18n = function () {
 
             // console.log(".i18n-input 写入中...");
             var insertInputEle = $(".i18n-input");
-            insertInputEle.each(function () {
+            insertInputEle.each(function() {
                 var selectAttr = $(this).attr('selectattr');
                 if (!selectAttr) {
                     selectAttr = "value";
-                }
-
+                };
                 $(this).attr(selectAttr, $.i18n.prop($(this).attr('selectname')));
             });
             // console.log("写入完毕");
@@ -143,27 +141,21 @@ var execI18n = function () {
 };
 
 /*Page execution load execution*/
-$(function () {
+$(function(){
 
     /*Perform I18n translation*/
     execI18n();
 
     /*Set the language selection to the value in the cache by default*/
-    // $("#language option[value=" + i18nLanguage + "]").prop("selected", true);
-    var languageList = $(".toggleLanguage");
-    $.each(languageList, function (i, val) {
-       if(i18nLanguage == $(this).attr("title")){
-           $("#language").text($(this).text());
-       }
-    });
+    $("#language option[value="+i18nLanguage+"]").prop("selected",true);
 
     /* Choose a language */
-    $(".toggleLanguage").on('click', function () {
-        var language = $(this).attr("title");
-        $("#language").text($(this).text());
-        getCookie("userLanguage", language, {
+    $("#language").on('change', function() {
+        var language = $(this).children('option:selected').val();
+        // console.log("language"+language);
+        getCookie("userLanguage",language,{
             expires: 30,
-            path: '/'
+            path:'/'
         });
         location.reload();
     });
