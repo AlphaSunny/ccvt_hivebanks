b2a8d961ff03633a1ec3137ef5a7618f1d1770cf
范助国 <ahino@fanzhuguodeMacBook-Pro.local>
2018-10-19 10:59:55 +0800

    dbFix

--- api/inc/db_connect.php
+++ api/inc/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 class DB_COM extends Mysql {
 public $schema = 'ccvt';
-protected $server = '52.53.151.223  ';
+protected $server = '52.53.151.223';
 protected $user = 'root';
 protected $password = '123456';
 protected $database = 'ccvt';
