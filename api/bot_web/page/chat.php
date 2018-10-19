3f5975eacee756ead4c90d10e1192f9d520e8c39
宋星伟 <songxingwei@songxingweideMacBook-Pro.local>
2018-10-19 16:24:14 +0800

    1

--- api/bot_web/page/chat.php
+++ api/bot_web/page/chat.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
-    <title>聊天记录</title>
+    <title>2聊天记录</title>
 </head>
 <style>
     body {
