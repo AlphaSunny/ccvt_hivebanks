1d1fb7dbc93beef5839797379e02cfbb876ccd10
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-23 17:15:30 +0800

    robot

--- h5/bot_web/js/groupMasterList.js
+++ h5/bot_web/js/groupMasterList.js
@@ -74,7 +74,8 @@ $(function () {
             shade: false,
             maxmin: true, //开启最大化最小化按钮
             area: ['800px', '450px'],
-            content: '../html/edit_group.html?groupName=' + groupName + 'is_del' + is_del + 'is_flirt' + is_flirt,
+            // content: '../html/edit_group.html?groupName=' + groupName + 'is_del' + is_del + 'is_flirt' + is_flirt,
+            content: '../html/edit_group.html',
             success:function (layero,index) {
                 var body = layer.getChildFrame('body',index);
                 // var iframeWin = window[layero.find('iframe')[0]['name']];
