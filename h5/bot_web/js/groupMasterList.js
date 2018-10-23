80a293ec3d9524638b6d0a5599010c71998d5f00
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-23 16:21:27 +0800

    robot

--- h5/bot_web/js/groupMasterList.js
+++ h5/bot_web/js/groupMasterList.js
@@ -21,8 +21,10 @@ layui.use(['table', 'form', 'layer', 'vip_table'], function () {
             , {field: 'name', title: '名称', width: 280}
             , {field: 'ba_id', title: 'ba_id', width: 380}
             , {field: 'del', title: '状态', width: 100}
+            , {field: 'is_del', title: '是否开启状态', width: 100}
             , {field: 'flirt', title: '调戏功能', width: 100}
-            , {fixed: 'right', title: '操作', width: 240, align: 'center', toolbar: '#barOption'} //这里的toolbar值是模板元素的选择器
+            , {field: 'is_flirt', title: '是否开启调戏功能', width: 100}
+            , {fixed: 'opt', title: '操作', width: 240, align: 'center', toolbar: '#barOption'} //这里的toolbar值是模板元素的选择器
         ]],
         page: true,
         id: 'dataCheck',
@@ -62,7 +64,6 @@ layui.use(['table', 'form', 'layer', 'vip_table'], function () {
 $(function () {
     //edit
     $(document).on("click", ".editBtn", function () {
-        console.log("edit");
         layer.open({
             type: 2,
             title: false,
@@ -72,7 +73,7 @@ $(function () {
             offset: 'rb', //右下角弹出
             time: 500, //2秒后自动关闭
             anim: 2,
-            content: ['../html/login.html', 'no'], //iframe的url，no代表不显示滚动条
+            content: ['../html/edit_group.html', 'no'], //iframe的url，no代表不显示滚动条
             end: function(){ //此处用于演示
                 layer.open({
                     type: 2,
