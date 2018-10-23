4366229c46a8b02d9c078d4f9bda79fe3dc03700
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-23 15:24:04 +0800

    robot

--- h5/bot_web/js/groupMasterList.js
+++ h5/bot_web/js/groupMasterList.js
@@ -65,7 +65,25 @@ $(function () {
         console.log("edit");
         layer.open({
             type: 2,
-            content: '../html/login.html' //这里content是一个URL，如果你不想让iframe出现滚动条，你还可以content: ['http://sentsin.com', 'no']
+            title: false,
+            closeBtn: 0, //不显示关闭按钮
+            shade: [0],
+            area: ['340px', '215px'],
+            offset: 'rb', //右下角弹出
+            time: 2000, //2秒后自动关闭
+            anim: 2,
+            content: ['../html/login.html', 'no'], //iframe的url，no代表不显示滚动条
+            end: function(){ //此处用于演示
+                layer.open({
+                    type: 2,
+                    title: '很多时候，我们想最大化看，比如像这个页面。',
+                    shadeClose: true,
+                    shade: false,
+                    maxmin: true, //开启最大化最小化按钮
+                    area: ['893px', '600px'],
+                    content: '//fly.layui.com/'
+                });
+            }
         });
         // layer.open({
         //     type: 1,
