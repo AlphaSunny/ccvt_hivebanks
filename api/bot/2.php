f0de138c4ac590fd653ad901a93bf4837c217d0c
宋星伟 <songxingwei@songxingweideMacBook-Pro.local>
2018-10-23 18:36:13 +0800

    1

--- api/bot/2.php
+++ api/bot/2.php
@@ -1,5 +1,5 @@
 <?php
 //echo shell_exec("sudo mkdir 222");
 
-echo exec("sudo cp /alidata/www/bot  /alidata/www/bot2", $res);
+echo exec("sudo cp -r /alidata/www/bot  /alidata/www/bot2", $res);
 ?>
