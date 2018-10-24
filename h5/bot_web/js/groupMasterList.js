cf8aa2daade70782e27166bd55966217fe15b455
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-24 13:55:14 +0800

    robot

--- h5/bot_web/js/groupMasterList.js
+++ h5/bot_web/js/groupMasterList.js
@@ -62,19 +62,19 @@ function GetGroupListFun(){
             tableIns.reload();
         });
 
-        form.on('checkbox(filter)', function(data){
-            console.log(data.elem); //得到checkbox原始DOM对象
-            console.log(data.elem.checked); //是否被选中，true或者false
-            console.log(data.value); //复选框value值，也可以通过data.elem.value得到
-            console.log(data.othis); //得到美化后的DOM对象
-        });
-
-        form.on('switch(filter)', function(data){
-            console.log(data.elem); //得到checkbox原始DOM对象
-            console.log(data.elem.checked); //开关是否开启，true或者false
-            console.log(data.value); //开关value值，也可以通过data.elem.value得到
-            console.log(data.othis); //得到美化后的DOM对象
-        });
+        // form.on('checkbox(filter)', function(data){
+        //     console.log(data.elem); //得到checkbox原始DOM对象
+        //     console.log(data.elem.checked); //是否被选中，true或者false
+        //     console.log(data.value); //复选框value值，也可以通过data.elem.value得到
+        //     console.log(data.othis); //得到美化后的DOM对象
+        // });
+        //
+        // form.on('switch(filter)', function(data){
+        //     console.log(data.elem); //得到checkbox原始DOM对象
+        //     console.log(data.elem.checked); //开关是否开启，true或者false
+        //     console.log(data.value); //开关value值，也可以通过data.elem.value得到
+        //     console.log(data.othis); //得到美化后的DOM对象
+        // });
     });
 }
 GetGroupListFun();
