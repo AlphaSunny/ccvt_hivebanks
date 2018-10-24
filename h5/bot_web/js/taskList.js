4939525d4fc5ae25d853ee2c4dea3a40b9842926
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-24 17:33:57 +0800

    robot

--- h5/bot_web/js/taskList.js
+++ h5/bot_web/js/taskList.js
@@ -149,46 +149,46 @@ $(function () {
             layer.msg(response.errmsg);
         });
 
-        var index = layer.open({
-            type: 2,
-            title: '编辑',
-            shadeClose: true,
-            shade: false,
-            maxmin: true, //开启最大化最小化按钮
-            area: ['800px', '450px'],
-            content: '../html/edit_task.html',
-            success: function (layero, index) {
-                var body = layer.getChildFrame('body', index);
-
-                //get group name
-                var groupNameInput = body.find("#groupNameInput");
-                // groupNameInput.val(taskGroupName);
-
-                //获取时间输入框
-                var timeInput = body.find("#time");
-
-                //获取内容输入框
-                var contentInput = body.find("#content");
-
-                //获取提交按钮
-                taskSubBtn = body.find("#taskSubBtn");
-
-                //提交编辑信息
-                taskSubBtn.click(function () {
-                    var time = timeInput.val(),
-                        content = contentInput.val(),
-                        group_id = "30";
-                    AddTask(token, time, group_id, content, function (response) {
-                        if (response.errcode == "0") {
-                            layer.close(index);
-                            GetTaskListFun();
-                        }
-                    }, function (response) {
-                        console.log(response);
-                    })
-                })
-
-            }
-        });
+        // var index = layer.open({
+        //     type: 2,
+        //     title: '编辑',
+        //     shadeClose: true,
+        //     shade: false,
+        //     maxmin: true, //开启最大化最小化按钮
+        //     area: ['800px', '450px'],
+        //     content: '../html/edit_task.html',
+        //     success: function (layero, index) {
+        //         var body = layer.getChildFrame('body', index);
+        //
+        //         //get group name
+        //         var groupNameInput = body.find("#groupNameInput");
+        //         // groupNameInput.val(taskGroupName);
+        //
+        //         //获取时间输入框
+        //         var timeInput = body.find("#time");
+        //
+        //         //获取内容输入框
+        //         var contentInput = body.find("#content");
+        //
+        //         //获取提交按钮
+        //         taskSubBtn = body.find("#taskSubBtn");
+        //
+        //         //提交编辑信息
+        //         taskSubBtn.click(function () {
+        //             var time = timeInput.val(),
+        //                 content = contentInput.val(),
+        //                 group_id = "30";
+        //             AddTask(token, time, group_id, content, function (response) {
+        //                 if (response.errcode == "0") {
+        //                     layer.close(index);
+        //                     GetTaskListFun();
+        //                 }
+        //             }, function (response) {
+        //                 console.log(response);
+        //             })
+        //         })
+        //
+        //     }
+        // });
     })
 });
