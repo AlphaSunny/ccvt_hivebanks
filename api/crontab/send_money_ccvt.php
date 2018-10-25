c78acc2b73c705648444386c5af7eb7b85bf0aa1
songxingwei <you@example.com>
2018-10-25 17:12:52 +0800

    1

--- api/crontab/send_money_ccvt.php
+++ api/crontab/send_money_ccvt.php
@@ -16,113 +16,112 @@ $ba_list = $db->fetchAll();
 print_r($ba_list);
 foreach ($ba_list as $k=>$value){
     echo $value['ba_id'];
-    //判断当前ba是否有余额
-    $data = sel_ba_amout($value['ba_id'],$day_start,$day_end);
-    if ($data['remark']==-1){
-        $data['ba_id'] = $value['ba_id'];
-        $data['time'] = date('Y-m-d H:i:s',time());
-        file_put_contents("ba_no_money.log",json_encode($data)."\n",FILE_APPEND);
-        break;
-    }
-    echo $value['ba_id'];
-    //循环ba,查询ba下微信用户及发言数
-    $sql = "select wechat,count(bot_message_id) as count from bot_message where ba_id='{$value['ba_id']}' AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}' group by wechat";
-    $db->query($sql);
-    $rows = $db->fetchAll();
-    foreach ($rows as $a=>$v){
-        $result = get_us_id($v['wechat']);
-        if ($result==0){
-            break;
-        }
-
-        //判断今日是否已经增过币
-        $send = send_money_if($value['ba_id'],$v['wechat'],$day_start,$day_end);
-        if ($send){
-            $db->Rollback($pInTrans);
-            echo $value['ba_id']."已增过币";
-            break;
-        }
-        //送币
-        $unit = la_unit();
-        $sql = "update us_base set base_amount=base_amount+'{$v['count']}'*'{$unit}' WHERE wechat='{$v['wechat']}'";
-        $db -> query($sql);
-        if (!$db->affectedRows()){
-            $db->Rollback($pInTrans);
-            echo "us修改余额失败";
-            break;
-        }
-
-        //ba减钱
-        $sql = "update ba_base set base_amount=base_amount-'{$v['count']}'*'{$unit}' WHERE ba_id='{$value['ba_id']}'";
-        $db -> query($sql);
-        if (!$db->affectedRows()){
-            $db->Rollback($pInTrans);
-            echo "ba修改余额失败";
-            break;
-        }
-
-
-        //增币记录
-        $d['bot_ls_id'] = get_guid();
-        $d['ba_id'] = $value['ba_id'];
-        $d['wechat'] = $v['wechat'];
-        $d['num'] = $v['count'];
-        $d['is_replacement'] = 0;
-        $d['send_time'] = date('Y-m-d H:i:s',time());
-        $d['bot_create_time'] = time();
-        $lgn_type = 'phone';
-        $d['tx_hash'] = hash('md5', $value['ba_id'] . $lgn_type . get_ip() . time() . date('Y-m-d H:i:s'));
-        $d['us_id'] = get_us_id($v['wechat']);
-        $sql = $db->sqlInsert("bot_Iss_records", $d);
-        $id = $db->query($sql);
-        if (!$id){
-            $db->Rollback($pInTrans);
-            echo "添加记录失败";
-            break;
-        }
-
-
-        //us添加基准资产变动记录
-        $us_type = 'us_send_balance';
-        $ctime = date('Y-m-d H:i:s');
-        $com_balance_us['hash_id'] = hash('md5', $d['us_id'] . $us_type . get_ip() . time() . rand(1000, 9999) . $ctime);
-        $com_balance_us['tx_id'] = $d['tx_hash'];
-        $com_balance_us['prvs_hash'] = get_recharge_pre_hash($d['us_id']);
-        $com_balance_us["credit_id"] = $d['us_id'];
-        $com_balance_us["debit_id"] = $value['ba_id'];
-        $com_balance_us["tx_type"] = "ba_send";
-        $com_balance_us["tx_amount"] = $v['count']*$unit;
-        $com_balance_us["credit_balance"] = get_us_account($d['us_id'])+($v['count']*$unit);
-        $com_balance_us["utime"] = time();
-        $com_balance_us["ctime"] = $ctime;
-
-        $sql = $db->sqlInsert("com_base_balance", $com_balance_us);
-        if (!$db->query($sql)) {
-            $db->Rollback($pInTrans);
-            break;
-        }
-
-        //ba添加基准资产变动记录
-        $us_type = 'ba_send_balance';
-        $com_balance_ba['hash_id'] = hash('md5', $value['ba_id']. $us_type . get_ip() . time() . rand(1000, 9999) . $ctime);
-        $com_balance_ba['tx_id'] = $d['tx_hash'];
-        $com_balance_ba['prvs_hash'] = get_recharge_pre_hash($value['ba_id']);
-        $com_balance_ba["credit_id"] = $value['ba_id'];
-        $com_balance_ba["debit_id"] = $d['us_id'];
-        $com_balance_ba["tx_type"] = "ba_send";
-        $com_balance_ba["tx_amount"] = $v['count']*$unit;
-        $com_balance_ba["credit_balance"] = get_ba_account($value['ba_id'])-($v['count']*$unit);
-        $com_balance_ba["utime"] = time();
-        $com_balance_ba["ctime"] = $ctime;
-
-        $sql = $db->sqlInsert("com_base_balance", $com_balance_ba);
-        if (!$db->query($sql)) {
-            $db->Rollback($pInTrans);
-            break;
-        }
-
-
-    }
+//    //判断当前ba是否有余额
+//    $data = sel_ba_amout($value['ba_id'],$day_start,$day_end);
+//    if ($data['remark']==-1){
+//        $data['ba_id'] = $value['ba_id'];
+//        $data['time'] = date('Y-m-d H:i:s',time());
+//        file_put_contents("ba_no_money.log",json_encode($data)."\n",FILE_APPEND);
+//        break;
+//    }
+//    //循环ba,查询ba下微信用户及发言数
+//    $sql = "select wechat,count(bot_message_id) as count from bot_message where ba_id='{$value['ba_id']}' AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}' group by wechat";
+//    $db->query($sql);
+//    $rows = $db->fetchAll();
+//    foreach ($rows as $a=>$v){
+//        $result = get_us_id($v['wechat']);
+//        if ($result==0){
+//            break;
+//        }
+//
+//        //判断今日是否已经增过币
+//        $send = send_money_if($value['ba_id'],$v['wechat'],$day_start,$day_end);
+//        if ($send){
+//            $db->Rollback($pInTrans);
+//            echo $value['ba_id']."已增过币";
+//            break;
+//        }
+//        //送币
+//        $unit = la_unit();
+//        $sql = "update us_base set base_amount=base_amount+'{$v['count']}'*'{$unit}' WHERE wechat='{$v['wechat']}'";
+//        $db -> query($sql);
+//        if (!$db->affectedRows()){
+//            $db->Rollback($pInTrans);
+//            echo "us修改余额失败";
+//            break;
+//        }
+//
+//        //ba减钱
+//        $sql = "update ba_base set base_amount=base_amount-'{$v['count']}'*'{$unit}' WHERE ba_id='{$value['ba_id']}'";
+//        $db -> query($sql);
+//        if (!$db->affectedRows()){
+//            $db->Rollback($pInTrans);
+//            echo "ba修改余额失败";
+//            break;
+//        }
+//
+//
+//        //增币记录
+//        $d['bot_ls_id'] = get_guid();
+//        $d['ba_id'] = $value['ba_id'];
+//        $d['wechat'] = $v['wechat'];
+//        $d['num'] = $v['count'];
+//        $d['is_replacement'] = 0;
+//        $d['send_time'] = date('Y-m-d H:i:s',time());
+//        $d['bot_create_time'] = time();
+//        $lgn_type = 'phone';
+//        $d['tx_hash'] = hash('md5', $value['ba_id'] . $lgn_type . get_ip() . time() . date('Y-m-d H:i:s'));
+//        $d['us_id'] = get_us_id($v['wechat']);
+//        $sql = $db->sqlInsert("bot_Iss_records", $d);
+//        $id = $db->query($sql);
+//        if (!$id){
+//            $db->Rollback($pInTrans);
+//            echo "添加记录失败";
+//            break;
+//        }
+//
+//
+//        //us添加基准资产变动记录
+//        $us_type = 'us_send_balance';
+//        $ctime = date('Y-m-d H:i:s');
+//        $com_balance_us['hash_id'] = hash('md5', $d['us_id'] . $us_type . get_ip() . time() . rand(1000, 9999) . $ctime);
+//        $com_balance_us['tx_id'] = $d['tx_hash'];
+//        $com_balance_us['prvs_hash'] = get_recharge_pre_hash($d['us_id']);
+//        $com_balance_us["credit_id"] = $d['us_id'];
+//        $com_balance_us["debit_id"] = $value['ba_id'];
+//        $com_balance_us["tx_type"] = "ba_send";
+//        $com_balance_us["tx_amount"] = $v['count']*$unit;
+//        $com_balance_us["credit_balance"] = get_us_account($d['us_id'])+($v['count']*$unit);
+//        $com_balance_us["utime"] = time();
+//        $com_balance_us["ctime"] = $ctime;
+//
+//        $sql = $db->sqlInsert("com_base_balance", $com_balance_us);
+//        if (!$db->query($sql)) {
+//            $db->Rollback($pInTrans);
+//            break;
+//        }
+//
+//        //ba添加基准资产变动记录
+//        $us_type = 'ba_send_balance';
+//        $com_balance_ba['hash_id'] = hash('md5', $value['ba_id']. $us_type . get_ip() . time() . rand(1000, 9999) . $ctime);
+//        $com_balance_ba['tx_id'] = $d['tx_hash'];
+//        $com_balance_ba['prvs_hash'] = get_recharge_pre_hash($value['ba_id']);
+//        $com_balance_ba["credit_id"] = $value['ba_id'];
+//        $com_balance_ba["debit_id"] = $d['us_id'];
+//        $com_balance_ba["tx_type"] = "ba_send";
+//        $com_balance_ba["tx_amount"] = $v['count']*$unit;
+//        $com_balance_ba["credit_balance"] = get_ba_account($value['ba_id'])-($v['count']*$unit);
+//        $com_balance_ba["utime"] = time();
+//        $com_balance_ba["ctime"] = $ctime;
+//
+//        $sql = $db->sqlInsert("com_base_balance", $com_balance_ba);
+//        if (!$db->query($sql)) {
+//            $db->Rollback($pInTrans);
+//            break;
+//        }
+//
+//
+//    }
 }
 $db->Commit($pInTrans);
 echo "OK!";
