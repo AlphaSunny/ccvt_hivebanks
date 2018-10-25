ed45ec819fb6c13d7cd721e421bc294344252641
Edwin <1045416962@qq.com>
2018-10-25 15:54:17 +0800

    robot

--- h5/bot_web/js/groupMasterList.js
+++ h5/bot_web/js/groupMasterList.js
@@ -20,12 +20,12 @@ function GetGroupListFun() {
             cols: [[                  //标题栏
                 {checkbox: true, sort: true, fixed: true, space: true}
                 , {field: 'id', title: 'id', width: 0}
-                , {field: 'name', title: '名称', width: 280}
+                , {field: 'name', title: '群名称', width: 150, align: 'center'}
                 // , {field: 'ba_id', title: 'ba_id', width: 380}
-                , {field: 'del', title: '状态', width: 100}
-                , {field: 'is_del', title: '是否开启状态', width: 100}
-                , {field: 'flirt', title: '调戏功能', width: 100}
-                , {field: 'is_flirt', title: '是否开启调戏功能', width: 100}
+                , {field: 'del', title: '状态', width: 100, align: 'center'}
+                , {field: 'is_del', title: '是否开启状态', width: 100, align: 'center'}
+                , {field: 'flirt', title: '调戏功能', width: 100, align: 'center'}
+                , {field: 'is_flirt', title: '是否开启调戏功能', width: 100, align: 'center'}
                 , {fixed: 'right', title: '操作', width: 240, align: 'center', toolbar: '#barOption'} //这里的toolbar值是模板元素的选择器
             ]],
             page: true,
