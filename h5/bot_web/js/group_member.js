c38ecb774a7b3530d7827bff7c4444fbba5dffa3
Edwin <1045416962@qq.com>
2018-10-26 15:25:44 +0800

    robot

--- h5/bot_web/js/group_member.js
+++ h5/bot_web/js/group_member.js
@@ -1,24 +1,36 @@
 $(function () {
-    $('#groupMemberTable').DataTable();
     //获取token
     var token = GetCookie("robot_token");
 
     //获取对应id
     var group_id = GetQueryString("group_id");
-    
+
+    $('#groupMemberTable').DataTable({
+        "ajax": "http://ccvt_test.fnying.com/api/bot_web/group_members_list.php?token=" + token + "&group_id=" + group_id,
+        "columns": [
+            {"data": "name"}
+            // {"data": "position"},
+            // {"data": "office"},
+            // {"data": "extn"},
+            // {"data": "start_date"},
+            // {"data": "salary"}
+        ]
+    });
+
+
     //获取群成员列表
-    GetGroupMember(token,group_id, function (response) {
-        console.log(response);
-        if(response.errcode == "0"){
-            var data = response.rows, tr = "";
-            $.each(data, function (i, val) {
-                tr+="<tr>" +
-                    "<td>"+ data[i].name +"</td>" +
-                    "</tr>"
-            });
-            $("#groupMember").html(tr);
-        }
-    }, function (response) {
-        layer.msg(response.errmsg);
-    })
+    // GetGroupMember(token,group_id, function (response) {
+    //     console.log(response);
+    //     if(response.errcode == "0"){
+    //         var data = response.rows, tr = "";
+    //         $.each(data, function (i, val) {
+    //             tr+="<tr>" +
+    //                 "<td>"+ data[i].name +"</td>" +
+    //                 "</tr>"
+    //         });
+    //         $("#groupMember").html(tr);
+    //     }
+    // }, function (response) {
+    //     layer.msg(response.errmsg);
+    // })
 });
