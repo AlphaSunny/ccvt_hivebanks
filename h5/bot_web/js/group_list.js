e39af690221ab0fcb0e81423104e2e8026a5f04d
Edwin <1045416962@qq.com>
2018-10-26 16:46:35 +0800

    robot

--- h5/bot_web/js/group_list.js
+++ h5/bot_web/js/group_list.js
@@ -8,9 +8,9 @@ $(function () {
                 {"data": "id", "class": "id"},
                 {"data": "name", "class": "name"},
                 {"data": "del", "class": "del"},
-                {"data": "is_del", "class": "is_del"},
+                {"data": "is_del", "class": "is_del none"},
                 {"data": "flirt", "class": "flirt"},
-                {"data": "is_flirt", "class": "is_flirt"},
+                {"data": "is_flirt", "class": "is_flirt none"},
             ]
         });
         setTimeout(function () {
