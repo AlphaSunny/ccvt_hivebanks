e66a9d301eb2340ed1199316b7101338a440010f
Edwin <1045416962@qq.com>
2018-10-26 11:10:59 +0800

    robot

--- h5/bot_web/js/task_list.js
+++ h5/bot_web/js/task_list.js
@@ -1,12 +1,12 @@
 $(function () {
     var token = GetCookie("robot_token");
     GetTaskList(token, function (response) {
-        if(response.errcode == "0"){
+        if (response.errcode == "0") {
             var data = response.rows, tr = "";
-            tr+="<tr class='text-center'>" +
-                "<td>"+ data[i].time +"</td>" +
-                "<td>"+ data[i].content +"</td>" +
-                "<td>"+ data[i].name +"</td>" +
+            tr += "<tr class='text-center'>" +
+                "<td>" + data[i].time + "</td>" +
+                "<td>" + data[i].content + "</td>" +
+                "<td>" + data[i].name + "</td>" +
                 "<td>" +
                 "<button class='btn-success btn-sm editBtn'>编辑</button>" +
                 "<button class='btn-sm btn-danger delBtn margin-left-5'>删除</button>" +
@@ -17,6 +17,18 @@ $(function () {
             $("#groupListTable").html(tr);
         }
     }, function (response) {
-        console.log(response);
+        layer.msg(response.errmsg);
     });
+
+    //删除任务
+    $(document).on("click", ".delBtn", function () {
+        layer.confirm('确定删除该条数据？', {
+            btn: ['取消','确认'] //按钮
+        })
+        // , function(){
+        //
+        // }, function(){
+        //     // layer.msg('的确很重要', {icon: 1});
+        // });
+    })
 });
