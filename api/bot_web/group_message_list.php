e15945b803293a9e4d8920b3f27f710b6b33aeec
songxingwei <you@example.com>
2018-10-26 16:01:20 +0800

    1

---
+++ api/bot_web/group_message_list.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once '../inc/common.php';
+require_once 'db/bot.php';
+
+
+header("cache-control:no-cache,must-revalidate");
+header("Content-Type:application/json;charset=utf-8");
+
+/*
+========================== 聊天记录查询 ==========================
+GET参数
+  token                用户token
+
+返回
+  errcode = 0     请求成功
+  rows                 记录数组
+*/
+
+php_begin();
+$args = array('token','group_id');
+chk_empty_args('GET', $args);
+
+
+
+// 返回数据做成
+$rtn_ary = array();
+$rtn_ary['errcode'] = '0';
+$rtn_ary['errmsg'] = '';
+$rtn_ary['count'] = count($rows);
+$rtn_ary['rows'] = $rows;
+$rtn_str = json_encode($rtn_ary);
+php_end($rtn_str);
+
