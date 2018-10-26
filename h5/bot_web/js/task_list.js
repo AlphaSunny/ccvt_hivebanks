131b2ae884e06aaaa0eb44c3aefaa0c3cc87a110
Edwin <1045416962@qq.com>
2018-10-26 11:06:28 +0800

    robot

--- h5/bot_web/js/task_list.js
+++ h5/bot_web/js/task_list.js
@@ -3,16 +3,16 @@ $(function () {
     GetTaskList(token, function (response) {
         if(response.errcode == "0"){
             var data = response.rows, tr = "";
+            tr+="<tr class='text-center'>" +
+                "<td>"+ data[i].time +"</td>" +
+                "<td>"+ data[i].content +"</td>" +
+                "<td>"+ data[i].name +"</td>" +
+                "<td>" +
+                "<button class='btn-success btn-sm editBtn'>编辑</button>" +
+                "<button class='btn-sm btn-danger delBtn margin-left-5'>删除</button>" +
+                "</td>" +
+                "</tr>";
             $.each(data, function (i, val) {
-                tr+="<tr class='text-center'>" +
-                    "<td>"+ data[i].time +"</td>" +
-                    "<td>"+ data[i].name +"</td>" +
-                    "<td>"+ data[i].content +"</td>" +
-                    "<td>" +
-                    "<button class='btn-success btn-sm editBtn'>编辑</button>" +
-                    "<button class='btn-sm btn-danger delBtn margin-left-5'>删除</button>" +
-                    "</td>" +
-                    "</tr>"
             });
             $("#groupListTable").html(tr);
         }
