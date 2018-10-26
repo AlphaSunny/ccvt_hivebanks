5ff393cebbafd3eb2ee811ec4ead7e515b2bb6f7
Edwin <1045416962@qq.com>
2018-10-26 09:47:50 +0800

    robot

--- h5/bot_web/js/group_list.js
+++ h5/bot_web/js/group_list.js
@@ -4,10 +4,10 @@ $(function () {
         if(response.errcode == "0"){
             var data = response.rows, tr = "";
             $.each(data, function (i, val) {
-                tr+="<tr class='text-center'>" +
+                tr+="<tr class='text-center trItem'>" +
                     "<td>"+ data[i].name +"</td>" +
-                    "<td>"+ data[i].del +"</td>" +
-                    "<td>"+ data[i].flirt +"</td>" +
+                    "<td class='is_del' name="+ data[i].is_del +">"+ data[i].del +"</td>" +
+                    "<td class='is_flirt' name="+ data[i].is_flirt +">"+ data[i].flirt +"</td>" +
                     "<td>" +
                     "<button class='btn-success btn-sm editBtn'>编辑</button>" +
                     // "<button class='btn-sm btn-danger delBtn margin-left-5'>详情</button>" +
@@ -22,6 +22,14 @@ $(function () {
 
     //编辑对应的群主-弹出编辑框
     $(document).on("click", ".editBtn", function (response) {
+        var is_del = $(this).parents(".trItem").find(".is_del").attr("name");
+        var is_flirt = $(this).parents(".trItem").find(".is_flirt").attr("name");
+        if(is_del == "1"){
+            $(".runSwitch").attr("checked", true);
+        }
+        if(is_flirt == "1"){
+            $(".trickSwitch").attr("checked", true);
+        }
         $("#editGroupModal").modal("show");
     })
 
