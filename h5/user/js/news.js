0cfbf3cb137d4b5fb539cf377355c35b95ac604e
Edwin <1045416962@qq.com>
2018-10-29 18:46:30 +0800

    h5

--- h5/user/js/news.js
+++ h5/user/js/news.js
@@ -41,51 +41,15 @@ $(function () {
                 a+="<p><a href='javascript:;' name="+ data[i].news_id +">"+ data[i].title +"</a></p>"
             });
             $(".latestNewsText").html(a);
-            // setInterval(function () {
-            //     // console.log("res");
-            //     $(".latestNewsText").css("transform","translateY(-2.5rem)");
-            // }, 3000);
-
         }
     }, function (response) {
         if(response.errcode == "-1"){
             $(".latestNewsText").html("<span>暂无更多动态</span>");
         }
     });
-
-    $.fn.extend({
-        Scroll: function (opt, callback) {
-            //参数初始化
-            if (!opt) var opt = {};
-            var _this = this.eq(0).find(".latestNewsText:first");
-            var lineH = _this.find(".latestNewsText>p:first").height(), //获取行高
-                line = opt.line ? parseInt(opt.line, 10) : parseInt(this.height() / lineH, 10), //每次滚动的行数，默认为一屏，即父容器高度
-                speed = opt.speed ? parseInt(opt.speed, 10) : 500, //卷动速度，数值越大，速度越慢（毫秒）
-                timer = opt.timer ? parseInt(opt.timer, 10) : 3000; //滚动的时间间隔（毫秒）
-            if (line == 0) line = 1;
-            var upHeight = 0 - line * lineH;
-            //滚动函数
-            scrollUp = function () {
-                _this.animate({
-                    marginTop: upHeight
-                }, speed, function () {
-                    for (var i = 1; i <= line; i++) {
-                        _this.find(".latestNewsText>p:first").appendTo(_this);
-                    }
-                    _this.css({marginTop: 0});
-                });
-            };
-            //鼠标事件绑定
-            var timerID = "";
-            _this.hover(function () {
-                clearInterval(timerID);
-            }, function () {
-                timerID = setInterval("scrollUp()", timer);
-            }).mouseout();
-        }
-    })
+    setInterval(function () {
+        console.log("res");
+        $(".latestNewsText").css("transform","translateY(-2.5rem)");
+    }, 3000);
 });
 
-$(document).ready(function () {
-    $(".latestNewsText").Scroll({line: 1, speed: 500, timer: 1000});
-});
