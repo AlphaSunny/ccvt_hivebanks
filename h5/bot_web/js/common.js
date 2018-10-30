2228be815482d88a7c2c80db2bc1e35317c96f28
Edwin <1045416962@qq.com>
2018-10-30 14:34:22 +0800

    h5

--- h5/bot_web/js/common.js
+++ h5/bot_web/js/common.js
@@ -214,3 +214,13 @@ function GetGroupMember(token, group_id,suc_func, error_func) {
         };
     CallRobotApi(api_url, post_data, suc_func, error_func);
 }
+
+//获取聊天记录
+function GetNewsRecord(token, group_id, suc_func, error_func) {
+    var api_url = "group_message_list.php",
+        post_data = {
+            "token": token,
+            "group_id": group_id
+        };
+    CallRobotApi(api_url, post_data, suc_func, error_func);
+}
