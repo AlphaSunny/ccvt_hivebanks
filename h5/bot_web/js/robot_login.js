3987717ebcdbe8f789ce685f9e6501f9f7a51428
Edwin <1045416962@qq.com>
2018-10-30 16:56:29 +0800

    h5

--- h5/bot_web/js/robot_login.js
+++ h5/bot_web/js/robot_login.js
@@ -1,7 +1,7 @@
 $(function () {
     var url = getRootPath();
    $.get({
-       url:url + "api/bot/get_qrcode.php",
+       url:url + "/api/bot/get_qrcode.php",
        async:false,
        success:function (response) {
            console.log(response);
