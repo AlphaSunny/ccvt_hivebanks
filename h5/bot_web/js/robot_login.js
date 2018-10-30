1cdf125bb554a06dfafea64303bcc727f9eeb7a5
Edwin <1045416962@qq.com>
2018-10-30 17:02:58 +0800

    h5

--- h5/bot_web/js/robot_login.js
+++ h5/bot_web/js/robot_login.js
@@ -1,10 +1,20 @@
 $(function () {
     var url = getRootPath();
-   $.get({
-       url:url + "/api/bot/get_qrcode.php",
-       async:false,
-       success:function (response) {
-           console.log(response);
-       }
-   })
+
+    function GetQr() {
+        $.get({
+            url: url + "/api/bot/get_qrcode.php",
+            async: false,
+            success: function (response) {
+                
+                $("#qr_img").src(response.qr_path);
+            }
+        })
+    }
+
+    GetQr();
+
+    $(".qr_btn").click(function () {
+        GetQr();
+    });
 });
