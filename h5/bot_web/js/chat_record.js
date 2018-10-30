ed5f4686c4f5258c0483dd1fede20823e7e8fdad
Edwin <1045416962@qq.com>
2018-10-30 14:37:54 +0800

    h5

--- h5/bot_web/js/chat_record.js
+++ h5/bot_web/js/chat_record.js
@@ -6,13 +6,19 @@ $(function () {
     var group_id = GetQueryString("group_id");
 
     //click into news record
+    var status = "1";
     $(".weChatBtn").click(function () {
        $(this).addClass("activeWeChatBtn").siblings(".weChatBtn").removeClass("activeWeChatBtn");
+       status = $(this).attr("title");
+        GetNewsRecordFun(status);
+   });
 
-       GetNewsRecord(token, group_id, function (response) {
+    function GetNewsRecordFun(status) {
+        GetNewsRecord(token, group_id, status, function (response) {
             console.log(response);
-       }, function (response) {
-           layer.msg("获取失败,请稍后重试！");
-       });
-   })
+        }, function (response) {
+            layer.msg("获取失败,请稍后重试！");
+        });
+    }
+    GetNewsRecordFun(status);
 });
