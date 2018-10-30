cec1986eadb6eb3bd8e93f6dbc33d3108bd922b5
Edwin <1045416962@qq.com>
2018-10-30 17:20:39 +0800

    h5

--- h5/bot_web/js/robot_login.js
+++ h5/bot_web/js/robot_login.js
@@ -1,21 +1,22 @@
-$(function () {
-    var url = getRootPath();
-
-    function GetQr() {
-        $.get({
-            url: url + "/api/bot/get_qrcode.php",
-            async: false,
-            success: function (response) {
-            console.log(response);
-            console.log(response.qr_path);
-                $("#qr_img").attr("src",response.rows.qr_path);
-            }
-        })
-    }
-
-    GetQr();
-
-    $(".qr_btn").click(function () {
-        GetQr();
-    });
-});
+// $(function () {
+//     var url = getRootPath();
+//
+//     function GetQr() {
+//         $.get({
+//             url: url + "/api/bot/get_qrcode.php",
+//             async: false,
+//             success: function (response) {
+//                 if(response.rows.robot_alive == "1"){
+//
+//                 }
+//                 $("#qr_img").attr("src",response.rows.qr_path);
+//             }
+//         })
+//     }
+//
+//     GetQr();
+//
+//     $(".qr_btn").click(function () {
+//         GetQr();
+//     });
+// });
