524034c1f5ad46fa781b1f4505777d2d87f6c77b
Edwin <1045416962@qq.com>
2018-10-31 14:22:52 +0800

    h5

--- api/bot_web/page/statistical.php
+++ api/bot_web/page/statistical.php
@@ -15,103 +15,103 @@
 <body>
 <div class="swiper-container">
     <div class="swiper-wrapper">
-<!--        <section id="statistical" class="swiper-slide">-->
-<!--            <div class="container">-->
-<!--                <div class="row">-->
-<!--                    <div class="col-md-12">-->
-<!--                        <h3>CCVT奖励统计</h3>-->
-<!--                        <div class="margin-bottom-2">-->
-<!--                            --><?php
-//                            require_once '../../inc/common.php';
-//                            ini_set("display_errors", "off");
-//                            $args = array('datetime');
-//                            chk_empty_args('GET', $args);
-//                            $db = new DB_COM();
-//                            $datetime = base64_decode(get_arg_str('GET', 'datetime'));
-//                            $group_name = base64_decode(get_arg_str('GET', 'group_name'));
-//                            $day_start = strtotime(date($datetime . ' 00:00:00'));
-//                            $day_end = strtotime(date($datetime . ' 23:59:59'));
-//
-//                            $sql = "select sum(num) as all_send_ccvt from bot_Iss_records WHERE bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
-//                            $db->query($sql);
-//                            $all_send_ccvt = $db->getField($sql, all_send_ccvt); //总赠送ccvt数量
-//
-//                            $sql = "select count(bot_message_id) as all_message from bot_message WHERE group_name='{$group_name}' AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
-//                            $db->query($sql);
-//                            $all_message = $db->getField($sql, all_message); //总聊天数量
-//                            ?>
-<!--                            <div class="sm_title_text_color">-->
-<!--                                <p>所属群:《风赢科技绝密小锋队》</p>-->
-<!--                                <p class="font-size-14">时间:--><?php //echo $datetime; ?><!--</p>-->
-<!--                            </div>-->
-<!--                            <div class="flex space-between font-size-14 sm_title_text_color">-->
-<!--                                <p>今日奖励总数量:--><?php //echo $all_send_ccvt; ?><!--(CCVT)</p>-->
-<!--                                <p>今日发言总数量:--><?php //echo $all_message; ?><!--(条)</p>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!---->
-<!--                        <div class="table-responsive">-->
-<!--                            <table class="table" id="statisticalTable">-->
-<!--                                <thead>-->
-<!--                                <tr>-->
-<!--                                    <!--                            <th>排名</th>-->-->
-<!--                                    <th class="text-center">名称</th>-->
-<!--                                    <!--                            <th class="text-center">拥有数量(CCVT)</th>-->-->
-<!--                                    <th class="text-center">获得(CCVT)</th>-->
-<!--                                    <th class="text-center">发言数</th>-->
-<!--                                </tr>-->
-<!--                                </thead>-->
-<!--                                <tbody>-->
-<!--                                --><?php
-//                                $sql = "select * from bot_Iss_records WHERE bot_create_time BETWEEN '{$day_start}' AND '{$day_end}' ORDER BY num DESC ";
-//                                $db->query($sql);
-//                                $list = $db->fetchAll();
-//                                foreach ($list as $k => $v) {
-//                                    ?>
-<!--                                    <tr>-->
-<!--                                        <!--                            <td>-->--><?php ////if ($k==0){
-//                                        ?>
-<!--                                        <!--                                    🥇-->-->
-<!--                                        <!--                                -->--><?php ////}elseif($k==1){
-//                                        ?>
-<!--                                        <!--                                    🥈-->-->
-<!--                                        <!--                                -->--><?php ////}elseif($k==2){
-//                                        ?>
-<!--                                        <!--                                    🥉-->-->
-<!--                                        <!--                                -->--><?php ////}else{ echo $k;}
-//                                        ?>
-<!--                                        <!--                            </td>-->-->
-<!--                                        <td>--><?php //echo $v['wechat']; ?><!--</td>-->
-<!--                                        <!--                            <td>-->-->
-<!--                                        <!--                                -->--><?php
-//                                        //                                    $sql = "select unit from la_base limit 1";
-//                                        //                                    $db->query($sql);
-//                                        //                                    $unit = $db->getField($sql,'unit');
-//                                        //                                    $sql = "select base_amount from us_base WHERE us_id='{$v['us_id']}'";
-//                                        //                                    $db->query($sql);
-//                                        //                                    $base_amount = $db->getField($sql,'base_amount')/$unit;
-//                                        //                                    echo $base_amount;
-//                                        //
-//                                        ?>
-<!--                                        <!--                            </td>-->-->
-<!--                                        <td>--><?php //echo $v['num']; ?><!--</td>-->
-<!--                                        <td>-->
-<!--                                            --><?php
-//                                            $sql = "select count(bot_message_id) as all_message from bot_message WHERE group_name='测试2' AND wechat='{$v['wechat']}' AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
-//                                            $db->query($sql);
-//                                            $count = $db->getField($sql, 'all_message');
-//                                            echo $count;
-//                                            ?>
-<!--                                        </td>-->
-<!--                                    </tr>-->
-<!--                                --><?php //} ?>
-<!--                                </tbody>-->
-<!--                            </table>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </section>-->
+        <section id="statistical" class="swiper-slide">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3>CCVT奖励统计</h3>
+                        <div class="margin-bottom-2">
+                            <?php
+                            require_once '../../inc/common.php';
+                            ini_set("display_errors", "off");
+                            $args = array('datetime');
+                            chk_empty_args('GET', $args);
+                            $db = new DB_COM();
+                            $datetime = base64_decode(get_arg_str('GET', 'datetime'));
+                            $group_name = base64_decode(get_arg_str('GET', 'group_name'));
+                            $day_start = strtotime(date($datetime . ' 00:00:00'));
+                            $day_end = strtotime(date($datetime . ' 23:59:59'));
+
+                            $sql = "select sum(num) as all_send_ccvt from bot_Iss_records WHERE bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
+                            $db->query($sql);
+                            $all_send_ccvt = $db->getField($sql, all_send_ccvt); //总赠送ccvt数量
+
+                            $sql = "select count(bot_message_id) as all_message from bot_message WHERE group_name='{$group_name}' AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
+                            $db->query($sql);
+                            $all_message = $db->getField($sql, all_message); //总聊天数量
+                            ?>
+                            <div class="sm_title_text_color">
+                                <p>所属群:《风赢科技绝密小锋队》</p>
+                                <p class="font-size-14">时间:<?php echo $datetime; ?></p>
+                            </div>
+                            <div class="flex space-between font-size-14 sm_title_text_color">
+                                <p>今日奖励总数量:<?php echo $all_send_ccvt; ?>(CCVT)</p>
+                                <p>今日发言总数量:<?php echo $all_message; ?>(条)</p>
+                            </div>
+                        </div>
+
+                        <div class="table-responsive">
+                            <table class="table" id="statisticalTable">
+                                <thead>
+                                <tr>
+                                    <!--                            <th>排名</th>-->
+                                    <th class="text-center">名称</th>
+                                    <!--                            <th class="text-center">拥有数量(CCVT)</th>-->
+                                    <th class="text-center">获得(CCVT)</th>
+                                    <th class="text-center">发言数</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                $sql = "select * from bot_Iss_records WHERE bot_create_time BETWEEN '{$day_start}' AND '{$day_end}' ORDER BY num DESC ";
+                                $db->query($sql);
+                                $list = $db->fetchAll();
+                                foreach ($list as $k => $v) {
+                                    ?>
+                                    <tr>
+                                        <!--                            <td>--><?php //if ($k==0){
+                                        ?>
+                                        <!--                                    🥇-->
+                                        <!--                                --><?php //}elseif($k==1){
+                                        ?>
+                                        <!--                                    🥈-->
+                                        <!--                                --><?php //}elseif($k==2){
+                                        ?>
+                                        <!--                                    🥉-->
+                                        <!--                                --><?php //}else{ echo $k;}
+                                        ?>
+                                        <!--                            </td>-->
+                                        <td><?php echo $v['wechat']; ?></td>
+                                        <!--                            <td>-->
+                                        <!--                                --><?php
+                                        //                                    $sql = "select unit from la_base limit 1";
+                                        //                                    $db->query($sql);
+                                        //                                    $unit = $db->getField($sql,'unit');
+                                        //                                    $sql = "select base_amount from us_base WHERE us_id='{$v['us_id']}'";
+                                        //                                    $db->query($sql);
+                                        //                                    $base_amount = $db->getField($sql,'base_amount')/$unit;
+                                        //                                    echo $base_amount;
+                                        //
+                                        ?>
+                                        <!--                            </td>-->
+                                        <td><?php echo $v['num']; ?></td>
+                                        <td>
+                                            <?php
+                                            $sql = "select count(bot_message_id) as all_message from bot_message WHERE group_name='测试2' AND wechat='{$v['wechat']}' AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
+                                            $db->query($sql);
+                                            $count = $db->getField($sql, 'all_message');
+                                            echo $count;
+                                            ?>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         <div class="swiper-slide" id="chat">
             <p class="text-center title"><?php echo base64_decode($_REQUEST['group_name']); ?></p>
             <ul class="chatList">
