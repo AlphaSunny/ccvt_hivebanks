c50d5c3249584f12badebe12d9b3224011fcc757
Edwin <1045416962@qq.com>
2018-10-31 14:06:13 +0800

    h5

--- api/bot_web/page/statistical.php
+++ api/bot_web/page/statistical.php
@@ -41,10 +41,10 @@
                             $all_message = $db->getField($sql, all_message); //总聊天数量
                             ?>
                             <div>
-                                <h3>风赢科技绝密小锋队</h3>
-                                <p>时间:<?php echo $datetime; ?></p>
+                                <p>风赢科技绝密小锋队</p>
+                                <p class="font-size-14">时间:<?php echo $datetime; ?></p>
                             </div>
-                            <div class="flex space-between">
+                            <div class="flex space-between font-size-14">
                                 <p>今日奖励总数量:<?php echo $all_send_ccvt; ?>(CCVT)</p>
                                 <p>今日发言总数量:<?php echo $all_message; ?>(条)</p>
                             </div>
