04bf0f3295bb2f454dc5c8d0e7e68fb556df5b64
Edwin <1045416962@qq.com>
2018-11-01 18:16:20 +0800

    h5

--- h5/assets/js/cnt.js
+++ h5/assets/js/cnt.js
@@ -1,12 +1,33 @@
 var arr = document.cookie.match(new RegExp("(^| )UUID=([^;]*)(;|$)"));
-var uuid = (arr != null) ? unescape(arr[2]):'';
+var uuid = (arr != null) ? unescape(arr[2]) : '';
 var ref = escape(document.referrer);
 var url = escape(window.location.href);
 var xmlhttp;
 if (window.XMLHttpRequest) {
-    xmlhttp=new XMLHttpRequest();
+    xmlhttp = new XMLHttpRequest();
 } else {
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
-xmlhttp.open("GET","http://www.fnying.com/php/cnt_action.php?referrer=" + ref + "&url=" + url + "&uuid=" + uuid, true);
+xmlhttp.open("GET", "http://www.fnying.com/php/cnt_action.php?referrer=" + ref + "&url=" + url + "&uuid=" + uuid, true);
 xmlhttp.send();
+
+$(function () {
+    //获取uuid
+    // var arr = document.cookie.match(new RegExp("(^| )UUID=([^;]*)(;|$)"));
+    // var uuid = uuid = (arr != null) ? unescape(arr[2]) : '';
+    if (!uuid) {
+        uuid = new Date().getTime();
+        // 取得UUID
+        var url = "http://www.fnying.com/php/get_uuid.php";
+        $.get({
+            url: url,
+            success: function (response) {
+                uuid = response.uuid;
+                SetCookie('UUID', uuid);
+            },
+            error: function (response) {
+                console.log(response);
+            }
+        })
+    }
+});
