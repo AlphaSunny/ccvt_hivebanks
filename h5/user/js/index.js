a3641c7d4883a69c02b6be5296e75e05aae11786
Edwin <1045416962@qq.com>
2018-11-01 17:23:43 +0800

    h5

--- h5/user/js/index.js
+++ h5/user/js/index.js
@@ -63,27 +63,27 @@ $(function () {
     });
 
 
-    // var $this = $(".latestNews");
-    // var scrollTimer;
-    // $this.hover(function () {
-    //     clearInterval(scrollTimer);
-    // }, function () {
-    //     scrollTimer = setInterval(function () {
-    //         scrollNews($this);
-    //     }, 3000);
-    // }).trigger("mouseleave");
-    //
-    // function scrollNews(obj) {
-    //     var $self = obj.find("ul");
-    //     var lineHeight = $self.find("li:first").height();
-    //     $self.animate({
-    //         "marginTop": -lineHeight + "px"
-    //     }, 2000, function () {
-    //         $self.css({
-    //             marginTop: 0
-    //         }).find("li:first").appendTo($self);
-    //     })
-    // }
+    var $this = $(".latestNews");
+    var scrollTimer;
+    $this.hover(function () {
+        clearInterval(scrollTimer);
+    }, function () {
+        scrollTimer = setInterval(function () {
+            scrollNews($this);
+        }, 3000);
+    }).trigger("mouseleave");
+
+    function scrollNews(obj) {
+        var $self = obj.find("ul");
+        var lineHeight = $self.find("li:first").height();
+        $self.animate({
+            "marginTop": -lineHeight + "px"
+        }, 2000, function () {
+            $self.css({
+                marginTop: 0
+            }).find("li:first").appendTo($self);
+        })
+    }
 
     //to news info
     $(document).on("click", ".toNewsInfo", function () {
