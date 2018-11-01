f198866a6e0cfa1a517a9e3a73ac10e2d2dabe04
Edwin <1045416962@qq.com>
2018-11-01 18:21:49 +0800

    h5

--- h5/assets/js/cnt.js
+++ h5/assets/js/cnt.js
@@ -18,7 +18,7 @@ $(function () {
     if (!uuid) {
         uuid = new Date().getTime();
         // 取得UUID
-        var url = "http://www.fnying.com/php/get_uuid.php";
+        var url = "http://ow.fnying.com/website/get_uuid.php";
         $.get({
             url: url,
             success: function (response) {
