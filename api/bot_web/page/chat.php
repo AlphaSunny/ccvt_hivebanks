bfac15b8794c9f36b62eef2c3b9a73bf94ab8f7d
Edwin <1045416962@qq.com>
2018-11-05 11:36:54 +0800

    h5

--- api/bot_web/page/chat.php
+++ api/bot_web/page/chat.php
@@ -151,6 +151,32 @@
     }
 </style>
 <body>
+<!--nav-->
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">CCVT</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">BA通道</a></li>
+                <li><a href="#">CA通道</a></li>
+                <li><a href="#">用户通道</a></li>
+                <li><a href="#">查看奖励统计</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!--nav-->
 <div id="chat">
     <p class="text-center title"><?php echo base64_decode($_REQUEST['group_name']); ?></p>
     <div class="box">
