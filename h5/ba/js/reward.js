736b71686b86186aa73dc8fa010eaba8057121f8
Edwin <1045416962@qq.com>
2018-11-06 16:02:22 +0800

    h5

--- h5/ba/js/reward.js
+++ h5/ba/js/reward.js
@@ -1,5 +1,40 @@
 $(function () {
-   $(".confirmTransfer").click(function () {
-       $("#transferModal").modal("show");
-   });
+    //get token
+    var token = GetCookie("ba_token");
+
+    var account = "", ccvt_num = "", why = "";
+    $(".confirmTransfer").click(function () {
+        ShowLoading("show");
+        account = $("#userNickName").val();
+        ccvt_num = $("#send_amount").val();
+        why = $("#sendWhy").val();
+        if (account.length <= 0) {
+            LayerFun("pleaseEnterOtherNickName");
+            ShowLoading("hide");
+            return;
+        }
+        if (ccvt_num.length <= 0) {
+            LayerFun("pleaseEnterTransferAmount");
+            ShowLoading("hide");
+            return;
+        }
+        if (why.length <= 0) {
+            LayerFun("pleaseEnterTransferReason");
+            ShowLoading("hide");
+            return;
+        }
+        $("#transferModal").modal("show");
+    });
+
+    $(".confirmTransferBtn").click(function () {
+        Transfer_CCVT(token, account, ccvt_num, why, function (response) {
+            if(response.errcode == "0"){
+                ShowLoading("hide");
+                console.log(response);
+            }
+        }, function (response) {
+            ShowLoading("hide");
+            console.log(response);
+        })
+    });
 });
