917410de5ba6e78506fb5dfbd73be044bc39e306
pool <epsilonsunny@gmai.com>
2018-11-06 16:08:08 +0800

    on

--- h5/user/js/index.js
+++ h5/user/js/index.js
@@ -29,7 +29,7 @@ $(function () {
     });
 
     $('.contract').click(function () {
-        window.location.href = 'contract/ccvtContract.html';
+        window.location.href = 'h5/contract/ccvtContract.html';
     });
 
 
