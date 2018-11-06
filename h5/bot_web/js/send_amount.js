7ab8962f0c49cecb13464ec1c7f908929652542a
Edwin <1045416962@qq.com>
2018-11-06 11:43:28 +0800

    h5

--- h5/bot_web/js/send_amount.js
+++ h5/bot_web/js/send_amount.js
@@ -20,7 +20,7 @@ $(function () {
         ajax: {
             "url": url + "/api/bot_web/iss_records_list.php?token=" + encodeURIComponent(token),
             "type": "GET",
-            "dataSrc":data,
+            "dataSrc":"data",
             success: function (res) {
                 console.log(res);
             }
