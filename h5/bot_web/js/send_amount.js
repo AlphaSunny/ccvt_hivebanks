58d50c98ff8f4808266993ce66ffaacc00a0bf4c
Edwin <1045416962@qq.com>
2018-11-06 13:54:05 +0800

    h5

--- h5/bot_web/js/send_amount.js
+++ h5/bot_web/js/send_amount.js
@@ -4,25 +4,14 @@ $(function () {
 
 
     var url = getRootPath();
-    // $('#sendAmountTable').DataTable({
-    //     "ajax": url + "/api/bot_web/iss_records_list.php?token=" + encodeURIComponent(token),
-    //     "order": [[3, "desc"]],
-    //     "deferRender":true,
-    //     "columns": [
-    //         {"data": "wechat"},
-    //         {"data": "amount"},
-    //         {"data": "num"},
-    //         {"data": "send_time"}
-    //     ]
-    // });
+
     $.ajax({
         "url": url + "/api/bot_web/iss_records_list.php?token=" + encodeURIComponent(token),
             "type": "GET",
             success: function (data) {
-            console.log(data.data);
-            console.log(JSON.stringify(data));
+            $(".all_amount").text(data.all_amount);
+            $(".all_chat").text(data.all_chat);
                 $('#sendAmountTable').DataTable({
-                    // ajax:data,
                     order: [[3, "desc"]],
                     deferRender:true,
                     data:data.data,
