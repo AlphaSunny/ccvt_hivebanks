a692413bb3aaff6f151f7468b2a94e9d4ca2e4d4
Edwin <1045416962@qq.com>
2018-11-06 11:39:30 +0800

    h5

--- h5/bot_web/js/send_amount.js
+++ h5/bot_web/js/send_amount.js
@@ -4,34 +4,31 @@ $(function () {
 
 
     var url = getRootPath();
+    // $('#sendAmountTable').DataTable({
+    //     "ajax": url + "/api/bot_web/iss_records_list.php?token=" + encodeURIComponent(token),
+    //     "order": [[3, "desc"]],
+    //     "deferRender":true,
+    //     "columns": [
+    //         {"data": "wechat"},
+    //         {"data": "amount"},
+    //         {"data": "num"},
+    //         {"data": "send_time"}
+    //     ]
+    // });
+
     $('#sendAmountTable').DataTable({
-        "ajax": url + "/api/bot_web/iss_records_list.php?token=" + encodeURIComponent(token),
-        "order": [[3, "desc"]],
-        "deferRender":true,
+        "ajax": {
+            "url": url + "/api/bot_web/iss_records_list.php?token=" + encodeURIComponent(token),
+            "type": "GET",
+            success: function (res) {
+                console.log(res);
+            }
+        },
         "columns": [
             {"data": "wechat"},
             {"data": "amount"},
             {"data": "num"},
             {"data": "send_time"}
-        ]
+        ],
     });
-
-    // $('#sendAmountTable').DataTable({
-    //     "ajax": {
-    //         "url": url + "/api/bot_web/iss_records_list.php?token=" + encodeURIComponent(token),
-    //         "dataSrc": function (json) {
-    //             console.log(json.all_amount);
-    //         }
-    //     }
-    // "ajax": url + "/api/bot_web/iss_records_list.php?token=" + encodeURIComponent(token),
-    // "columns": [
-    //     {"data": "wechat"},
-    //     {"data": "amount"},
-    //     {"data": "num"},
-    //     {"data": "send_time"}
-    // ],
-    // success:function (res) {
-    //     console.log(res);
-    // }
-    // });
 });
