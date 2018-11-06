54535b1d0d5c863721f2b4071152c99455cb05fe
songxingwei <you@example.com>
2018-11-06 17:42:05 +0800

    1

--- api/bot_web/db/bot.php
+++ api/bot_web/db/bot.php
@@ -229,7 +229,6 @@ function iss_records_list($da)
         $sql .=" and wechat LIKE '$nickname%'";
     }
     $sql .= " order by bot_create_time desc";
-    echo $sql;die;
     $db->query($sql);
     $data = array();
     $rows = $db -> fetchAll();
