4dd806ecc570313f07110378a5a1c62654893ef8
Edwin <1045416962@qq.com>
2018-11-07 11:38:30 +0800

    h5

--- api/bot_web/page/chat.php
+++ api/bot_web/page/chat.php
@@ -12,7 +12,7 @@
 <!--nav-->
 <!--<nav class="navbar navbar-default">-->
 <!--    <div class="container-fluid">-->
-<!--        <!-- Brand and toggle get grouped for better mobile display -->-->
+<!--        <!-- Brand and toggle get grouped for better mobile display -->
 <!--        <div class="navbar-header">-->
 <!--            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">-->
 <!--                <span class="sr-only">Toggle navigation</span>-->
@@ -23,7 +23,7 @@
 <!--            <a class="navbar-brand" href="#">CCVT</a>-->
 <!--        </div>-->
 <!---->
-<!--        <!-- Collect the nav links, forms, and other content for toggling -->-->
+<!--        <!-- Collect the nav links, forms, and other content for toggling -->
 <!--        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">-->
 <!--            <ul class="nav navbar-nav navbar-right">-->
 <!--                <li><a href="javascript:;" class="baLogin">BA通道</a></li>-->
