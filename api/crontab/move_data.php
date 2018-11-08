1a5936c7b65be0b51cc39d5ff0229932c66a17cb
songxingwei <you@example.com>
2018-11-08 11:11:08 +0800

    1

---
+++ api/crontab/move_data.php
@@ -0,0 +1,91 @@
+<?php
+require_once "../inc/common.php";
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+
+
+$db = new DB_COM();
+
+
+$sql = "select * from us_base where 1 ORDER BY ctime ASC ";
+$db->query($sql);
+$rows = $db->fetchAll();
+if ($rows){
+    foreach ($rows as $k=>$v){
+        $sql = "select * from com_transfer_request WHERE debit_id='{$v['us_id']}' AND transfer_type=1 AND flag=1";
+        $db->query($sql);
+        $row = $db->fetchRow();
+        if (!$row){
+            if ($v['ctime']<'2018-10-01 00:00:00'){
+                $send_money = "1000";
+            }elseif($v['ctime']>='2018-10-01 00:00:00' and $v['ctime']<'2018-10-07 23:59:59'){
+                $send_money = "500";
+            }else{
+                $send_money = "50";
+            }
+            into_transfer($v['us_id'],$send_money,$v['ctime'],1,'注册赠送');
+        }
+    }
+}else{
+    echo "注册赠送没有数据可以操作";
+    die();
+}
+echo "ok";
+
+
+
+function into_transfer($us_id,$send_money,$time,$flag,$detail){
+    $db = new DB_COM();
+
+    $data['hash_id'] = hash('md5', $us_id . $flag . get_ip() . time() . rand(1000, 9999) . $time);
+    $data['prvs_hash'] = get_pre_hash($us_id,$flag);
+    $data['credit_id'] = get_ba_id();
+    $data['debit_id'] = $us_id;
+    $data['tx_amount'] = $send_money*la_unit();
+    $data['tx_hash'] = hash('md5', $us_id . $flag . get_ip() . time() . date('Y-m-d H:i:s'));
+    $data['flag'] = $flag;
+    $data['transfer_type'] = 1;
+    $data['transfer_state'] = 1;
+    $data['tx_detail'] = json_encode($detail);
+    $data['ctime'] = strtotime($time);
+    $data['utime'] = $time;
+    $sql = $db->sqlInsert("us_send_ccvt_records", $data);
+    $id = $db->query($sql);
+    if (!$id){
+        echo $us_id."错误";
+        die();
+    }
+}
+
+
+
+
+//======================================
+// 函数: 获取上传交易hash
+//======================================
+function get_pre_hash($us_id,$flag){
+    $db = new DB_COM();
+    $sql = "SELECT hash_id FROM com_transfer_request WHERE debit_id = '{$us_id}' and flag = '{$flag}' ORDER BY  ctime DESC LIMIT 1";
+    $hash_id = $db->getField($sql, 'hash_id');
+    if($hash_id == null)
+        return 0;
+    return $hash_id;
+}
+
+function get_ba_id(){
+    $db = new DB_COM();
+    $sql = "select ba_id from ba_base limit 1";
+    $ba_id = $db->getField($sql,'ba_id');
+    if ($ba_id==null){
+        return 0;
+    }
+    return $ba_id;
+}
+//la汇率
+function la_unit(){
+    $db = new DB_COM();
+    $sql = "select unit from la_base limit 1";
+    $db->query($sql);
+    $rows = $db->fetchRow();
+    return $rows['unit'];
+}
