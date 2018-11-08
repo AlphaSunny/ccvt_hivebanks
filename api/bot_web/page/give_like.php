53c93ff178178c55e8926def6630ff3cd6c7fe50
songxingwei <you@example.com>
2018-11-08 18:24:26 +0800

    1

--- api/bot_web/page/give_like.php
+++ api/bot_web/page/give_like.php
@@ -32,11 +32,12 @@ $give_num = get_arg_str('GET', 'give_num');
 //验证token
 $us_id = check_token($token);
 
+
 //判断是否已达到上限
 $chcek = check_max_give($us_id,$give_num);
 if ($chcek==1){
     exit_error('101','已达到最大上限');
-}elseif ($chcek==2){
+}elseif ($chcek==2 || $give_num<=0){
     exit_error('102','金额错误');
 }
 
