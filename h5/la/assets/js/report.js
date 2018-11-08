c194d1bdcf9dfcf669c6ad06269d4d584d0abafd
Edwin <1045416962@qq.com>
2018-11-08 14:17:44 +0800

    h5

--- h5/la/assets/js/report.js
+++ h5/la/assets/js/report.js
@@ -131,7 +131,7 @@ $(function () {
                             "data": "wechat",
                             render: function (data) {
                                 if (data == null) {
-                                    data = "未绑定";
+                                    data = "--";
                                     return data;
                                 }else {
                                     data = data;
