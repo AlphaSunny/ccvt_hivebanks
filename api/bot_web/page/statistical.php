279432fd0e79acb068ccae3e80bda71e1842982e
songxingwei <you@example.com>
2018-11-08 18:52:34 +0800

    1

--- api/bot_web/page/statistical.php
+++ api/bot_web/page/statistical.php
@@ -123,6 +123,7 @@ $url = $data['api_url'] . "/api/bot_web/page/chat.php?datetime=" . base64_encode
                         <tbody>
                         <?php
                         $sql = "select us_id,wechat,amount,num,send_time,bot_create_time,(SELECT glory_level_integral from us_base WHERE us_id=us_id) as glory_level_integral from bot_Iss_records WHERE bot_create_time BETWEEN '{$day_start}' AND '{$day_end}' ORDER BY num DESC ";
+                        echo $sql;die;
                         $db->query($sql);
                         $list = $db->fetchAll();
                         foreach ($list as $k => $v) {
