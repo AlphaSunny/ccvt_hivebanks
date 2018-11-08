16a0c0eb9006a146dbe372045f1e36f205dcebbd
Edwin <1045416962@qq.com>
2018-11-08 16:29:04 +0800

    h5

--- api/bot_web/page/statistical.php
+++ api/bot_web/page/statistical.php
@@ -31,25 +31,25 @@
 <!--                <li><a href="javascript:;" class="usLogin"></a></li>-->
 <!--                <li class="accountNone"><a href="javascript:;" class="i18n toAccountBtn alreadyLogin" name="account">Account</a></li>-->
 <!---->
-                <?php
-                    require_once '../../inc/common.php';
-                    ini_set("display_errors", "off");
-                    $args = array('datetime');
-                    chk_empty_args('GET', $args);
-                    $db = new DB_COM();
-                    $datetime = base64_decode(get_arg_str('GET', 'datetime'));
-                    $group_name = base64_decode(get_arg_str('GET', 'group_name'));
-                    $day_start = strtotime(date($datetime . ' 00:00:00'));
-                    $day_end = strtotime(date($datetime . ' 23:59:59'));
+<?php
+require_once '../../inc/common.php';
+ini_set("display_errors", "off");
+$args = array('datetime');
+chk_empty_args('GET', $args);
+$db = new DB_COM();
+$datetime = base64_decode(get_arg_str('GET', 'datetime'));
+$group_name = base64_decode(get_arg_str('GET', 'group_name'));
+$day_start = strtotime(date($datetime . ' 00:00:00'));
+$day_end = strtotime(date($datetime . ' 23:59:59'));
 
-                    $json_string = file_get_contents('../../h5/assets/json/config_url.json');
-                    $data = json_decode($json_string, true);
+$json_string = file_get_contents('../../h5/assets/json/config_url.json');
+$data = json_decode($json_string, true);
 
-                    $group_name2 =  urlencode(base64_encode($group_name));
+$group_name2 = urlencode(base64_encode($group_name));
 
-                    $url = $data['api_url']."/api/bot_web/page/chat.php?datetime=".base64_encode($datetime)."&group_name=".$group_name2."&status=".base64_encode(2);
+$url = $data['api_url'] . "/api/bot_web/page/chat.php?datetime=" . base64_encode($datetime) . "&group_name=" . $group_name2 . "&status=" . base64_encode(2);
 
-                ?>
+?>
 <!--                <li><a href="--><?php //echo $url;?><!--">查看聊天记录</a></li>-->
 <!--            </ul>-->
 <!--        </div>-->
@@ -67,89 +67,89 @@
 <!--</div>-->
 <!--<div class="swiper-container">-->
 <!--    <div class="swiper-wrapper">-->
-        <section id="statistical" class="swiper-slide">
-            <div class="container">
-                <div class="row title_box">
-                    <div class="col-md-12 col-sm-12 flex center space-between">
-                        <h3>CCVT奖励统计</h3>
-                        <a href="javascript:;" class="login">登录</a>
-                        <span class="amount_box">
+<section id="statistical" class="swiper-slide">
+    <div class="container">
+        <div class="row title_box">
+            <div class="col-md-12 col-sm-12 flex center space-between">
+                <h3>CCVT奖励统计</h3>
+                <a href="javascript:;" class="login">登录</a>
+                <span class="amount_box">
                             余额:
                             <span class="amount"></span>
                         </span>
+            </div>
+        </div>
+        <div class="row margin-top-5">
+            <div class="col-md-12">
+                <div class="margin-bottom-2">
+                    <?php
+                    $sql = "select unit from la_base limit 1";
+                    $db->query($sql);
+                    $unit = $db->getField($sql, 'unit');
+
+                    $sql = "select sum(amount)/'{$unit}' as all_send_ccvt from bot_Iss_records WHERE bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
+                    $db->query($sql);
+                    $all_send_ccvt = $db->getField($sql, 'all_send_ccvt'); //总赠送ccvt数量
+
+                    $sql = "select count(bot_message_id) as all_message from bot_message WHERE group_name='{$group_name}' AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
+                    $db->query($sql);
+                    $all_message = $db->getField($sql, 'all_message'); //总聊天数量
+                    ?>
+                    <div class="sm_title_text_color">
+                        <div class="flex center space-between">
+                            <p>所属群:《<?php echo $group_name; ?>》</p>
+                            <a href="<?php echo $url; ?>">查看聊天记录</a>
+                        </div>
+                        <p class="font-size-14">时间:<?php echo $datetime; ?></p>
+                    </div>
+                    <div class="flex space-between font-size-14 sm_title_text_color">
+                        <p>今日奖励总数量:<?php echo $all_send_ccvt; ?>(CCVT)</p>
+                        <p>今日发言总数量:<?php echo $all_message; ?>(条)</p>
                     </div>
                 </div>
-                <div class="row margin-top-5">
-                    <div class="col-md-12">
-                        <div class="margin-bottom-2">
-                            <?php
-                            $sql = "select unit from la_base limit 1";
-                            $db->query($sql);
-                            $unit = $db->getField($sql,'unit');
 
-                            $sql = "select sum(amount)/'{$unit}' as all_send_ccvt from bot_Iss_records WHERE bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
-                            $db->query($sql);
-                            $all_send_ccvt = $db->getField($sql, 'all_send_ccvt'); //总赠送ccvt数量
-
-                            $sql = "select count(bot_message_id) as all_message from bot_message WHERE group_name='{$group_name}' AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
-                            $db->query($sql);
-                            $all_message = $db->getField($sql, 'all_message'); //总聊天数量
+                <div class="table-responsive">
+                    <table class="table" id="statisticalTable">
+                        <thead>
+                        <tr>
+                            <!--                            <th>排名</th>-->
+                            <th class="text-left">名称</th>
+                            <!--                            <th class="text-center">拥有数量(CCVT)</th>-->
+                            <th class="text-center">获得(CCVT)</th>
+                            <th class="text-center">发言数</th>
+                            <th class="text-center">点赞</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $sql = "select * from bot_Iss_records WHERE bot_create_time BETWEEN '{$day_start}' AND '{$day_end}' ORDER BY num DESC ";
+                        $db->query($sql);
+                        $list = $db->fetchAll();
+                        foreach ($list as $k => $v) {
                             ?>
-                            <div class="sm_title_text_color">
-                                <div class="flex center space-between">
-                                    <p>所属群:《<?php echo $group_name;?>》</p>
-                                    <a href="<?php echo $url;?>">查看聊天记录</a>
-                                </div>
-                                <p class="font-size-14">时间:<?php echo $datetime; ?></p>
-                            </div>
-                            <div class="flex space-between font-size-14 sm_title_text_color">
-                                <p>今日奖励总数量:<?php echo $all_send_ccvt; ?>(CCVT)</p>
-                                <p>今日发言总数量:<?php echo $all_message; ?>(条)</p>
-                            </div>
-                        </div>
+                            <tr>
 
-                        <div class="table-responsive">
-                            <table class="table" id="statisticalTable">
-                                <thead>
-                                <tr>
-                                    <!--                            <th>排名</th>-->
-                                    <th class="text-left">名称</th>
-                                    <!--                            <th class="text-center">拥有数量(CCVT)</th>-->
-                                    <th class="text-center">获得(CCVT)</th>
-                                    <th class="text-center">发言数</th>
-                                    <th class="text-center">点赞</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                $sql = "select * from bot_Iss_records WHERE bot_create_time BETWEEN '{$day_start}' AND '{$day_end}' ORDER BY num DESC ";
-                                $db->query($sql);
-                                $list = $db->fetchAll();
-                                foreach ($list as $k => $v) {
+                                <td><?php echo $v['wechat']; ?></td>
+
+                                <td><?php echo $v['amount'] / $unit; ?></td>
+                                <td>
+                                    <?php
+                                    $sql = "select count(bot_message_id) as all_message from bot_message WHERE group_name='{$group_name}' AND wechat='{$v['wechat']}' AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
+                                    $db->query($sql);
+                                    $count = $db->getField($sql, 'all_message');
+                                    echo $count;
                                     ?>
-                                    <tr>
-
-                                        <td><?php echo $v['wechat']; ?></td>
-
-                                        <td><?php echo $v['amount']/$unit; ?></td>
-                                        <td>
-                                            <?php
-                                            $sql = "select count(bot_message_id) as all_message from bot_message WHERE group_name='{$group_name}' AND wechat='{$v['wechat']}' AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}'";
-                                            $db->query($sql);
-                                            $count = $db->getField($sql, 'all_message');
-                                            echo $count;
-                                            ?>
-                                        </td>
-                                        <td><img src="img/zan.svg" class="zan_img" alt=""></td>
-                                    </tr>
-                                <?php } ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                                </td>
+                                <td><img src="img/zan.svg" class="zan_img" alt=""></td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
+</section>
 
 <!--        <section class="swiper-slide">-->
 <!--            <div id="chat">-->
@@ -188,7 +188,8 @@
 <!---->
 <!--                            <div class="infoBox flex">-->
 <!--                                <div class="photo">-->
-<!--                                    <img src="http://52.53.151.223:8000/--><?php //echo $v['head_img'] ?><!--" alt="">-->
+<!--                                    <img src="http://52.53.151.223:8000/-->
+<?php //echo $v['head_img'] ?><!--" alt="">-->
 <!--                                </div>-->
 <!--                                <div class="infoNameBox">-->
 <!--                                    <p class="name">--><?php //echo $v['bot_nickname'] ?><!--</p>-->
@@ -203,7 +204,8 @@
 //                                            } elseif ($v['type'] == "Video" || $v['type'] == "Recording") {
 //                                                ?>
 <!--                                                <audio id="audioplayer" preload="auto" controls style="width:150%;">-->
-<!--                                                    <source src="--><?php //echo $v['bot_content'] ?><!--" type="audio/mp3">-->
+<!--                                                    <source src="-->
+<?php //echo $v['bot_content'] ?><!--" type="audio/mp3">-->
 <!--                                                </audio>-->
 <!--                                                --><?php
 //                                            } else {
@@ -230,9 +232,23 @@
 <!--            loop: true-->
 <!--        });-->
 <!--    </script>-->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/common.js"></script>
-    <script src="js/statistical.js"></script>
+
+<!--modal-->
+<div class="c-float-popWrap confirmMode hide" style="opacity: 1; top: 264px; left: 28px;">
+    <div class="weui_mask_transparent"></div>
+    <div class="c-float-modePop">
+        <div class="warnMsg">点赞👍数量</div>
+        <div class="content"><label>数量：<input class="confirm_input" placeholder="请输入"></label></div>
+        <div class="doBtn">
+            <button class="cancel">取 消</button>
+            <button class="ok">确 定</button>
+        </div>
+    </div>
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/common.js"></script>
+<script src="js/statistical.js"></script>
 </body>
 </html>
