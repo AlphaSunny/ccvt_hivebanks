b0bf7470a524481a50ac1a7ea604597c68611f3b
Edwin <1045416962@qq.com>
2018-11-08 10:50:02 +0800

    h5

--- api/bot_web/page/js/register.js
+++ api/bot_web/page/js/register.js
@@ -11,19 +11,15 @@ $(function () {
     });
 
     //切换手机/邮箱登录
-    $(".toggle_login_btn").click(function () {
+    $(".toggle_reg_btn").click(function () {
         $(this).addClass("active").siblings(".toggle_login_btn").removeClass("active");
-        // if ($(this).hasClass("email_login_btn")) {
-        //     $(".intl-tel-input").fadeOut("fast");
-        //     $("#phoneLoginBtn").fadeOut("fast");
-        //     $("#email").fadeIn("fast");
-        //     $("#emailLoginBtn").fadeIn("fast");
-        // } else {
-        //     $("#email").fadeOut("fast");
-        //     $("#emailLoginBtn").fadeOut("fast");
-        //     $(".intl-tel-input").fadeIn("fast");
-        //     $("#phoneLoginBtn").fadeIn("fast");
-        // }
+        if ($(this).hasClass("email_reg_btn")) {
+            $(".phone_reg_box").fadeOut("fast");
+            $("#email").fadeIn("fast");
+        } else {
+            $("#email").fadeOut("fast");
+            $(".phone_reg_box").fadeIn("fast");
+        }
     });
 
     //切换验证码
