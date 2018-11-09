8b75032eb58cfd0d83b9ebf8e11bc5a4682880c8
songxingwei <you@example.com>
2018-11-09 11:27:05 +0800

    1

--- api/bot_web/page/chat.php
+++ api/bot_web/page/chat.php
@@ -61,7 +61,6 @@ $url = $data['api_url'] . "/api/bot_web/page/statistical.php?datetime=" . base64
         $tblPrefix = "@风赢小助手";
         $tblPrefix2 = "@小助手";
         $sql = "select bot_nickname,bot_content,bot_send_time,head_img,type,bot_create_time from bot_message WHERE group_name='{$group_name}' AND (bot_content NOT LIKE '$tblPrefix%' OR bot_content NOT LIKE '$tblPrefix2%') AND (bot_nickname!='风赢小助手' OR bot_nickname!='小助手') AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}' ORDER BY bot_create_time  ASC ";
-        echo $sql;die;
         $db->query($sql);
         $rows = $db->fetchAll();
         $ti = -1;
