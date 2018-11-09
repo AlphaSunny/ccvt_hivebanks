e7616f0adbfbf23dc1d65d484a35da2ddd122ea3
范助国 <ahino@fanzhuguodeMacBook-Pro.local>
2018-11-09 15:20:39 +0800

    reportFormDev

--- api/bot_web/page/statistical.php
+++ api/bot_web/page/statistical.php
@@ -154,10 +154,10 @@ $url = $data['api_url'] . "/api/bot_web/page/chat.php?datetime=" . base64_encode
                         <?php
                             $token = $_COOKIE['statistics_user_token'];
                             if ($token){
-                                var_dump($token.'\r');
-//                                $token = urlencode(str_replace(' ','',$token));
-                                $token = urlencode(($token));
-                                $token = str_replace('+','',$token);
+//                                var_dump($token.'\r');
+////                                $token = urlencode(str_replace(' ','',$token));
+//                                $token = urlencode(($token));
+//                                $token = str_replace('+','',$token);
                                 var_dump($token);
                                 $us_id = check_token($token);
                                 $sql = "select sum(tx_amount)/'{$unit}' as all_am from us_glory_integral_change_log WHERE credit_id='{$us_id}' AND ctime BETWEEN '{$s_time}' AND '{$e_time}'";
