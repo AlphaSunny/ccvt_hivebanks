a76a87af2dcf04619ae4a64b55032313e9eecc10
Edwin <1045416962@qq.com>
2018-11-09 10:06:39 +0800

    h5

--- api/bot_web/page/statistical.php
+++ api/bot_web/page/statistical.php
@@ -11,26 +11,6 @@
 </head>
 
 <body>
-<!--nav-->
-<!--<nav class="navbar navbar-default">-->
-<!--    <div class="container-fluid">-->
-<!--        <!-- Brand and toggle get grouped for better mobile display -->
-<!--        <div class="navbar-header">-->
-<!--            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">-->
-<!--                <span class="sr-only">Toggle navigation</span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--            </button>-->
-<!--            <a class="navbar-brand" href="#">CCVT</a>-->
-<!--        </div>-->
-<!---->
-<!--        <!-- Collect the nav links, forms, and other content for toggling -->
-<!--        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">-->
-<!--            <ul class="nav navbar-nav navbar-right">-->
-<!--                <li><a href="javascript:;" class="usLogin"></a></li>-->
-<!--                <li class="accountNone"><a href="javascript:;" class="i18n toAccountBtn alreadyLogin" name="account">Account</a></li>-->
-<!---->
 <?php
 require_once '../../inc/common.php';
 ini_set("display_errors", "off");
@@ -50,23 +30,6 @@ $group_name2 = urlencode(base64_encode($group_name));
 $url = $data['api_url'] . "/api/bot_web/page/chat.php?datetime=" . base64_encode($datetime) . "&group_name=" . $group_name2 . "&status=" . base64_encode(2);
 
 ?>
-<!--                <li><a href="--><?php //echo $url;?><!--">查看聊天记录</a></li>-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</nav>-->
-<!--nav-->
-<!--<div class="box">-->
-<!--    <div class="img_box">-->
-<!--        <img src="img/more.svg" alt="">-->
-<!--    </div>-->
-<!--    <ul class="more_box">-->
-<!--        <li><a href="javascript:;" id="register">注册</a></li>-->
-<!--        <li><a href="javascript:;" id="login">登录</a></li>-->
-<!--    </ul>-->
-<!--</div>-->
-<!--<div class="swiper-container">-->
-<!--    <div class="swiper-wrapper">-->
 <section id="statistical" class="swiper-slide">
     <div class="container">
         <div class="row title_box">
@@ -112,9 +75,7 @@ $url = $data['api_url'] . "/api/bot_web/page/chat.php?datetime=" . base64_encode
                     <table class="table" id="statisticalTable">
                         <thead>
                         <tr>
-                            <!--                            <th>排名</th>-->
-                            <th class="text-left">名称</th>
-                            <!--                            <th class="text-center">拥有数量(CCVT)</th>-->
+                            <th class="text-left">名称(荣耀积分)</th>
                             <th class="text-center">获得(CCVT)</th>
                             <th class="text-center">发言数</th>
                             <th class="text-center">点赞</th>
@@ -165,87 +126,6 @@ $url = $data['api_url'] . "/api/bot_web/page/chat.php?datetime=" . base64_encode
     </div>
 </section>
 
-<!--        <section class="swiper-slide">-->
-<!--            <div id="chat">-->
-<!--                <p class="text-center title">--><?php //echo base64_decode($_REQUEST['group_name']); ?><!--</p>-->
-<!--                <ul class="chatList">-->
-<!--                    --><?php
-//                    require_once '../../inc/common.php';
-//                    ini_set("display_errors", "off");
-//                    $args = array('datetime');
-//                    chk_empty_args('GET', $args);
-//                    $db = new DB_COM();
-//                    $datetime = base64_decode(get_arg_str('GET', 'datetime'));
-//                    $group_name = base64_decode(get_arg_str('GET', 'group_name'));
-//                    $day_start = strtotime(date($datetime . ' 00:00:00'));
-//                    $day_end = strtotime(date($datetime . ' 23:59:59'));
-//
-//                    $tblPrefix = "@风赢小助手";
-//                    $tblPrefix2 = "@小助手";
-//                    $sql = "select bot_nickname,bot_content,bot_send_time,head_img,type from bot_message WHERE group_name='{$group_name}' AND (bot_content NOT LIKE '$tblPrefix%' OR bot_content NOT LIKE '$tblPrefix2%') AND (bot_nickname!='风赢小助手' OR bot_nickname!='小助手') AND bot_create_time BETWEEN '{$day_start}' AND '{$day_end}' ORDER BY bot_create_time  ASC ";
-//                    $db->query($sql);
-//                    $rows = $db->fetchAll();
-//                    $ti = -1;
-//                    foreach ($rows as $k => $v) {
-//                        ?>
-<!--                        <li class="chatItem">-->
-<!--                            --><?php
-//                            if ($ti != -1 && ($v['bot_create_time'] - $ti) > 60) {
-//                                ?>
-<!--                                <div class="text-center timeBox">-->
-<!--                                    <span class="time">--><?php //echo $v['bot_send_time']; ?><!--</span>-->
-<!--                                </div>-->
-<!--                                --><?php
-//                            }
-//                            $ti = $v['bot_create_time'];
-//                            ?>
-<!---->
-<!--                            <div class="infoBox flex">-->
-<!--                                <div class="photo">-->
-<!--                                    <img src="http://52.53.151.223:8000/-->
-<?php //echo $v['head_img'] ?><!--" alt="">-->
-<!--                                </div>-->
-<!--                                <div class="infoNameBox">-->
-<!--                                    <p class="name">--><?php //echo $v['bot_nickname'] ?><!--</p>-->
-<!--                                    <div class="chatInfo">-->
-<!--                                        <p class="chatContent">-->
-<!--                                            --><?php
-//                                            if ($v['type'] == "Picture") {
-//                                                ?>
-<!--                                                <img src="--><?php //echo $v['bot_content'] ?><!--"-->
-<!--                                                     style="width: 100%;height: 150px">-->
-<!--                                                --><?php
-//                                            } elseif ($v['type'] == "Video" || $v['type'] == "Recording") {
-//                                                ?>
-<!--                                                <audio id="audioplayer" preload="auto" controls style="width:150%;">-->
-<!--                                                    <source src="-->
-<?php //echo $v['bot_content'] ?><!--" type="audio/mp3">-->
-<!--                                                </audio>-->
-<!--                                                --><?php
-//                                            } else {
-//                                                ?>
-<!--                                                --><?php //echo $v['bot_content'] ?>
-<!--                                                --><?php
-//                                            }
-//                                            ?>
-<!--                                        </p>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!---->
-<!--                            </div>-->
-<!--                        </li>-->
-<!--                        --><?php
-//                    } ?>
-<!--                </ul>-->
-<!--            </div>-->
-<!--        </section>-->
-
-<!--    </div>-->
-<!--    <script>-->
-<!--        var mySwiper = new Swiper(".swiper-container", {-->
-<!--            loop: true-->
-<!--        });-->
-<!--    </script>-->
 
 <!--modal-->
 <div class="c-float-popWrap confirmMode hiden" style="opacity: 1; top: 264px; left: 28px;">
