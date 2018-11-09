aa9e7dc9db81d743af20eba89a02dd48b138ba76
Edwin <1045416962@qq.com>
2018-11-09 14:55:35 +0800

    h5

--- h5/user/js/account.js
+++ h5/user/js/account.js
@@ -136,8 +136,8 @@ $(function () {
 
         $('#qrcode').qrcode({
             text: url,
-            width: 200,
-            height: 200
+            width: 180,
+            height: 180
         });
 
         //get canvas qr
@@ -151,7 +151,7 @@ $(function () {
         qrImg.src = "img/inviteImg.png";
         qrImg.onload = function () {
             content.drawImage(this, 0, 0, 568, 886);
-            content.drawImage(qrctx, 20, 700, 160, 160);//二维码位置 左/上/右/下
+            content.drawImage(qrctx, 30, 700, 160, 160);//二维码位置 左/上/右/下
             var base64 = canvas.toDataURL("images/png");
             $("#base64Img").attr("src", base64);
             $(".inviteImgBox, #qrcode").remove();
