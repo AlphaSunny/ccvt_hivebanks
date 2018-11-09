5d2fdfe6fca7494b83d6c5d2e12518265aaa7dce
songxingwei <you@example.com>
2018-11-09 14:51:33 +0800

    1

--- api/bot_web/page/give_like.php
+++ api/bot_web/page/give_like.php
@@ -22,7 +22,8 @@ $args = array('token','give_us_id','give_num');
 chk_empty_args('GET', $args);
 
 // 用户token
-$token = get_arg_str('GET', 'token',128);
+//$token = get_arg_str('GET', 'token',128);
+$token = $_GET['token'];
 echo $token;
 // 用户id
 $give_us_id = get_arg_str('GET', 'give_us_id');
