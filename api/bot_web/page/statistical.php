e0765ef43729f0ca892a95444a8079f61770064f
songxingwei <you@example.com>
2018-11-09 14:35:50 +0800

    1

--- api/bot_web/page/statistical.php
+++ api/bot_web/page/statistical.php
@@ -153,6 +153,7 @@ $url = $data['api_url'] . "/api/bot_web/page/chat.php?datetime=" . base64_encode
                     <span class="already_count" style="color: #333333">
                         <?php
                             $token = $_COOKIE['statistics_user_token'];
+                        echo $token;die;
                             if ($token){
                                 $us_id = check_token($token);
                                 $sql = "select sum(tx_amount)/'{$unit}' as all_am from us_glory_integral_change_log WHERE credit_id='{$us_id}' AND ctime BETWEEN '{$s_time}' AND '{$e_time}'";
