c08c0abd7de01e55d1b7f62316aba268d7b19936
songxingwei <you@example.com>
2018-11-13 14:12:14 +0800

    1

--- api/crontab/send_money_ccvt.php
+++ api/crontab/send_money_ccvt.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL | E_STRICT);
 //$day_start = strtotime(date('Y-m-d 08:00:00')); //早上八点
 //$day_end = strtotime(date('Y-m-d 22:00:00'));    //晚上十点
 
-$day_start = strtotime(date('2018-11-11 08:00:00')); //早上八点
+$day_start = strtotime(date('2018-11-11 00:00:00')); //早上八点
 $day_end = strtotime(date('2018-11-11 22:00:00'));    //晚上十点
 
 $db = new DB_COM();
