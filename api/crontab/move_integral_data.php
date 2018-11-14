0a86daf84ec528ef38cd72df1f7fe341f8cacc30
songxingwei <you@example.com>
2018-11-14 11:19:34 +0800

    1

--- api/crontab/move_integral_data.php
+++ api/crontab/move_integral_data.php
@@ -48,6 +48,8 @@ function into_transfer($us_id,$give_us_id,$send_money,$time,$flag,$detail,$type)
         die;
     }
 
+    echo $flag;
+
     //增加荣耀积分(减少荣耀积分)
     $sql = "select * from us_asset WHERE asset_id='GLOP' AND us_id='{$give_us_id}'";
     $db->query($sql);
