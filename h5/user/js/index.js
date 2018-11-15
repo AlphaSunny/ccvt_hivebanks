4e4aaf326d9dccae3f0e8509b362b1a0d36721cf
Edwin <1045416962@qq.com>
2018-11-15 17:07:20 +0800

    h5

--- h5/user/js/index.js
+++ h5/user/js/index.js
@@ -85,7 +85,7 @@ $(function () {
         var lineHeight = $self.find("li:first").height();
         $self.animate({
             "marginTop": -lineHeight + "px"
-        }, 2000, function () {
+        }, 3000, function () {
             $self.css({
                 marginTop: 0
             }).find("li:first").appendTo($self);
