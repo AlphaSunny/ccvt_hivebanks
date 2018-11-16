7fd3120f1e08ee4fd6a15453343a8cc10b1436dc
songxingwei <you@example.com>
2018-11-16 15:43:25 +0800

    1

--- api/crontab/scale_upgrade.php
+++ api/crontab/scale_upgrade.php
@@ -13,5 +13,20 @@ $sql = "select * from us_asset WHERE 1";
 $db->query($sql);
 $rows = $db->fetchAll();
 if ($rows){
-    
+    $unit = la_unit();
+    //积分
+    foreach ($rows as $k=>$v){
+        set_time_limit(0);
+        $scale = $v['base_amount']/$unit;
+        echo $scale;
+    }
+}
+
+//la汇率
+function la_unit(){
+    $db = new DB_COM();
+    $sql = "select unit from la_base limit 1";
+    $db->query($sql);
+    $rows = $db->fetchRow();
+    return $rows['unit'];
 }
