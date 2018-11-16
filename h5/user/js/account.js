5e09dad9bec823471d68ac49e0cb91035f2e94b1
Edwin <1045416962@qq.com>
2018-11-16 13:21:03 +0800

    h5

--- h5/user/js/account.js
+++ h5/user/js/account.js
@@ -150,9 +150,9 @@ $(function () {
         qrImg.crossOrigin = "*";
         qrImg.src = "img/inviteImg.jpg";
         qrImg.onload = function () {
-            content.drawImage(this, 0, 0, 568, 886);
+            content.drawImage(this, 0, 0, 568, 886);//设置狂傲
             content.drawImage(qrctx, 52, 685, 160, 160);//二维码位置 左/上/右/下
-            var base64 = canvas.toDataURL("images/png");
+            var base64 = canvas.toDataURL("images/png");//转成URL
             $("#base64Img").attr("src", base64);
             $(".inviteImgBox, #qrcode").remove();
         };
