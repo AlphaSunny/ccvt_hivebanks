90332358b04fb43a5630fcb098d5971c572f22b5
Edwin <1045416962@qq.com>
2018-11-18 17:56:20 +0800

    h5

--- h5/la/assets/js/userList.js
+++ h5/la/assets/js/userList.js
@@ -10,19 +10,45 @@ $(function () {
             if(data == false){
                 GetDataEmpty('userList', '4');
             }
-            $.each(data, function (i, val) {
-                tr+='<tr>' +
-                    '<td><a href="javascript:;" class="us_id">'+ data[i].us_id +'</a></td>' +
-                    // '<td>'+ data[i].base_amount +'&nbsp;<span class="base_type">BTC</span></td>' +
-                    // '<td>'+ data[i].lock_amount +'&nbsp;<span class="base_type">BTC</span></td>' +
-                    '<td>'+ data[i].us_level +'</td>' +
-                    '<td>'+ data[i].security_level +'</td>' +
-                    // '<td>'+ data[i].utime +'</td>' +
-                    '<td>'+ data[i].ctime +'</td>' +
-                    // '<td><a href="#!">编辑</a></td>' +
-                    '</tr>'
+
+            $("#userList").DataTable({
+                destroy: true,
+                deferRender: true,
+                lengthMenu: [ 10, 20, 50, 70, 100 ],
+                searching:false,//是否显示搜索框
+                info:false,//是否显示表左下角文字
+                language: {
+                    paginate: {
+                        url: "dataTables.german.lang",
+                        first:"<<",
+                        previous: "<",
+                        next: ">",
+                        last:">>",
+                        loadingRecords:"Please wait - loading..",
+                    }
+                },
+                data:data,
+                columns:[
+                    {"data":"us_id"},
+                    {"data":"us_level"},
+                    {"data":"security_level"},
+                    {"data":"ctime"},
+                ],
             });
-            $('#userList').html(tr);
+
+            // $.each(data, function (i, val) {
+            //     tr+='<tr>' +
+            //         '<td><a href="javascript:;" class="us_id">'+ data[i].us_id +'</a></td>' +
+            //         // '<td>'+ data[i].base_amount +'&nbsp;<span class="base_type">BTC</span></td>' +
+            //         // '<td>'+ data[i].lock_amount +'&nbsp;<span class="base_type">BTC</span></td>' +
+            //         '<td>'+ data[i].us_level +'</td>' +
+            //         '<td>'+ data[i].security_level +'</td>' +
+            //         // '<td>'+ data[i].utime +'</td>' +
+            //         '<td>'+ data[i].ctime +'</td>' +
+            //         // '<td><a href="#!">编辑</a></td>' +
+            //         '</tr>'
+            // });
+            // $('#userList').html(tr);
         }
     }, function (response) {
         GetDataFail('userList', '4');
