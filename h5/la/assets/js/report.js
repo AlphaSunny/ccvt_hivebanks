cd6a0ac36cc89a83315248cf6d3a59710aeed4c5
Edwin <1045416962@qq.com>
2018-11-18 17:36:13 +0800

    h5

--- h5/la/assets/js/report.js
+++ h5/la/assets/js/report.js
@@ -126,7 +126,7 @@ $(function () {
                     order: [[4, "desc"]],
                     destroy: true,
                     deferRender: true,
-                    lengthMenu: [ 10, 25, 50, 75, 100 ],
+                    lengthMenu: [ 10, 20, 50, 70, 100 ],
                     language: {
                         paginate: {
                             url: "dataTables.german.lang",
