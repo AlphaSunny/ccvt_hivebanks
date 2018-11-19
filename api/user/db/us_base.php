5a1a5b7524a695672f301b9e1963edc5da1b9f88
songxingwei <you@example.com>
2018-11-19 20:33:04 +0800

    1

--- api/user/db/us_base.php
+++ api/user/db/us_base.php
@@ -64,7 +64,7 @@ function ins_base_user_reg_base_info($data_base)
     send_to_us_ccvt($data_base['us_id'],'reg_send','50','注册赠送','1');
 
     //邀请人获取50ccvt
-    if ($data_base['invite_code']){
+    if ($data_base['invite_code']!=''){
         send_to_us_ccvt(get_invite_code_us($data_base['invite_code']),'invite_send','50','邀请赠送','2');
     }
 
