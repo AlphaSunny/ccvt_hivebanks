96a8163ffd0c64a9b44a88e092b238986fe6eaa2
Edwin <1045416962@qq.com>
2018-11-19 16:54:10 +0800

    h5

--- h5/la/assets/js/report.js
+++ h5/la/assets/js/report.js
@@ -126,17 +126,17 @@ $(function () {
                     order: [[4, "desc"]],
                     destroy: true,
                     deferRender: true,
-                    lengthMenu: [ 10, 20, 50, 70, 100 ],
-                    searching:false,//是否显示搜索框
-                    info:false,//是否显示表左下角文字
+                    lengthMenu: [10, 20, 50, 70, 100],
+                    searching: false,//是否显示搜索框
+                    info: false,//是否显示表左下角文字
                     language: {
                         paginate: {
                             url: "dataTables.german.lang",
-                            first:"<<",
+                            first: "<<",
                             previous: "<",
                             next: ">",
-                            last:">>",
-                            loadingRecords:"Please wait - loading..",
+                            last: ">>",
+                            loadingRecords: "Please wait - loading..",
                         }
                     },
                     data: response.rows.gift_detail,
@@ -206,32 +206,50 @@ $(function () {
 
     //获取每天用户增长趋势图
     var day = "7", user_data = "";
-    GetDayUserUp(token,day, function (response) {
-        if(response.errcode == "0"){
-            user_data = response.rows;
-            //用户增长趋势图
-            Morris.Line({
-                element: 'user-line-chart',
-                data: response.rows,
-                xkey: "date",
-                ykeys: ["num"],
-                labels: ['注册数'],
-                xLabels:"day",
-                fillOpacity: 0.6,
-                hideHover: 'auto',
-                smooth: true,// 是否平滑显示
-                parseTime:false,
-                behaveLikeLine: true,
-                resize: true,
-                pointFillColors: ['#ffffff'],
-                pointStrokeColors: ['black'],
-                lineColors: ['green']
-            });
-        }
-    }, function (response) {
 
+    function GetDayUserFun(day) {
+        GetDayUserUp(token, day, function (response) {
+            if (response.errcode == "0") {
+                user_data = response.rows;
+                //用户增长趋势图
+                Morris.Line({
+                    element: 'user-line-chart',
+                    data: response.rows,
+                    xkey: "date",
+                    ykeys: ["num"],
+                    labels: ['注册数'],
+                    xLabels: "day",
+                    fillOpacity: 0.6,
+                    hideHover: 'auto',
+                    smooth: true,// 是否平滑显示
+                    parseTime: false,
+                    behaveLikeLine: true,
+                    resize: true,
+                    pointFillColors: ['#ffffff'],
+                    pointStrokeColors: ['black'],
+                    lineColors: ['green']
+                });
+            }
+        }, function (response) {
+            LayerFun(response.errcode);
+        });
+    }
+
+    GetDayUserFun(day);
+
+    $(".day_7").click(function () {
+        day = 7;
+        GetDayUserFun(day);
     });
 
+    $(".day_15").click(function () {
+        day = 15;
+        GetDayUserFun(day);
+    });
+    $(".day_30").click(function () {
+        day = 30;
+        GetDayUserFun(day);
+    });
 
     /* MORRIS DONUT CHART
 			----------------------------------------*/
