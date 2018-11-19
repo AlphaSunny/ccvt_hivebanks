a8446f0cde315f7fa57480a3e46e34fd38910c14
Edwin <1045416962@qq.com>
2018-11-19 11:06:27 +0800

    h5

--- api/bot_web/page/chat.php
+++ api/bot_web/page/chat.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/chat.css">
     <title>聊天记录</title>
 </head>
