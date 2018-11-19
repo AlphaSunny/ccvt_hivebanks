168f73b7fc70e4cb4e502744a2fb84eb001ae04d
Edwin <1045416962@qq.com>
2018-11-19 14:13:45 +0800

    h5

--- h5/la/assets/js/report.js
+++ h5/la/assets/js/report.js
@@ -245,11 +245,11 @@ $(function () {
     Morris.Line({
         element: 'user-line-chart',
         data: [
-            { year: '2008', value: 20 },
-            { year: '2009', value: 10 },
-            { year: '2010', value: 5 },
-            { year: '2011', value: 5 },
-            { year: '2012', value: 20 }
+            { year: '2008/11/10', value: 20 },
+            { year: '2008/11/11', value: 10 },
+            { year: '2008/11/12', value: 5 },
+            { year: '2008/11/13', value: 5 },
+            { year: '2008/11/14', value: 20 }
         ],
         xkey: "year",
         ykeys: ["value"],
