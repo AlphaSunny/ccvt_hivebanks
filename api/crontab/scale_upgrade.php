1b9e65122c520e9d52475b6f5ff64876490f033c
songxingwei <you@example.com>
2018-11-20 11:12:42 +0800

    1

--- api/crontab/scale_upgrade.php
+++ api/crontab/scale_upgrade.php
@@ -26,7 +26,7 @@ if ($rows){
 function scale_upgrade($us_id,$scale){
 
     //判断是否可以升级
-    $us_scale = get_us_base($us_id)['scale']+1;
+    $us_scale = get_us_base($us_id)['scale'];
     //获取当前积分的等级
     $sca = get_scale_info($scale);
     print_r($sca)."<br />";
