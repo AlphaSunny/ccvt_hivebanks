5300e1c54365d4753487e85ce536be89178658ee
Edwin <1045416962@qq.com>
2018-11-20 15:32:37 +0800

    h5

--- h5/user/js/main.js
+++ h5/user/js/main.js
@@ -19,7 +19,7 @@ $(function () {
     link.appendTo($('head')[1]);
 
     //favicon
-    var favicon_link = "<link rel='shortcut ico' href='../favicon.ico'/>";
+    var favicon_link = "<link rel='shortcut ico' href='../../favicon.ico'/>";
     link.appendTo($('head')[0]);
 
     //cnt.js
