87b58d39109125b0963cd5deb7fad5382bfeb911
范助国 <ahino@fanzhuguodeMacBook-Pro.local>
2018-11-20 17:01:29 +0800

    total

--- api/la/admin/report_form/db/us_base.php
+++ api/la/admin/report_form/db/us_base.php
@@ -131,10 +131,10 @@ function total_daily($day){
 
     $data = array();
     $db = new DB_COM();
-    
+
     for($day ;$day>0 ;$day--){
 
-        $sql = "select sum(base_amount)/(select unit from la_base) as sum ,
+        $sql = "select sum(base_amount)/(select unit from la_base) as sum 
               from us_base where DATE_FORMAT(FROM_UNIXTIME(UNIX_TIMESTAMP(ctime)), '%Y-%m-%d') between 
               date_sub(curdate(),interval 99999 day) and date_sub(curdate(),interval {$day} day) ;";
         $db->query($sql);
