9230662db5d3243c7740bcd568748cd3160ff1a5
songxingwei <you@example.com>
2018-11-23 18:46:25 +0800

    1

--- api/crontab/1.php
+++ api/crontab/1.php
@@ -24,7 +24,7 @@ echo "ok";
 function randomkeys($length)
 {
     $key= '';
-    $pattern = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ';
+    $pattern = '123456789abcdefghjklmnpqrstuvwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ';
     for($i=0;$i<$length;$i++)   
     {   
         $key .= $pattern{mt_rand(0,35)};    //生成php随机数   
