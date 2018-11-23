d8d7b9a612640834dc2fdd4f992f871a8d4bd019
songxingwei <you@example.com>
2018-11-23 14:54:24 +0800

    1

--- api/crontab/1.php
+++ api/crontab/1.php
@@ -5,10 +5,10 @@ error_reporting(E_ALL | E_STRICT);
 
 
 
-for($i=0;$i<60;$i++){
+for($i=0;$i<40;$i++){
     $data['id'] = get_guid();
     $data['coupon_code'] = "ccvt-".randomkeys(8);
-    $data['amount'] = 100;
+    $data['amount'] = 200;
     $data['ctime'] = date('Y-m-d H:i:s');
     $data['utime'] = time();
     $db = new DB_COM();
