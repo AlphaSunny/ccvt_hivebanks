0f3a0677447142d4323ba12443c5efd5f5401aa5
songxingwei <you@example.com>
2018-11-23 14:28:15 +0800

    1

---
+++ api/crontab/1.php
@@ -0,0 +1,18 @@
+<?php
+require_once "../inc/common.php";
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+
+
+function randomkeys($length)
+{
+    $key= '';
+    $pattern = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ';
+    for($i=0;$i<$length;$i++)   
+    {   
+        $key .= $pattern{mt_rand(0,35)};    //生成php随机数   
+    }   
+    return $key;   
+}   
+echo randomkeys(8); 
+?>
