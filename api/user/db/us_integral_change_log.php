6c2e045e6bb6519e5104081768d62b58804898aa
songxingwei <you@example.com>
2018-11-26 16:53:22 +0800

    1

--- api/user/db/us_integral_change_log.php
+++ api/user/db/us_integral_change_log.php
@@ -9,7 +9,7 @@ function  us_integral_change_log($us_id)
 {  
     $db = new DB_COM();
     $unit = get_la_base_unit();
-    $sql = "select tx_amount/'{$unit}',utime,tx_detail from us_glory_integral_change_log WHERE debit_id='{$us_id}'";
+    $sql = "select tx_amount/'{$unit}' as tx_amount,utime,tx_detail from us_glory_integral_change_log WHERE debit_id='{$us_id}'";
     $db->query($sql);
     $rows = $db->fetchAll();
     return $rows;
