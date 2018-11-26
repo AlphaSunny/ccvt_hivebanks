eae96f5541beb8f6d779ab31b06abfde79923184
songxingwei <you@example.com>
2018-11-26 17:41:06 +0800

    1

---
+++ api/crontab/2.php
@@ -0,0 +1,11 @@
+<?php
+require_once "../inc/common.php";
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+
+$db = new DB_COM();
+$sql = "select name,(select count(bot_message_id) from bot_message where bot_nickname=name) as count from bot_group_members WHERE group_id=1";
+$db->query($sql);
+$rows = $db->fetchAll();
+print_r($rows);
+?>
