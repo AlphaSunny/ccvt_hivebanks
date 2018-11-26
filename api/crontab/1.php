495a71c2591769297a47c06ba089fc415500e208
songxingwei <you@example.com>
2018-11-26 11:39:00 +0800

    1

--- api/crontab/1.php
+++ api/crontab/1.php
@@ -5,12 +5,14 @@ error_reporting(E_ALL | E_STRICT);
 
 
 
-for($i=0;$i<10;$i++){
+for($i=0;$i<50;$i++){
     $data['id'] = get_guid();
     $data['coupon_code'] = "ccvt-".randomkeys(8);
     $data['amount'] = 1000;
     $data['ctime'] = date('Y-m-d H:i:s');
     $data['utime'] = time();
+    $data['effective_date'] = date('Y-m-d H:i:s');
+    $data['expiry_date'] = '2018-12-31 23:59:59';
     $db = new DB_COM();
     $sql = $db->sqlInsert("us_voucher", $data);
     $id = $db->query($sql);
