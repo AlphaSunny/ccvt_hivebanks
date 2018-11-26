9767ab6fca3fdc56ee594ad3e3f63d97d912ff12
songxingwei <you@example.com>
2018-11-26 17:15:21 +0800

    1

--- api/user/exchange_voucher.php
+++ api/user/exchange_voucher.php
@@ -53,6 +53,7 @@ if (!$rows){
 $rtn_ary = array();
 $rtn_ary['errcode'] = '0';
 $rtn_ary['errmsg'] = '';
+$rtn_ary['us_amount'] = get_us_account($us_id)/get_la_base_unit();
 $rtn_str = json_encode($rtn_ary);
 php_end($rtn_str);
 
