39a040c97b8202cc80a6ffda56a0e3ea924d3c22
songxingwei <you@example.com>
2018-11-26 17:43:55 +0800

    1

--- api/crontab/2.php
+++ api/crontab/2.php
@@ -5,7 +5,6 @@ error_reporting(E_ALL | E_STRICT);
 
 $db = new DB_COM();
 $sql = "select name,(select count(bot_message_id) from bot_message where bot_nickname=name) as count from bot_group_members WHERE group_id=1";
-echo $sql;
 $db->query($sql);
 $rows = $db->fetchAll();
 print_r($rows);
