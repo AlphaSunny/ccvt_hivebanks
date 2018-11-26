95ef1b3055c6f8e8437bda3e97de038e791235b4
songxingwei <you@example.com>
2018-11-26 16:52:04 +0800

    1

--- api/user/db/us_integral_change_log.php
+++ api/user/db/us_integral_change_log.php
@@ -8,7 +8,7 @@
 function  us_integral_change_log($us_id)
 {  
     $db = new DB_COM();
-    $sql = "select debit_id,tx_amount,utime,tx_detail from us_glory_integral_change_log WHERE debit_id='{$us_id}'";
+    $sql = "select tx_amount,utime,tx_detail from us_glory_integral_change_log WHERE debit_id='{$us_id}'";
     $db->query($sql);
     $rows = $db->fetchAll();
     return $rows;
