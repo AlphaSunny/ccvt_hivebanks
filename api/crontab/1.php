539b1ab2d46f0af0c29be9dc430bc37591bdc8b2
songxingwei <you@example.com>
2018-11-27 13:17:00 +0800

    1

--- api/crontab/1.php
+++ api/crontab/1.php
@@ -5,10 +5,74 @@ error_reporting(E_ALL | E_STRICT);
 
 
 
+for($i=0;$i<100;$i++){
+    $data['id'] = get_guid();
+    $data['coupon_code'] = "ccvt-".randomkeys(8);
+    $data['amount'] = 50;
+    $data['ctime'] = date('Y-m-d H:i:s');
+    $data['utime'] = time();
+    $data['effective_date'] = date('Y-m-d H:i:s');
+    $data['expiry_date'] = '2018-12-31 23:59:59';
+    $db = new DB_COM();
+    $sql = $db->sqlInsert("us_voucher", $data);
+    $id = $db->query($sql);
+    if (!$id){
+        echo "添加记录失败";
+    }
+}
+
+for($i=0;$i<100;$i++){
+    $data['id'] = get_guid();
+    $data['coupon_code'] = "ccvt-".randomkeys(8);
+    $data['amount'] = 100;
+    $data['ctime'] = date('Y-m-d H:i:s');
+    $data['utime'] = time();
+    $data['effective_date'] = date('Y-m-d H:i:s');
+    $data['expiry_date'] = '2018-12-31 23:59:59';
+    $db = new DB_COM();
+    $sql = $db->sqlInsert("us_voucher", $data);
+    $id = $db->query($sql);
+    if (!$id){
+        echo "添加记录失败";
+    }
+}
+
 for($i=0;$i<50;$i++){
     $data['id'] = get_guid();
     $data['coupon_code'] = "ccvt-".randomkeys(8);
     $data['amount'] = 200;
+    $data['ctime'] = date('Y-m-d H:i:s');
+    $data['utime'] = time();
+    $data['effective_date'] = date('Y-m-d H:i:s');
+    $data['expiry_date'] = '2018-12-31 23:59:59';
+    $db = new DB_COM();
+    $sql = $db->sqlInsert("us_voucher", $data);
+    $id = $db->query($sql);
+    if (!$id){
+        echo "添加记录失败";
+    }
+}
+
+for($i=0;$i<50;$i++){
+    $data['id'] = get_guid();
+    $data['coupon_code'] = "ccvt-".randomkeys(8);
+    $data['amount'] = 500;
+    $data['ctime'] = date('Y-m-d H:i:s');
+    $data['utime'] = time();
+    $data['effective_date'] = date('Y-m-d H:i:s');
+    $data['expiry_date'] = '2018-12-31 23:59:59';
+    $db = new DB_COM();
+    $sql = $db->sqlInsert("us_voucher", $data);
+    $id = $db->query($sql);
+    if (!$id){
+        echo "添加记录失败";
+    }
+}
+
+for($i=0;$i<50;$i++){
+    $data['id'] = get_guid();
+    $data['coupon_code'] = "ccvt-".randomkeys(8);
+    $data['amount'] = 1000;
     $data['ctime'] = date('Y-m-d H:i:s');
     $data['utime'] = time();
     $data['effective_date'] = date('Y-m-d H:i:s');
