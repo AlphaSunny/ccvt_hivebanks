ba89625a9bc781b4b51486b538437410091a5cab
Edwin <1045416962@qq.com>
2018-11-28 14:09:14 +0800

    h5

--- h5/assets/js/glory.js
+++ h5/assets/js/glory.js
@@ -31,19 +31,19 @@ $(function () {
                 $.each(data, function (i, val) {
                     if (i <= 9) {
                         li += "<li class='wow bounceInRight' data-wow-delay=" + Number((i + 1) * 0.4) + 's' + ">" +
-                            "<div>" + arr[i].name + "</div>" +
+                            "<div>" + data[i].us_account + "</div>" +
                             "<div>" +
-                            "<small>" + arr[i].glory + "</small>" +
-                            "<small>" + arr[i].level + "</small>" +
+                            "<small>" + data[i].base_amount + "</small>" +
+                            "<small>" + data[i].scale + "</small>" +
                             "</div>" +
                             "</li>";
                         $(".item").html(li);
                     } else if (10 <= i <= 19) {
                         li_2 += "<li class='wow bounceInLeft' data-wow-delay=" + Number((i + 1) * 0.4) + 's' + ">" +
-                            "<div>" + arr[i].name + "</div>" +
+                            "<div>" + data[i].us_account + "</div>" +
                             "<div>" +
-                            "<small>" + arr[i].glory + "</small>" +
-                            "<small>" + arr[i].level + "</small>" +
+                            "<small>" + data[i].base_amount + "</small>" +
+                            "<small>" + data[i].scale + "</small>" +
                             "</div>" +
                             "</li>";
                         $(".item_2").html(li_2);
