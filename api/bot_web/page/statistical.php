8dfb7e12ec7ae1b9908ec9de01e9578dff126190
songxingwei <you@example.com>
2018-11-28 12:20:44 +0800

    1

--- api/bot_web/page/statistical.php
+++ api/bot_web/page/statistical.php
@@ -73,6 +73,7 @@ $url = $data['api_url'] . "/api/bot_web/page/chat.php?datetime=" . base64_encode
                             <a href="<?php echo $url; ?>">查看聊天记录</a>
                         </div>
                         <p class="font-size-14">时间:<?php echo $datetime; ?></p>
+                        <p class="font-size-14">今日奖励总人数:<?php echo $all_send_ccvt; ?></p>
                     </div>
                     <div class="flex space-between font-size-14 sm_title_text_color">
                         <p>今日奖励总数量:<?php echo $all_send_ccvt; ?>(CCVT)</p>
