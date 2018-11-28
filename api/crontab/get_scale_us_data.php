642cbb8d72244f48754c2282983de4cbf7b81905
songxingwei <you@example.com>
2018-11-28 11:42:39 +0800

    1

--- api/crontab/get_scale_us_data.php
+++ api/crontab/get_scale_us_data.php
@@ -7,6 +7,16 @@ error_reporting(E_ALL | E_STRICT);
 //获取升级用户信息
 
 $db = new DB_COM();
-
+$unit = get_la_base_unit();
 //用户
-$sql = "select * from us_base as us LEFT JOIN us_asset as us_as on us.us_id=us_as.us_id where us.scale=1 ORDER BY us_as.base_amount DESC limit 20";
+$sql = "select us.us_account,us_as.base_amount/'{$unit}' as base_amount,us_scale from us_base as us LEFT JOIN us_asset as us_as on us.us_id=us_as.us_id where us.scale=1 ORDER BY us_as.base_amount DESC limit 20";
+$db->query($sql);
+$rows = $db->fetchAll();
+
+// 返回数据做成
+$rtn_ary = array();
+$rtn_ary['errcode'] = '0';
+$rtn_ary['errmsg'] = '';
+$rtn_ary['rows'] =$rows;
+$rtn_str = json_encode($rtn_ary);
+php_end($rtn_str);
