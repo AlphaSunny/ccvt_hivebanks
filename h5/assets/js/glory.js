85e165566e6aba4c1338b984b6a155c55156e22e
Edwin <1045416962@qq.com>
2018-11-28 14:17:46 +0800

    h5

--- h5/assets/js/glory.js
+++ h5/assets/js/glory.js
@@ -23,39 +23,35 @@ $(function () {
     // ];
     var li = "", li_2 = "";
     $.ajax({
-        url:"https://ccvt_test.fnying.com/api/crontab/get_scale_us_data.php",
-        type:"GET",
-        success:function (response) {
-            // if(response.errcode == "0"){
-            //     var data = JSON.parse(response.rows);
-                var data = response.rows;
-                console.log(response.rows);
-                console.log(data);
-                $.each(data, function (i, val) {
-                    console.log(val);
-                    if (i <= 9) {
-                        li += "<li class='wow bounceInRight' data-wow-delay=" + Number((i + 1) * 0.4) + 's' + ">" +
-                            "<div>" + data[i].us_account + "</div>" +
-                            "<div>" +
-                            "<small>" + data[i].base_amount + "</small>" +
-                            "<small>" + data[i].scale + "</small>" +
-                            "</div>" +
-                            "</li>";
-                        $(".item").html(li);
-                    } else if (10 <= i <= 19) {
-                        li_2 += "<li class='wow bounceInLeft' data-wow-delay=" + Number((i + 1) * 0.4) + 's' + ">" +
-                            "<div>" + data[i].us_account + "</div>" +
-                            "<div>" +
-                            "<small>" + data[i].base_amount + "</small>" +
-                            "<small>" + data[i].scale + "</small>" +
-                            "</div>" +
-                            "</li>";
-                        $(".item_2").html(li_2);
-                    }
-                });
-            // }
+        url: "https://ccvt_test.fnying.com/api/crontab/get_scale_us_data.php",
+        type: "GET",
+        dataType:"jsonp",
+        success: function (response) {
+            var data = response.rows;
+            $.each(data, function (i, val) {
+                console.log(val);
+                if (i <= 9) {
+                    li += "<li class='wow bounceInRight' data-wow-delay=" + Number((i + 1) * 0.4) + 's' + ">" +
+                        "<div>" + data[i].us_account + "</div>" +
+                        "<div>" +
+                        "<small>" + data[i].base_amount + "</small>" +
+                        "<small>" + data[i].scale + "</small>" +
+                        "</div>" +
+                        "</li>";
+                    $(".item").html(li);
+                } else if (10 <= i <= 19) {
+                    li_2 += "<li class='wow bounceInLeft' data-wow-delay=" + Number((i + 1) * 0.4) + 's' + ">" +
+                        "<div>" + data[i].us_account + "</div>" +
+                        "<div>" +
+                        "<small>" + data[i].base_amount + "</small>" +
+                        "<small>" + data[i].scale + "</small>" +
+                        "</div>" +
+                        "</li>";
+                    $(".item_2").html(li_2);
+                }
+            });
         },
-        error:function () {
+        error: function () {
 
         }
     });
