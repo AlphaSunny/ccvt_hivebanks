fd473c16c30d52cae657c5e6196468e5e95c45cc
Edwin <1045416962@qq.com>
2018-11-28 10:58:19 +0800

    h5

---
+++ h5/assets/js/glory.js
@@ -0,0 +1,25 @@
+$(function () {
+    var arr = [
+        {"name": "edwin", "level": "1", "glory": "100"},
+        {"name": "edwin", "level": "1", "glory": "100"},
+        {"name": "edwin", "level": "1", "glory": "100"},
+        {"name": "edwin", "level": "1", "glory": "100"},
+        {"name": "edwin", "level": "1", "glory": "100"},
+        {"name": "edwin", "level": "1", "glory": "100"},
+        {"name": "edwin", "level": "1", "glory": "100"},
+        {"name": "edwin", "level": "1", "glory": "100"},
+        {"name": "edwin", "level": "1", "glory": "100"},
+        {"name": "edwin", "level": "1", "glory": "100"},
+    ];
+    var li = "";
+    $.each(arr, function (i, val) {
+        li += "<li class='wow bounceInLeft' data-wow-delay=" + Number((i + 1) * 0.5) + 's' + ">" +
+            "<div>" + arr[i].name + "</div>" +
+            "<div>" +
+            "<small>" + arr[i].glory + "</small>" +
+            "<small>" + arr[i].level + "</small>" +
+            "</div>" +
+            "</li>"
+    });
+    $(".item").html(li);
+});
