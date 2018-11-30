f8f2d191513dc4489e39607ac45394cb3ea6c257
Edwin <1045416962@qq.com>
2018-11-30 17:07:55 +0800

    h5

--- h5/assets/js/glory.js
+++ h5/assets/js/glory.js
@@ -1,40 +1,6 @@
 $(function () {
     var li = "", li_2 = "";
 
-    function GetGlory() {
-        $.ajax({
-            url: "https://ccvt_test.fnying.com/api/crontab/get_scale_us_data.php",
-            type: "GET",
-            dataType: "jsonp",
-            success: function (response) {
-                var data = response.rows;
-                $(".up_title,#first_box").remove();
-                console.log(data.length);
-                var first_col = Math.floor(data.length / 2); //12
-                var first_col2 = Math.ceil(data.length / 2); //13
-                console.log(first_col);
-                console.log(first_col2);
-                $(".leaderboard_2 ol").css("counter-reset","leaderboard_2 "+ Math.floor(data.length / 2) +"");
-                $.each(data, function (i, val) {
-                    if (i < Math.floor(data.length / 2)) {
-                        li += "<li class='wow bounceInRight' data-wow-delay=" + Number((i + 1) * 0.2) + 's' + ">" +
-                            "<div>" + data[i].us_account + "</div>" +
-                            "</li>";
-                        $(".item").html(li);
-                    } else {
-                        li_2 += "<li class='wow bounceInLeft' data-wow-delay=" + Number((i + 1) * 0.2) + 's' + ">" +
-                            "<div>" + data[i].us_account + "</div>" +
-                            "</li>";
-                        $(".item_2").html(li_2);
-                    }
-                });
-            },
-            error: function () {
-                alert("排行榜获取失败");
-            }
-        });
-    }
-
     function SetProgress(i) {
         $(".progress_span").css("width", i + "%");
         $(".progress_text").text(i + "%");
@@ -49,8 +15,8 @@ $(function () {
             $(".up_title").text("升级成功");
 
             setTimeout(function () {
-                GetGlory();
-            },2000);
+                window.location.href="glory2.html";
+            },1000);
         }
         if (i <= 100) {
             setTimeout(function () {
