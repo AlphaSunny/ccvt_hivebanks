115fa7341edf7b61513436e27239ccdf7dff6974
songxingwei <you@example.com>
2018-11-30 10:24:55 +0800

    1

--- api/crontab/get_voucher.php
+++ api/crontab/get_voucher.php
@@ -1,12 +1,13 @@
 <head runat="server">
     <title></title>
     <style type="text/css">
-        .c{text-align: center}
+        .c{text-align: center;padding-top: 300px}
 
     </style>
 </head>
 <body>
 <div class="c">
-<h2>ccvt-dfasdfasdfsdf</h2>
+
+<h1>ccvt-dfasdfasdfsdf</h1>
 </div>
 </body>
