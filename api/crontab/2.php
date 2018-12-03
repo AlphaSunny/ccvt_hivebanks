f8c4505098e5db87f1566d2fed514b13709ab39e
songxingwei <you@example.com>
2018-12-03 14:53:07 +0800

    1

--- api/crontab/2.php
+++ api/crontab/2.php
@@ -4,12 +4,16 @@ ini_set("display_errors", "On");
 error_reporting(E_ALL | E_STRICT);
 
 $db = new DB_COM();
-$sql = "select name,(select count(bot_message_id) from bot_message where bot_nickname=name) as count from bot_group_members WHERE group_id=9 ORDER BY count DESC ";
+//$sql = "select name,(select count(bot_message_id) from bot_message where bot_nickname=name) as count from bot_group_members WHERE group_id=9 ORDER BY count DESC ";
+//
+//$db->query($sql);
+//$rows = $db->fetchAll();
+//foreach ($rows as $k=>$v){
+//    echo "发言数:".$v['count']."&nbsp;&nbsp;&nbsp;&nbsp;".$v['name']."<br />";
+//}
 
-$db->query($sql);
-$rows = $db->fetchAll();
-foreach ($rows as $k=>$v){
-    echo "发言数:".$v['count']."&nbsp;&nbsp;&nbsp;&nbsp;".$v['name']."<br />";
+for ($i=0;$i<10;$i++){
+    echo $i."<br />";
 }
 
 ?>
