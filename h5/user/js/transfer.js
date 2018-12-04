36f2472b532646ab6031195d22aea27e143ddc9e
Edwin <1045416962@qq.com>
2018-12-04 17:56:41 +0800

    h5

--- h5/user/js/transfer.js
+++ h5/user/js/transfer.js
@@ -4,14 +4,14 @@ $(function () {
     var is_pass_hash = "";
     //get bind info
     BindingInformation(token, function (response) {
-        if(response.errcode == "0"){
+        if (response.errcode == "0") {
             var data = response.rows;
             $.each(data, function (i, val) {
-                if(data[i].bind_name == "pass_hash" && data[i].bind_flag == "1"){
+                if (data[i].bind_name == "pass_hash" && data[i].bind_flag == "1") {
                     is_pass_hash = "is_pass_hash";
                 }
             });
-            if(is_pass_hash){
+            if (!is_pass_hash) {
                 $("#not_bind_funPass_modal").fadeIn();
             }
         }
@@ -19,9 +19,6 @@ $(function () {
         LayerFun(response.errcode);
     });
 
-    $(".customize_modal_cancel_btn").click(function () {
-        $("#not_bind_funPass_modal").css("display","none");
-    });
     $(".exchange_confirm_btn").click(function () {
         window.location.href = "./fundPasswordBind.html?transfer_funPass=transfer_funPass";
     })
