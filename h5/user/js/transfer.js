bc31e6a68a5bc3aff8bd76f0040df4357f034029
Edwin <1045416962@qq.com>
2018-12-04 17:45:36 +0800

    h5

--- h5/user/js/transfer.js
+++ h5/user/js/transfer.js
@@ -7,34 +7,22 @@ $(function () {
         if(response.errcode == "0"){
             var data = response.rows;
             $.each(data, function (i, val) {
-                // if(data[i].bind_name == "pass_hash" && data[i].bind_flag == "1"){
-                //     is_pass_hash = "is_pass_hash";
-                // }
+                if(data[i].bind_name == "pass_hash" && data[i].bind_flag == "1"){
+                    is_pass_hash = "is_pass_hash";
+                }
             });
-            if(!is_pass_hash){
-                alert("未绑定资金密码");
+            if(is_pass_hash){
+                $("#not_bind_funPass_modal").fadeIn();
             }
         }
     }, function (response) {
         LayerFun(response.errcode);
     });
 
-    // window.location.href = "funPasswordBind.html?transfer_funPass= transfer_funPass";
-    // var us_account = GetCookie('us_account');
-    // $(".payNumber").val(us_account);
-    // Transfer in/out record
-    //     limit = 0, offset = 5,
-    //     trans_api_url = 'log_balance.php';
-    // AllRecord(token,limit,offset,trans_api_url,function (response){
-    //     if(response.errcode == '0'){
-    //         var data = response.rows;
-    //
-    //     }
-    // },function (response){
-    //     execI18n();
-    //     layer.msg(response.errcode);
-    //     if(response.errcode == '114'){
-    //         window.location.href = 'login.html';
-    //     }
-    // });
+    $(".customize_modal_cancel_btn").click(function () {
+        $("#not_bind_funPass_modal").css("display","none");
+    });
+    $(".exchange_confirm_btn").click(function () {
+        window.location.href = "funPasswordBind.html?transfer_funPass= transfer_funPass";
+    })
 });
