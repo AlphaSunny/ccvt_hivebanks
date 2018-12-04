3ac9bab1a6f97af81e46cd6e5407b670ab68b16f
Edwin <1045416962@qq.com>
2018-12-04 17:21:54 +0800

    h5

--- h5/user/js/transfer.js
+++ h5/user/js/transfer.js
@@ -23,9 +23,8 @@ $(function () {
     // var us_account = GetCookie('us_account');
     // $(".payNumber").val(us_account);
     // Transfer in/out record
-    var token = GetCookie('user_token'),
-        limit = 0, offset = 5,
-        trans_api_url = 'log_balance.php';
+    //     limit = 0, offset = 5,
+    //     trans_api_url = 'log_balance.php';
     // AllRecord(token,limit,offset,trans_api_url,function (response){
     //     if(response.errcode == '0'){
     //         var data = response.rows;
