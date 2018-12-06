5eaf77218a96a9fdbc0ad0d35eace9472930e147
Edwin <1045416962@qq.com>
2018-12-06 18:07:01 +0800

    h5

--- h5/leaderBoard/js/chat_person.js
+++ h5/leaderBoard/js/chat_person.js
@@ -1,9 +1,15 @@
 $(function () {
-   var wechat = GetQueryString("wechat");
-   
-   GetChatPerson(wechat,function (response) {
-       console.log(response);
-   },function (response) {
-       
-   })
+    var wechat = GetQueryString("wechat");
+
+    GetChatPerson(wechat, function (response) {
+        console.log(response);
+        if (response.errcode == "0") {
+            var data = response.rows;
+            if (data.length <= 0) {
+                $('.chat_content').html("<h1>暂无聊天内容</h1>")
+            }
+        }
+    }, function (response) {
+        alert(response.errmsg);
+    })
 });
