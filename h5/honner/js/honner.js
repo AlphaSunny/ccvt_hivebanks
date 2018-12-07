4e1f1781ccf82d14752a456a0c88c4b292a57c36
Edwin <1045416962@qq.com>
2018-12-07 18:20:22 +0800

    h5

--- h5/honner/js/honner.js
+++ h5/honner/js/honner.js
@@ -149,11 +149,15 @@ $(function () {
     //     var wechat = $(this).text();
     //     window.location.href = "chat_person.html?wechat=" + encodeURI(encodeURI(wechat));
     // });
-
+    //显示聊天内容
     $(document).on("click", ".message_icon", function () {
         var wechat = $(this).parents("tr").find(".link_name").text();
         $("iframe").attr("src", "./chat_person.html?wechat=" + encodeURI(encodeURI(wechat))).fadeIn();
         // window.location.href = "chat_person.html?wechat=" + encodeURI(encodeURI(wechat));
+    });
+    //关闭聊天内容close_page
+    $(document).on("click", ".close_page", function () {
+        $("iframe").attr("display", "none");
     });
 
     //赞/踩
