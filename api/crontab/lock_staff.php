89edf5532531b1485de0882e6f1c9a5bf9258672
范助国 <ahino@fanzhuguodeMacBook-Pro.local>
2018-12-07 15:18:40 +0800

    lockFix

--- api/crontab/lock_staff.php
+++ api/crontab/lock_staff.php
@@ -215,6 +215,10 @@ function get_us_account($us_id){
 
 
 function getMillisecond(){
-    list($s1,$s2)=explode(' ',microtime());
-    return (float)sprintf('%.0f',(floatval($s1)+floatval($s2))*1000);
+
+    $time = explode (" ", microtime () );
+    $time = $time [1] . ($time [0] * 1000);
+    $time2 = explode ( ".", $time );
+    $time = $time2 [0];
+    return $time;
 }
