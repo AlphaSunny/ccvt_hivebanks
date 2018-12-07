b2d1876d08bdea9fd55b2b636fb2a325006cecd3
Edwin <1045416962@qq.com>
2018-12-07 17:56:41 +0800

    h5

--- h5/honner/js/honner.js
+++ h5/honner/js/honner.js
@@ -152,7 +152,7 @@ $(function () {
 
     $(document).on("click", ".message_icon", function () {
         var wechat = $(this).parents("tr").find(".link_name").text();
-        $("iframe").attr("src", "chat_person.html?wechat=" + encodeURI(encodeURI(wechat))).fadeIn();
+        $("iframe").attr("src", "./chat_person.html?wechat=" + encodeURI(encodeURI(wechat))).fadeIn();
         // window.location.href = "chat_person.html?wechat=" + encodeURI(encodeURI(wechat));
     });
 
