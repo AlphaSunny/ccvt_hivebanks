69259629ad705357bab010ff6922627a0245c284
songxingwei <you@example.com>
2018-12-07 14:44:40 +0800

    1

--- api/crontab/2.php
+++ api/crontab/2.php
@@ -18,10 +18,10 @@ $time = "9:05:10";
 $time = str_replace('：', ':', $time);
 
 $tmparray = explode(':',$time);
-print_r($tmparray);die;
-if(count($tmparray)>2){
-    exit_error('139','格式错误');
-}
+print_r($tmparray);
+print_r($tmparray[0]);
+
+
 
 
 //等级规则数据
