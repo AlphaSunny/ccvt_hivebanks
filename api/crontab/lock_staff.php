d8cfdd1aaa297ff621405987c123474bad0621d6
范助国 <ahino@fanzhuguodeMacBook-Pro.local>
2018-12-07 11:43:16 +0800

    lockFix

--- api/crontab/lock_staff.php
+++ api/crontab/lock_staff.php
@@ -9,7 +9,10 @@ require_once '../inc/common.php';
 header("cache-control:no-cache,must-revalidate");
 header("Content-Type:application/json;charset=utf-8");
 
+define('UNIT','100000000');
+define('BA_ID','6C69520E-E454-127B-F474-452E65A3EE75');
 lock_auto();
+
 
 function lock_auto(){
 
@@ -36,7 +39,7 @@ function lock_auto(){
 
 function ba_cut($amount){
     $db = new DB_COM();
-    $sql = "update ba_base set base_amount = base_amount - $amount where  ba_id='6C69520E-E454-127B-F474-452E65A3EE75'";
+    $sql = "update ba_base set base_amount = base_amount - $amount where  ba_id= BA_ID";
     $db->query($sql);
     $res = $db->affectedRows();
     if($res)
@@ -61,4 +64,106 @@ function log_info($phone){
     if($db->affectedRows())
         return true;
     return false;
+}
+
+function log_com_base($us_id,$amount){
+    $db = new DB_COM();
+    $data = array();
+    $us_type = 'big_us_lock';
+    $ctime = date('Y-m-d H:i:s');
+
+    $data['hash_id'] = hash('md5', BA_ID . $us_type . get_ip() . time() . rand(1000, 9999) . $ctime);
+    $data['tx_id'] = hash('md5', BA_ID  . 'phone' . get_ip() . time() . date('Y-m-d H:i:s'));;
+    $data['prvs_hash'] = get_recharge_pre_hash(BA_ID);
+
+    $data['prvs_hash'] = $data['prvs_hash'] == 0 ? $data['hash_id'] : $data['prvs_hash'];
+    $data['debit_id'] = $us_id;
+    $data['credit_id']= BA_ID;
+    $data['tx_type'] = 'big_us_lock';
+    $data["tx_amount"] = $amount*UNIT;
+    $data["credit_balance"] = get_ba_account(BA_ID-($amount*UNIT));
+    $data["utime"] = time();
+    $data["ctime"] = $ctime;
+    $sql = $db->sqlInsert("com_base_balance", $data);
+
+    $uata = array();
+    $uata['hash_id'] = hash('md5', $us_id . $us_type . get_ip() . time() . rand(1000, 9999) . $ctime);
+    $uata['tx_id'] = hash('md5', BA_ID  . 'phone' . get_ip() . time() . date('Y-m-d H:i:s'));;
+    $uata['prvs_hash'] = get_recharge_pre_hash($us_id);
+    $uata['prvs_hash'] = $uata['prvs_hash'] == 0 ? $uata['hash_id'] : $uata['prvs_hash'];
+
+    $uata['credit_id'] = $us_id;
+    $uata['debit_id']= BA_ID;
+    $uata['tx_type'] = 'big_us_lock';
+    $uata["tx_amount"] = $amount*UNIT;
+    $uata["credit_balance"] = get_ba_account(BA_ID-($amount*UNIT));
+    $uata["utime"] = time();
+    $uata["ctime"] = $ctime;
+    $uql = $db->sqlInsert("com_base_balance", $uata);
+
+    if($db->query($sql)&&$db->query($uata))
+        return true;
+    return false;
+
+
+}
+
+function log_com_transfer($us_id,$amount){
+
+    $db = new DB_COM();
+    $data['hash_id'] = hash('md5', BA_ID . 11 . get_ip() . time() . rand(1000, 9999) . date('Y-m-d H:i:s'));
+    $prvs_hash = get_transfer_pre_hash(BA_ID);
+    $data['prvs_hash'] = $prvs_hash == 0 ? $data['hash_id'] : $prvs_hash;
+    $data['credit_id'] = BA_ID;
+    $data['debit_id'] = $us_id;
+    $data['tx_amount'] = $amount*UNIT;
+    $data['credit_balance'] = get_ba_account(BA_ID-$amount);
+    $data['tx_hash'] = hash('md5', BA_ID . 11 . get_ip() . time() . date('Y-m-d H:i:s'));
+    $data['flag'] = 11;
+    $data['transfer_type'] = 'ba-us';
+    $data['transfer_state'] = 1;
+    $data['tx_detail'] = 'big_account_lock';
+    $data['give_or_receive'] = 1;
+    $data['ctime'] = time();
+    $data['utime'] = date('Y-m-d H:i:s',time());
+    $sql = $db->sqlInsert("com_transfer_request", $data);
+
+    $dat['hash_id'] = hash('md5', $us_id . 11 . get_ip() . time() . rand(1000, 9999) . date('Y-m-d H:i:s'));
+    $prvs_hash = get_transfer_pre_hash($us_id);
+    $dat['prvs_hash'] = $prvs_hash == 0 ? $data['hash_id'] : $prvs_hash;
+    $dat['credit_id'] = $us_id;
+    $dat['debit_id'] = BA_ID;
+    $dat['tx_amount'] = $amount*UNIT;
+    $dat['credit_balance'] = get_us_account($us_id)+$dat['tx_amount'];
+    $dat['tx_hash'] = hash('md5', $us_id . 11 . get_ip() . time() . date('Y-m-d H:i:s'));
+    $dat['flag'] = 11;
+    $dat['transfer_type'] = 'ba-us';
+    $dat['transfer_state'] = 1;
+    $dat['tx_detail'] = 'big_account_lock';
+    $dat['give_or_receive'] = 2;
+    $dat['ctime'] = time();
+    $dat['utime'] = date('Y-m-d H:i:s',time());
+    $uql = $db->sqlInsert("com_transfer_request", $dat);
+
+    if($db->query($sql)&&$db->query($uql))
+        return true;
+    return false;
+
+}
+
+
+
+//======================================
+// 函数: 获取充值的前置hash
+// 参数: ba_id                 baID
+// 返回: hash_id               前置hashid
+//======================================
+function  get_recharge_pre_hash($ba_id)
+{
+    $db = new DB_COM();
+    $sql = "SELECT hash_id FROM com_base_balance WHERE credit_id = '{$ba_id}' and tx_type = 'ba_send' ORDER BY  ctime DESC LIMIT 1";
+    $hash_id = $db->getField($sql, 'hash_id');
+    if($hash_id == null)
+        return 0;
+    return $hash_id;
 }
