81b18d52244e3b69688439473f84ac8050b6ad70
Edwin <1045416962@qq.com>
2018-12-07 13:52:49 +0800

    h5

--- h5/leaderBoard/js/chat_person.js
+++ h5/leaderBoard/js/chat_person.js
@@ -23,7 +23,7 @@ $(function () {
                     }
                     if (data[i].type == "Picture") {
                         li += "<li class='chat_item'>" +
-                            "<p class='chat_item_name'>" + data[i].bot_nickname.substring(0,1) + "</p>" +
+                            "<p class='chat_item_name'>" + data[i].bot_nickname.substr(0,1) + "</p>" +
                             "<div class='chat_item_content_box'>" +
                             "<p class='name'><span>" + data[i].bot_nickname + "</span>&nbsp;&nbsp;<span>" + data[i].bot_send_time + "</span></p>" +
                             "<p class='chat_item_content'><img src='" + data[i].bot_content + "' alt=''></p>" +
