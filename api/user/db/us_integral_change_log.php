966392f2d7c6052878838771d4ae26f4cad9ca3d
songxingwei <you@example.com>
2018-12-10 18:22:15 +0800

    1

--- api/user/db/us_integral_change_log.php
+++ api/user/db/us_integral_change_log.php
@@ -1,5 +1,19 @@
 <?php
 
+
+//======================================
+// 函数: 获取荣耀积分变动记录总数
+// 参数:
+// 返回: count        记录总数
+//======================================
+function  get_us_integral_change_log_total($us_id)
+{
+    $db = new DB_COM();
+    $sql = "select * from us_glory_integral_change_log WHERE debit_id='{$us_id}'";
+    $db -> query($sql);
+    $count = $db -> affectedRows();
+    return $count;
+}
 //======================================
 // 函数: 获取荣耀积分变动记录
 // 参数: us_id        用户id
--- api/user/us_integral_change_log.php
+++ api/user/us_integral_change_log.php
@@ -28,6 +28,9 @@ $token = get_arg_str('GET', 'token',100);
 // 验证token
 $us_id = check_token($token);
 
+// 获取当前用户的交易总记录
+$total = get_us_integral_change_log_total($us_id);
+
 // 取得分页参数
 list($limit, $offset) = get_paging_arg('GET');
 
@@ -39,6 +42,7 @@ $rtn_ary = array();
 $rtn_ary['errcode'] = '0';
 $rtn_ary['errmsg'] = '';
 $rtn_ary['rows'] =$rows;
+$rtn_ary['total'] = $total;
 $rtn_str = json_encode($rtn_ary);
 php_end($rtn_str);
 
