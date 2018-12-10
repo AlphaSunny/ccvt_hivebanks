62aab079cecb4996660d99825aea574bd89e48b4
Edwin <1045416962@qq.com>
2018-12-10 16:14:58 +0800

    h5

--- h5/user/js/account.js
+++ h5/user/js/account.js
@@ -100,13 +100,13 @@ $(function () {
                 console.log(totalPage);
                 console.log(typeof totalPage);
                 if (totalPage <= 1) {
-                    // count = 1;
+                    count = 1;
                     console.log(1);
                 } else if (1 < totalPage && totalPage <= 6) {
-                    // count = totalPage;
+                    count = totalPage;
                     console.log("1-6");
                 } else {
-                    // count = 6;
+                    count = 6;
                     console.log("6---")
                 }
 
