a3740966bfefe2d76971596a0b60e28c3238beb2
Edwin <1045416962@qq.com>
2018-12-10 14:18:06 +0800

    h5

--- h5/honner/js/honner.js
+++ h5/honner/js/honner.js
@@ -22,7 +22,7 @@ $(function () {
         })
     }
 
-    var limit = 10, offset = 0, total = "";
+    var limit = 50, offset = 0, total = "";
 
     function Fun(limit, offset) {
         var tr = "", li = "";
