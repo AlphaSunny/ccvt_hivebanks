6096048dd6a0f01f038c3a16fda86da44b1c6d5f
songxingwei <you@example.com>
2018-12-11 14:21:11 +0800

    1

--- api/user/bnd_group.php
+++ api/user/bnd_group.php
@@ -4,8 +4,7 @@ require_once '../inc/common.php';
 require_once 'db/us_bind.php';
 require_once 'db/us_base.php';
 require_once 'db/us_log_bind.php';
-require_once '../inc/judge_format.php';
-require_once '../plugin/bind/GoogleAuthenticator/GoogleAuthenticator.php';
+
 
 header("cache-control:no-cache,must-revalidate");
 header("Content-Type:application/json;charset=utf-8");
