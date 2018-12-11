d4aec958eeb709e7ecf31bbea59b11355d78c309
songxingwei <you@example.com>
2018-12-11 18:28:53 +0800

    1

--- api/bot/get_topic.php
+++ api/bot/get_topic.php
@@ -5,24 +5,19 @@ header("cache-control:no-cache,must-revalidate");
 header("Content-Type:application/json;charset=utf-8");
 
 /*
-========================== 获取话题存入数据库 ==========================
+========================== 获取最新话题 ==========================
 
 */
 
 php_begin();
 
-$args = array('name','type');
-chk_empty_args('POST', $args);
-
-$data['name'] = get_arg_str('POST','name');
-$data['type'] = get_arg_str('POST','type');
-$data['ctime'] = time();
-//存入数据库
-$rows = to_topic_table($data);
+//获取最新文章
+$rows = get_news();
 
 $rtn_ary = array();
 $rtn_ary['errcode'] = '0';
 $rtn_ary['errmsg'] = '';
+$rtn_ary['content'] = $content;
 $rtn_str = json_encode($rtn_ary);
 php_end($rtn_str);
 
