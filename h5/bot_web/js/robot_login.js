9b637d172238a5583a8fa31353b71fb10f7fc98f
Edwin <1045416962@qq.com>
2018-12-12 15:50:56 +0800

    h5

--- h5/bot_web/js/robot_login.js
+++ h5/bot_web/js/robot_login.js
@@ -8,7 +8,7 @@ function GetWeChatGroupFun() {
             var data = response.rows;
             $.each(data, function (i, val) {
                 li += "<li class='list-group-item row'>" +
-                    "<div class='col-md-6 col-sm-6'>" +
+                    "<div>" +
                     "<input type='radio' name ='weChatGroup' id=" + data[i].id + " value=" + data[i].id + ">" +
                     "<label class='margin-left-1' for=" + data[i].id + ">" + data[i].name + "</label>" +
                     "</div>" +
