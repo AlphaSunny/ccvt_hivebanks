ef6a0d67944b607217aa3a18d62222bc04ac66bb
songxingwei <you@example.com>
2018-12-12 17:07:23 +0800

    1

--- api/crontab/move_all_data.php
+++ api/crontab/move_all_data.php
@@ -114,8 +114,7 @@ foreach ($suocang as $k=>$v){
 }
 
 
-$list = array_merge($scale_changes,$suocang);
-//$list = array_merge($reg_user,$invite_rows,$bot_rows,$voucher,$glory,$tiaozhang,$scale_changes,$suocang);
+$list = array_merge($reg_user,$invite_rows,$bot_rows,$voucher,$glory,$tiaozhang,$scale_changes,$suocang);
 array_multisort(array_column($list,'ctime'),SORT_ASC,$list);
 $ba_id = get_ba_id();
 $la_id = get_la_id();
