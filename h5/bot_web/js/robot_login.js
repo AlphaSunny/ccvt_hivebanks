99c89c0a037bf17ca423225f951d5b179c39180f
Edwin <1045416962@qq.com>
2018-12-12 15:30:54 +0800

    h5

--- h5/bot_web/js/robot_login.js
+++ h5/bot_web/js/robot_login.js
@@ -1,16 +1,39 @@
 var token = GetCookie("robot_token");
-function GetWeChatGroupFun(){
+
+function GetWeChatGroupFun() {
     //获取群列表
+    var li = "<li class='list-group-item row'></li>", div_group = "", div_type = "";
     GetWeChatGroup(token, function (response) {
-        console.log(response);
+        if (response.errcode == "0") {
+            var data = response.rows;
+            $.each(data, function (i, val) {
+                div_group = "<div class='col-md-6 col-sm-6'>" +
+                    "<input type='radio' name ='weChatGroup' id=" + data[i].id + " value=" + data[i].id + ">" +
+                    "<label for=" + data[i].id + ">" + data[i].name + "</label>" +
+                    "</div>";
+                li.append(div_group);
+            });
+        }
     }, function (response) {
         layer.msg(response);
     });
 
     //获取群类型
     GetWeChatGroupType(token, function (response) {
-        console.log(response);
+        if (response.errcode == "0") {
+            var data = response.rows;
+            $.each(data, function (i, val) {
+                div_type += "<div class='col-md-6 col-sm-6'>" +
+                    "<button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'>选择类型</button>" +
+                    "<ul class='dropdown-menu'>" +
+                    "<li class='type_dropdown_item' name=" + data[i].id + ">" + data[i].name + "</li>" +
+                    "</ul>" +
+                    "</div>";
+                li.append(div_type);
+            })
+        }
     }, function (response) {
         layer.msg(response);
     });
+    $(".list_group").html(li);
 }
