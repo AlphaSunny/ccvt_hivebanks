921ccce4cb7bda7ec3b2ff51701776ca4f4028ae
Edwin <1045416962@qq.com>
2018-12-12 14:38:26 +0800

    h5

--- h5/bot_web/js/robot_login.js
+++ h5/bot_web/js/robot_login.js
@@ -1,10 +1,9 @@
 var token = GetCookie("robot_token");
-//获取群列表
-GetWeChatGroup(token, function (response) {
-    console.log(response);
-}, function (response) {
-    layer.msg(response);
-});
-// $(function () {
-//
-// });
+function GetWeChatGroupFun(){
+    //获取群列表
+    GetWeChatGroup(token, function (response) {
+        console.log(response);
+    }, function (response) {
+        layer.msg(response);
+    });
+}
