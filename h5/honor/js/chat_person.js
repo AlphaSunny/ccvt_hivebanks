abb098834ece33eef98da67fdca1e4face2eecfb
Edwin <1045416962@qq.com>
2018-12-12 11:43:53 +0800

    h5

--- h5/honor/js/chat_person.js
+++ h5/honor/js/chat_person.js
@@ -70,7 +70,7 @@ $(function () {
     });
 
     //默认在最底部
-    var height = $("body").height();
+    var height = $("#chat_content").height();
 
     $("#chat_content").scrollTop(9999999999);
     console.log(height);
