cf05d881328b0be1169c7f1f2c013d99b0fa04e4
songxingwei <you@example.com>
2018-12-12 19:40:46 +0800

    1

--- api/crontab/move_all_data.php
+++ api/crontab/move_all_data.php
@@ -88,7 +88,7 @@ echo "赞踩:".$diancai/$unit."<br />";
 
 //兑换码兑换
 $sql = "select us_id,amount as send_money,exchange_time as ctime from us_voucher WHERE us_id!=0 AND is_effective=2 AND us_id='C8DC1099-D680-B12F-98AF-4054992F7B07'";
-
+echo $sql;
 $db->query($sql);
 $voucher = $db->fetchAll();
 foreach ($voucher as $k=>$v){
