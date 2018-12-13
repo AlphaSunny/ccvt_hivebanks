ef0a8005f76216ce42d321d62f93a6b23bfca246
Edwin <1045416962@qq.com>
2018-12-13 18:14:20 +0800

    h5

--- h5/bot_web/total_robot/js/common.js
+++ h5/bot_web/total_robot/js/common.js
@@ -157,6 +157,18 @@ function EditGroup(token, group_name, admin_del, group_id, suc_func, error_func)
         };
     CallRobotApi(api_url, post_data, suc_func, error_func);
 }
+//查看群成员列表
+function GetGroupMember(token, group_id, limit, offset, status, suc_func, error_func) {
+    var api_url = "group_members_list.php",
+        post_data = {
+            "token": token,
+            "group_id": group_id,
+            "limit": limit,
+            "offset": offset,
+            "status": status
+        };
+    CallRobotApi(api_url, post_data, suc_func, error_func);
+}
 
 /**
  * Disable button
