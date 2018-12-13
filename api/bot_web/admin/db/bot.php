c289ebd01fdce570d4e6e9a4ad0b85a6551142c1
songxingwei <you@example.com>
2018-12-13 10:46:29 +0800

    1

--- api/bot_web/admin/db/bot.php
+++ api/bot_web/admin/db/bot.php
@@ -114,7 +114,7 @@ function get_group_members_list($group_id,$status)
             $sql .= " AND bot_create_time between '{$start}' and '{$end}'";
         }
         $db->query($sql);
-        $row['chat_num'] = $db->getField($sql,'count');
+        $row[$k]['chat_num'] = $db->getField($sql,'count');
     }
     return $row;
 }
