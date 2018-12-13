3dcc66481c017e85324ed3c5ddafbd2f91a17ab1
Edwin <1045416962@qq.com>
2018-12-13 10:56:31 +0800

    h5

--- h5/bot_web/js/common.js
+++ h5/bot_web/js/common.js
@@ -245,11 +245,13 @@ function AddTask(token, time, group_id, content, suc_func, error_func) {
 }
 
 //查看群成员列表
-function GetGroupMember(token, group_id, suc_func, error_func) {
+function GetGroupMember(token, group_id, limit, offset, suc_func, error_func) {
     var api_url = "group_members_list.php",
         post_data = {
             "token": token,
-            "group_id": group_id
+            "group_id": group_id,
+            "limit": limit,
+            "offset": offset
         };
     CallRobotApi(api_url, post_data, suc_func, error_func);
 }
--- h5/bot_web/js/group_member.js
+++ h5/bot_web/js/group_member.js
@@ -7,13 +7,13 @@ $(function () {
 
     var url = getRootPath();
     var status = "-1";
-    $('#groupMemberTable').DataTable({
-        "ajax": url + "/api/bot_web/group_members_list.php?token=" + encodeURIComponent(token) + "&group_id=" + group_id+ "&status=" + status,
-        "deferRender":true,
-        "columns": [
-            {"data": "name"}
-        ]
-    });
+    // $('#groupMemberTable').DataTable({
+    //     "ajax": url + "/api/bot_web/group_members_list.php?token=" + encodeURIComponent(token) + "&group_id=" + group_id+ "&status=" + status,
+    //     "deferRender":true,
+    //     "columns": [
+    //         {"data": "name"}
+    //     ]
+    // });
 
     //查看聊天记录
     $(".lookChatCode").click(function () {
@@ -21,18 +21,25 @@ $(function () {
     });
 
     //获取群成员列表
-    // GetGroupMember(token,group_id, function (response) {
-    //     console.log(response);
-    //     if(response.errcode == "0"){
-    //         var data = response.rows, tr = "";
-    //         $.each(data, function (i, val) {
-    //             tr+="<tr>" +
-    //                 "<td>"+ data[i].name +"</td>" +
-    //                 "</tr>"
-    //         });
-    //         $("#groupMember").html(tr);
-    //     }
-    // }, function (response) {
-    //     layer.msg(response.errmsg);
-    // })
+    var limit = 10, offset = 0;
+
+    function GetGroupMemberFun() {
+        GetGroupMember(token, group_id,limit, offset, function (response) {
+            console.log(response);
+            if (response.errcode == "0") {
+                var data = response.rows, tr = "";
+                $.each(data, function (i, val) {
+                    tr += "<tr>" +
+                        "<td>" + data[i].name + "</td>" +
+                        "</tr>"
+                });
+                $("#groupMember").html(tr);
+            }
+        }, function (response) {
+            layer.msg(response.errmsg);
+        })
+    }
+
+    GetGroupMemberFun();
+
 });
