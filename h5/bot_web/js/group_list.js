197457437d1c16e3462f191089bafcf17cadec24
Edwin <1045416962@qq.com>
2018-12-13 15:02:08 +0800

    h5

--- h5/bot_web/js/group_list.js
+++ h5/bot_web/js/group_list.js
@@ -27,8 +27,31 @@ $(function () {
     // }
 
     //获取群列表
+    var tr = "", opt = "";
     GetGroupList(token, function (response) {
-        console.log(response);
+        if(response.errcode == "0"){
+            var data = response.rows;
+            $.each(data, function (i, val) {
+                if(data[i].is_audit == "1"){
+                    opt = "审核中";
+                }else if(data[i].is_audit == "3"){
+                    opt = "未通过";
+                }else {
+                    opt = "<button class='btn-success btn-sm editBtn'><i class='fa fa-pencil' aria-hidden='true'></i>编辑</button>" +
+                        "<button class='btn-sm btn-info infoBtn margin-left-5'><i class='fa fa-eye' aria-hidden='true'></i>详情</button>";
+                }
+                tr+="<tr>" +
+                    "<td class='id'>"+ data[i].id +"</td>" +
+                    "<td class='name'>"+ data[i].name +"</td>" +
+                    "<td>"+ data[i].del +"</td>" +
+                    "<td class='none is_del'>"+ data[i].is_del +"</td>" +
+                    "<td>"+ data[i].flirt +"</td>" +
+                    "<td class='none is_flirt'>"+ data[i].is_flirt +"</td>" +
+                    "<td class='opt'>+ opt +</td>" +
+                    "</tr>";
+            });
+            $("#groupListTable").html(tr);
+        }
     }, function (response) {
         layer.msg(response.errmsg);
     });
@@ -38,10 +61,10 @@ $(function () {
     $(document).on("click", ".editBtn", function () {
         $(".addSubBtn").addClass("none");
         $(".editSubBtn").removeClass("none");
-        group_id = $(this).parents("tr[role='row']").find(".id").text();//获取群id
-        var group_name = $(this).parents("tr[role='row']").find(".name").text();//获取群名称
-        var is_del = $(this).parents("tr[role='row']").find(".is_del").text();//获取是否运行状态
-        var is_flirt = $(this).parents("tr[role='row']").find(".is_flirt").text();//获取是否开启调戏功能
+        group_id = $(this).parents("tr").find(".id").text();//获取群id
+        var group_name = $(this).parents("tr").find(".name").text();//获取群名称
+        var is_del = $(this).parents("tr").find(".is_del").text();//获取是否运行状态
+        var is_flirt = $(this).parents("tr").find(".is_flirt").text();//获取是否开启调戏功能
         if (is_del == "1") {
             $("#runSwitch").addClass("active").val("1");
         } else {
