74dd01176374243e953e73c682622c33fb83310c
Edwin <1045416962@qq.com>
2018-12-13 13:28:29 +0800

    h5

--- h5/bot_web/js/group_member.js
+++ h5/bot_web/js/group_member.js
@@ -23,11 +23,12 @@ $(function () {
     });
 
     //获取群成员列表
-    var limit = 10, offset = 0, status = "-1";
+    var limit = 10, offset = 0, status = "-1", loading = "";
 
     function GetGroupMemberFun(token, limit, offset, status) {
         var tr = "", totalPage = "", count = "";
         GetGroupMember(token, group_id, limit, offset, status, function (response) {
+            layer.close(loading);
             if (response.errcode == "0") {
                 var data = response.rows;
                 totalPage = response.total;
@@ -54,11 +55,15 @@ $(function () {
                     prevPageText: "<<",
                     nextPageText: ">>",
                     callback: function (current) {
-                        GetGroupMemberFun(token, limit, (current - 1) * limit,status)
+                        GetGroupMemberFun(token, limit, (current - 1) * limit,status);
+                        loading = layer.load(1, {
+                            shade: [0.1,'#fff'] //0.1透明度的白色背景
+                        });
                     }
                 });
             }
         }, function (response) {
+            layer.close(loading);
             layer.msg(response.errmsg);
         })
     }
