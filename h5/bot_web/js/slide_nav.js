f33b9617ae9d0ca1468195f6de50676764d753b0
Edwin <1045416962@qq.com>
2018-12-14 18:11:22 +0800

    h5

--- h5/bot_web/js/slide_nav.js
+++ h5/bot_web/js/slide_nav.js
@@ -4,9 +4,8 @@ $.ajax({
     url: url + "/h5/bot_web/json/slide_nav.json",
     dataType: "json",
     success: function (res) {
-        console.log(res);
         $.each(res, function (i, val) {
-            li+="<li class='treeview-item'>" +
+            li+="<li class='treeview'>" +
                 "<a class='app-menu__item' href="+ res[i].href +"></a>" +
                 "<i class='app-menu__icon fa "+ res[i].fa_icon +"'></i>" +
                 "<span class='app-menu__label'>"+ res[i].name +"</span>" +
