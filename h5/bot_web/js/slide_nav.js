d1c6640bf836a3cb39dddb43c82af04a711b9deb
Edwin <1045416962@qq.com>
2018-12-14 18:06:30 +0800

    h5

--- h5/bot_web/js/slide_nav.js
+++ h5/bot_web/js/slide_nav.js
@@ -1,9 +1,17 @@
-var url = getRootPath();
+var url = getRootPath(),li = "";
 $.ajax({
     type: "GET",
     url: url + "/h5/bot_web/json/slide_nav.json",
-    dataType:"json",
+    dataType: "json",
     success: function (res) {
         console.log(res);
+        $.each(res, function (i, val) {
+            li+="<li>" +
+                "<a class='app-menu__item' href="+ res[i].href +"></a>" +
+                "<i class='app-menu__icon fa "+ data[i].fa_icon +"'></i>" +
+                "<span class='app-menu__label'>"+ data[i].name +"</span>" +
+                "</li>"
+        });
+        $(".app-menu").html(li);
     }
 });
