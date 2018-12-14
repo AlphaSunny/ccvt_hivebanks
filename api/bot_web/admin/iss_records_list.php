c40509fddbf196aab6c8d2ce35bbbdebbb1caf7c
songxingwei <you@example.com>
2018-12-14 13:53:32 +0800

    1

--- api/bot_web/admin/iss_records_list.php
+++ api/bot_web/admin/iss_records_list.php
@@ -60,6 +60,7 @@ $rtn_ary['errmsg'] = '';
 $rtn_ary['rows'] = $rows['rows'];
 $rtn_ary['all_amount'] = $rows['all_amount'];
 $rtn_ary['all_chat'] = $rows['all_chat'];
+$rtn_ary['total'] = $total;
 $rtn_str = json_encode($rtn_ary);
 php_end($rtn_str);
 
