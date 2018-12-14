9824e1231b4606e7722f9ae072180303dc87f146
Edwin <1045416962@qq.com>
2018-12-14 18:01:01 +0800

    h5

--- h5/bot_web/js/slide_nav.js
+++ h5/bot_web/js/slide_nav.js
@@ -2,6 +2,7 @@ var url = getRootPath();
 $.ajax({
     type: "POST",
     url: url + "/h5/bot_web/json/slide_nav.json",
+    dataType:"json",
     success: function (res) {
         console.log(res);
     }
