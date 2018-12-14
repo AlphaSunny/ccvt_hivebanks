2cd790d7609d657a31374938649f029c6f1b22df
Edwin <1045416962@qq.com>
2018-12-14 13:31:02 +0800

    h5

--- h5/bot_web/total_robot/js/send_amount.js
+++ h5/bot_web/total_robot/js/send_amount.js
@@ -3,40 +3,47 @@ $(function () {
     var token = GetCookie("total_robot_token");
 
 
-    var url = getRootPath();
+    // var url = getRootPath();
+    var limit = 10, offset = 0;
 
-    function GetAmount(start_time, end_time, nickname) {
-        $.ajax({
-            "url": url + "/api/bot_web/admin/iss_records_list.php?token=" + encodeURIComponent(token) + "&start_time=" + start_time + "&end_time=" + end_time + "&nickname=" + nickname,
-            "type": "GET",
-            success: function (data) {
-                $(".all_amount").text(data.all_amount);
-                $(".all_chat").text(data.all_chat);
-                $('#sendAmountTable').DataTable({
-                    order: [[3, "desc"]],
-                    destroy: true,
-                    deferRender: true,
-                    data: data.data,
-                    columns: [
-                        {"data": "wechat"},
-                        {"data": "amount"},
-                        {"data": "num"},
-                        {"data": "send_time"}
-                    ],
-                });
-            }
+    function GetAmountFun(start_time, end_time, nickname) {
+        var tr = "";
+        GetAmount(token, start_time, end_time, nickname, limit, offset, function (response) {
+            console.log(response);
+        }, function (response) {
+            layer.msg(response.errmsg, {icon: 2});
         });
+        // $.ajax({
+        //     "url": url + "/api/bot_web/admin/iss_records_list.php?token=" + encodeURIComponent(token) + "&start_time=" + start_time + "&end_time=" + end_time + "&nickname=" + nickname,
+        //     "type": "GET",
+        //     success: function (data) {
+        //         $(".all_amount").text(data.all_amount);
+        //         $(".all_chat").text(data.all_chat);
+        //         $('#sendAmountTable').DataTable({
+        //             order: [[3, "desc"]],
+        //             destroy: true,
+        //             deferRender: true,
+        //             data: data.data,
+        //             columns: [
+        //                 {"data": "wechat"},
+        //                 {"data": "amount"},
+        //                 {"data": "num"},
+        //                 {"data": "send_time"}
+        //             ],
+        //         });
+        //     }
+        // });
     }
 
     var start_time = "", end_time = "", nickname = "";
-    GetAmount(start_time, end_time, nickname);
+    GetAmountFun(start_time, end_time, nickname);
 
 
     $(".searchBtn").click(function () {
         start_time = $("#startTime").val();
         end_time = $("#endTime").val();
         nickname = $("#nickname").val();
-        GetAmount(start_time, end_time, nickname);
+        GetAmountFun(start_time, end_time, nickname);
     });
 
     //Set time
