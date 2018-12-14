432345588099a092e13f943fd722cf24dc507a7e
Edwin <1045416962@qq.com>
2018-12-14 11:26:15 +0800

    h5

--- h5/bot_web/total_robot/js/task_list.js
+++ h5/bot_web/total_robot/js/task_list.js
@@ -58,7 +58,7 @@ $(function () {
                         "<td class='content'>" + data[i].content + "</td>" +
                         "<td class='name'>" + data[i].name + "</td>" +
                         "<td>" +
-                        "<button class='btn-success btn-sm editBtn'><i class='fa fa-pencil' aria-hidden='true'></i>编辑</button>" +
+                        // "<button class='btn-success btn-sm editBtn'><i class='fa fa-pencil' aria-hidden='true'></i>编辑</button>" +
                         "<button class='btn-sm btn-danger delBtn margin-left-5'><i class='fa fa-trash' aria-hidden='true'></i>删除</button>" +
                         "</td>" +
                         "</tr>"
@@ -96,52 +96,52 @@ $(function () {
     }
 
     //编辑任务
-    $(document).on("click", ".editBtn", function () {
-        var group_name = $(this).parents("tr").find(".name").text();
-        var time = $(this).parents("tr").find(".time").text();
-        var content = $(this).parents("tr").find(".content").text();
-        var task_id = $(this).parents("tr").find(".id").text();
-        $("#selectGroupName").fadeOut("fast");
-        $("#timer_id").val(task_id);
-        $("#groupName").val(group_name);
-        $("#time").val(time);
-        $("#content").val(content);
-        $(".addSubBtn").addClass("none");
-        $(".editSubBtn").removeClass("none");
-        $("#editTaskModal").modal("show");
-    });
-
-    //确认编辑
-    $(".editSubBtn").click(function () {
-        var timer_id = $("#timer_id").val();
-        var time = $("#time").val();
-        var content = $("#content").val();
-        if (time.length <= 0) {
-            layer.msg("请输入时间");
-            return;
-        }
-        if (content.length <= 0) {
-            layer.msg("请输入内容");
-            return;
-        }
-        //loading
-        var loading = layer.load(1, {
-            shade: [0.1, '#fff'] //0.1透明度的白色背景
-        });
-        EditTask(token, timer_id, time, content, function (response) {
-            if (response.errcode == "0") {
-                layer.close(loading);
-                // GetTaskListFun();
-                $("#editTaskModal").modal("hide");
-                // window.location.reload();
-                table.ajax.reload();
-            }
-        }, function (response) {
-            layer.close(loading);
-            $("#editTaskModal").modal("hide");
-            layer.msg(response.errmsg);
-        })
-    });
+    // $(document).on("click", ".editBtn", function () {
+    //     var group_name = $(this).parents("tr").find(".name").text();
+    //     var time = $(this).parents("tr").find(".time").text();
+    //     var content = $(this).parents("tr").find(".content").text();
+    //     var task_id = $(this).parents("tr").find(".id").text();
+    //     $("#selectGroupName").fadeOut("fast");
+    //     $("#timer_id").val(task_id);
+    //     $("#groupName").val(group_name);
+    //     $("#time").val(time);
+    //     $("#content").val(content);
+    //     $(".addSubBtn").addClass("none");
+    //     $(".editSubBtn").removeClass("none");
+    //     $("#editTaskModal").modal("show");
+    // });
+    //
+    // //确认编辑
+    // $(".editSubBtn").click(function () {
+    //     var timer_id = $("#timer_id").val();
+    //     var time = $("#time").val();
+    //     var content = $("#content").val();
+    //     if (time.length <= 0) {
+    //         layer.msg("请输入时间");
+    //         return;
+    //     }
+    //     if (content.length <= 0) {
+    //         layer.msg("请输入内容");
+    //         return;
+    //     }
+    //     //loading
+    //     var loading = layer.load(1, {
+    //         shade: [0.1, '#fff'] //0.1透明度的白色背景
+    //     });
+    //     EditTask(token, timer_id, time, content, function (response) {
+    //         if (response.errcode == "0") {
+    //             layer.close(loading);
+    //             // GetTaskListFun();
+    //             $("#editTaskModal").modal("hide");
+    //             // window.location.reload();
+    //             table.ajax.reload();
+    //         }
+    //     }, function (response) {
+    //         layer.close(loading);
+    //         $("#editTaskModal").modal("hide");
+    //         layer.msg(response.errmsg);
+    //     })
+    // });
 
     //添加信息
     // $(".addTaskBtn").click(function () {
