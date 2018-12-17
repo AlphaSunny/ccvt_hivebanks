d05720616e2536250707617d116e233d8f8672a8
Edwin <1045416962@qq.com>
2018-12-17 14:05:48 +0800

    h5

--- h5/bot_web/js/task_list.js
+++ h5/bot_web/js/task_list.js
@@ -148,7 +148,7 @@ $(function () {
     });
 
     //选择图片
-    $("#upload_image").on("change",function () {
+    $("#file").on("change",function () {
         console.log("change");
         var formDate = new FormData($("#upload_image")[0]);
         console.log(formDate);
