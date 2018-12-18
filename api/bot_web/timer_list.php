34ee2b88a46e90966ccfd1c8dde676e9acad7f3c
Edwin <1045416962@qq.com>
2018-12-18 13:40:49 +0800

    h5

--- api/bot_web/timer_list.php
+++ api/bot_web/timer_list.php
@@ -36,6 +36,7 @@ $rtn_ary['errcode'] = '0';
 $rtn_ary['errmsg'] = '';
 $rtn_ary['count'] = count($rows);
 //$rtn_ary['data'] = $rows;
+$rtn_ary['rows'] = $rows;
 $rtn_str = json_encode($rtn_ary);
 php_end($rtn_str);
 
