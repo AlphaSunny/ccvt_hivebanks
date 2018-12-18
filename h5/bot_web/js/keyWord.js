571510734a41902df096c64b8c7917dd8b8cf83e
Edwin <1045416962@qq.com>
2018-12-18 09:46:55 +0800

    h5

--- h5/bot_web/js/keyWord.js
+++ h5/bot_web/js/keyWord.js
@@ -1,10 +1,12 @@
 $(function () {
 
-    //编辑-添加-显示弹框
+    //添加-显示弹框
     $(".add_key_word_btn").click(function () {
         $("#myModalLabel").text("添加AI关键字");
         $("#keyWordModal").modal("show");
     });
+
+    // 编辑-
     $(".edit_key_word_btn").click(function () {
         var key_word = $(this).parents("tr").find(".key_word").text();
         var key_word_content = $(this).parents("tr").find(".key_word_content").text();
