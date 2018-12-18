30545e9f612078653daedb1821ae2b88a5c2a511
Edwin <1045416962@qq.com>
2018-12-18 14:16:18 +0800

    h5

--- h5/bot_web/js/task_list.js
+++ h5/bot_web/js/task_list.js
@@ -105,7 +105,7 @@ $(function () {
             $(".content_text").fadeOut(300);
             $(".content_image").fadeIn(300);
             $(".upload_img_box").fadeIn(300);
-            $("#upload_image").attr("src", content);
+            $("#upload_img").attr("src", content);
         }
 
         $("#selectGroupName").fadeOut("fast");
