25fb8e4e6ffe33ba27adca43a59b5012b0350aa6
Edwin <1045416962@qq.com>
2018-12-18 17:21:02 +0800

    h5

--- h5/honor/js/chat_person.js
+++ h5/honor/js/chat_person.js
@@ -34,6 +34,8 @@ $(function () {
             shade: [0.1, '#fff']
         });
         GetChatPerson(wechat, group_id, search_content, function (response) {
+            console.log(wechat);
+            console.log(group_id);
             layer.close(index);
             if (response.errcode == "0") {
                 var data = response.rows;
