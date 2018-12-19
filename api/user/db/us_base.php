0fb105431ba36b12272d948ce8d1689f33670ca1
songxingwei <you@example.com>
2018-12-19 18:29:03 +0800

    1

--- api/user/db/us_base.php
+++ api/user/db/us_base.php
@@ -202,6 +202,17 @@ function send_to_us_ccvt($us_id,$type,$money,$why,$flag)
 
 }
 
+//获取用户可用余额
+function get_us_available_account($us_id){
+    $db = new DB_COM();
+    $sql = "select base_amount from us_base WHERE us_id='{$us_id}' limit 1";
+    $db->query($sql);
+    $base_amount = $db -> getField($sql,'base_amount');
+    if($base_amount == null)
+        return 0;
+    return $base_amount;
+}
+
 
 //获取用户余额
 function get_us_account($us_id){
@@ -562,7 +573,7 @@ function send_to_us_ccvt_voucher($us_id,$voucher,$flag,$why,$type)
 function  check_us_amount($us_id,$account)
 {
     $unit = get_la_base_unit();
-    $us_account = get_us_account($us_id)/$unit;
+    $us_account = get_us_available_account($us_id)/$unit;
     if ($us_account<$account){
         return false;
     }
@@ -880,5 +891,152 @@ function black_judge($us_nm){
     }
 
     return false;
+
+}
+
+
+//======================================
+// 函数: 判断是否存在ccvt用户账号
+// 参数: account          账号
+// 返回:
+//======================================
+function  check_us_account($account)
+{
+    $db = new DB_COM();
+    $sql = "SELECT * FROM us_base where us_account = '{$account}' limit 1";
+    $db -> query($sql);
+    $row = $db -> fetchRow();
+    return $row;
+}
+//======================================
+// 函数: 用户转账
+// 参数: $data        内容
+// 返回:
+//======================================
+function  us_send_ccvt_record($data)
+{
+    //转账
+    us_send_ccvt($data['us_id'],$data['trans_us_id'],$data['num'],'13',"用户转账");
+    return true;
+}
+
+//======================================
+// 函数: ba转账(ccvt)
+// 参数: data        信息数组
+// 返回: true         创建成功
+//       false        创建失败
+//======================================
+function us_send_ccvt($us_id,$trans_us_id,$money,$flag,$why)
+{
+    $db = new DB_COM();
+
+    $pInTrans = $db->StartTrans();  //开启事务
+    //us加钱
+    $unit = get_la_base_unit();
+    $sql = "update us_base set base_amount=base_amount+'{$money}'*'{$unit}' WHERE us_id='{$trans_us_id}'";
+    $db -> query($sql);
+    if (!$db->affectedRows()){
+        $db->Rollback($pInTrans);
+        return false;
+    }
+
+    //us减钱
+    $sql = "update us_base set base_amount=base_amount-'{$money}'*'{$unit}' WHERE us_id='{$us_id}'";
+    $db -> query($sql);
+    if (!$db->affectedRows()){
+        $db->Rollback($pInTrans);
+        return false;
+    }
+
+    /******************************转账记录表***************************************************/
+    //增币记录  赠送者
+    $data['hash_id'] = hash('md5', $us_id . $flag . get_ip() . time() . rand(1000, 9999) . date('Y-m-d H:i:s'));
+    $prvs_hash = get_transfer_pre_hash($us_id);
+    $data['prvs_hash'] = $prvs_hash == 0 ? $data['hash_id'] : $prvs_hash;
+    $data['credit_id'] = $us_id;
+    $data['debit_id'] = $trans_us_id;
+    $data['tx_amount'] = $money*$unit;
+    $data['credit_balance'] = get_us_account($us_id)-$data['tx_amount'];
+    $data['tx_hash'] = hash('md5', $us_id . $flag . get_ip() . time() . date('Y-m-d H:i:s'));
+    $data['flag'] = $flag;
+    $data['transfer_type'] = 'us-us';
+    $data['transfer_state'] = 1;
+    $data['tx_detail'] = $why;
+    $data['give_or_receive'] = 1;
+    $data['ctime'] = time();
+    $data['utime'] = date('Y-m-d H:i:s',time());
+    $sql = $db->sqlInsert("com_transfer_request", $data);
+    $id = $db->query($sql);
+    if (!$id){
+        $db->Rollback($pInTrans);
+        return false;
+    }
+
+    //接收者
+    $dat['hash_id'] = hash('md5', $trans_us_id . $flag . get_ip() . time() . rand(1000, 9999) . date('Y-m-d H:i:s'));
+    $prvs_hash = get_transfer_pre_hash($trans_us_id);
+    $dat['prvs_hash'] = $prvs_hash == 0 ? $data['hash_id'] : $prvs_hash;
+    $dat['credit_id'] = $trans_us_id;
+    $dat['debit_id'] = $us_id;
+    $dat['tx_amount'] = $money*$unit;
+    $dat['credit_balance'] = get_us_account($trans_us_id)+$dat['tx_amount'];
+    $dat['tx_hash'] = hash('md5', $trans_us_id . $flag . get_ip() . time() . date('Y-m-d H:i:s'));
+    $dat['flag'] = $flag;
+    $dat['transfer_type'] = 'us-us';
+    $dat['transfer_state'] = 1;
+    $dat['tx_detail'] = $why;
+    $dat['give_or_receive'] = 2;
+    $dat['ctime'] = time();
+    $dat['utime'] = date('Y-m-d H:i:s',time());;
+    $sql = $db->sqlInsert("com_transfer_request", $dat);
+    $id = $db->query($sql);
+    if (!$id){
+        $db->Rollback($pInTrans);
+        return false;
+    }
+
+    /***********************资金变动记录表***********************************/
+    //us添加基准资产变动记录
+    $us_type = 'us_reg_send_balance';
+    $ctime = date('Y-m-d H:i:s');
+    $com_balance_us['hash_id'] = hash('md5', $us_id . $us_type . get_ip() . time() . rand(1000, 9999) . $ctime);
+    $com_balance_us['tx_id'] = $data['tx_hash'];
+    $prvs_hash = get_recharge_pre_hash($us_id);
+    $com_balance_us['prvs_hash'] = $prvs_hash == 0 ? $com_balance_us['hash_id'] : $prvs_hash;
+    $com_balance_us["credit_id"] = $us_id;
+    $com_balance_us["debit_id"] = $trans_us_id;
+    $com_balance_us["tx_type"] = "us_us_transfer_out";
+    $com_balance_us["tx_amount"] = $money*$unit;
+    $com_balance_us["credit_balance"] = get_us_account($us_id)-$com_balance_us["tx_amount"];
+    $com_balance_us["utime"] = time();
+    $com_balance_us["ctime"] = $ctime;
+    $sql = $db->sqlInsert("com_base_balance", $com_balance_us);
+    if (!$db->query($sql)) {
+        $db->Rollback($pInTrans);
+        return false;
+    }
+
+    //us添加基准资产变动记录
+    $us_type = 'ba_reg_send_balance';
+    $com_balance_ba['hash_id'] = hash('md5', $trans_us_id. $us_type . get_ip() . time() . rand(1000, 9999) . $ctime);
+    $com_balance_ba['tx_id'] = $data['tx_hash'];
+    $prvs_hash = get_recharge_pre_hash($trans_us_id);
+    $com_balance_ba['prvs_hash'] = $prvs_hash == 0 ? $com_balance_ba['hash_id'] : $prvs_hash;
+    $com_balance_ba["credit_id"] = $trans_us_id;
+    $com_balance_ba["debit_id"] = $us_id;
+    $com_balance_ba["tx_type"] = "us_us_transfer_in";
+    $com_balance_ba["tx_amount"] = $money*$unit;
+    $com_balance_ba["credit_balance"] = get_ba_account($trans_us_id)+$com_balance_ba["tx_amount"];
+    $com_balance_ba["utime"] = time();
+    $com_balance_ba["ctime"] = $ctime;
+    $sql = $db->sqlInsert("com_base_balance", $com_balance_ba);
+    if (!$db->query($sql)) {
+        $db->Rollback($pInTrans);
+        return false;
+    }
+
+    $db->Commit($pInTrans);
+    return true;
+
 
 }
