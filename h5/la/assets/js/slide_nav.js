cc24fb7758c7a73f8e46f0dbbf52b47ec470b9f3
Edwin <1045416962@qq.com>
2018-12-21 16:05:05 +0800

    h5

--- h5/la/assets/js/slide_nav.js
+++ h5/la/assets/js/slide_nav.js
@@ -1,2 +1,10 @@
 var url = getRootPath();
 console.log(url);
+$.ajax({
+    type: "GET",
+    dataType: "json",
+    url: url + "/la/assets/json/slide_nav.json",
+    success:function (response) {
+        console.log(response);
+    }
+});
