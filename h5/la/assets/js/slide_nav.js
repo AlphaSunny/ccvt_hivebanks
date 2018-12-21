038558345803ae03b52f0df9bbe4a47b6d670c2d
Edwin <1045416962@qq.com>
2018-12-21 16:09:51 +0800

    h5

--- h5/la/assets/js/slide_nav.js
+++ h5/la/assets/js/slide_nav.js
@@ -3,7 +3,7 @@ console.log(url);
 $.ajax({
     type: "GET",
     dataType: "json",
-    url: url + "/la/assets/json/slide_nav.json",
+    url: url + "/h5/la/assets/json/slide_nav.json",
     success:function (response) {
         console.log(response);
     }
