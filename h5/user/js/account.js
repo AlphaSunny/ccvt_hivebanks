9992a8407a44de96d2c480022d5adb1329735970
Edwin <1045416962@qq.com>
2018-12-22 14:59:26 +0800

    h5

--- h5/user/js/account.js
+++ h5/user/js/account.js
@@ -213,8 +213,8 @@ $(function () {
 
         $('#qrcode').qrcode({
             text: url,
-            width: 100,
-            height: 100
+            width: 200,
+            height: 200
         });
 
         //get canvas qr
