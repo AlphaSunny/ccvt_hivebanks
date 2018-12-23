513c0a22d2afbe7cccc6596789fb69cb5fa64675
Edwin <1045416962@qq.com>
2018-12-23 14:25:24 +0800

    h5

--- h5/chat_recode/js/chat_recode.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    console.log(height);
-    $('html,body').scrollTop(999999999);
-});
