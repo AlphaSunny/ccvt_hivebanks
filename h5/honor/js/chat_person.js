7faa5206203fc3ebd262877f1858db4cc04923b9
Edwin <1045416962@qq.com>
2018-12-23 15:46:55 +0800

    h5

--- h5/honor/js/chat_person.js
+++ h5/honor/js/chat_person.js
@@ -44,8 +44,11 @@ $(function () {
                 }
 
                 if (data.length <= 0 && offset <= 0) {
-                    console.log(data.length+"ggg");
-                    $('.chat_content').html("<h1 style='text-align: center;color:#000000'>暂无聊天内容</h1>").css("height", "100%");
+                    console.log(data.length + "ggg");
+                    $(".chat_content").prepend("<h1 style='text-align: center;color:#000000'>暂无聊天内容</h1>").css("height", "100%");
+                    $(".none_weChat,.chat_item_ul").fadeOut();
+                } else {
+                    $(".none_weChat,.chat_item_ul").fadeOut();
                 }
                 if (data.length <= 9) {
                     $('.chat_content').css("height", "100%");
