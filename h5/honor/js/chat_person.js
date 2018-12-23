e974e25c7d797ba3b82089891bc97293e3939bd9
Edwin <1045416962@qq.com>
2018-12-23 15:00:41 +0800

    h5

--- h5/honor/js/chat_person.js
+++ h5/honor/js/chat_person.js
@@ -45,7 +45,7 @@ $(function () {
                 if (data.length <= 0 && offset <= 0) {
                     $('.chat_content').html("<h1 style='text-align: center;color:#ffffff'>暂无聊天内容</h1>").css("height", "100%");
                 }
-                if(data.length <= 9){
+                if(total <= 9){
                     $('.chat_content').css("height", "100%");
                 }
 
