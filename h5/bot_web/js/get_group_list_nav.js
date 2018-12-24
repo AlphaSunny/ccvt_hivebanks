14011162a256b6e639aa24d625ac774e7a40c5ad
Edwin <1045416962@qq.com>
2018-12-24 12:35:09 +0800

    h5

--- h5/bot_web/js/get_group_list_nav.js
+++ h5/bot_web/js/get_group_list_nav.js
@@ -24,7 +24,7 @@ $(function () {
     GetGroupListNav();
 
     //是否是group_member页面
-    var is_group_member  = window.location.href;
+    var is_group_member  = window.location.hostname;
     console.log(is_group_member);
     var reg = new RegExp("group_member.html");
     console.log(reg.exec(is_group_member));
