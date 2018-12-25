14bc5f8c4a0e565eac474f1cc3de126374890487
pool <epsilonsunny@gmai.com>
2018-12-25 10:28:23 +0800

    on

--- h5/ba/js/BaWithdrawInfo.js
+++ h5/ba/js/BaWithdrawInfo.js
@@ -5,11 +5,11 @@ $(function () {
     GetWithdrawInfo(token, function (response) {
         if(response.errcode == '0'){
             var data = response.rows;
-            $('.tx_hash').text(data[0].tx_hash);
-            $('.asset_id').text(data[0].asset_id);
-            $('.bit_amount').text(data[0].bit_amount);
-            $('.tx_time').text(data[0].tx_time);
-            $('.bit_address').text(data[0].bit_address);
+            $('.tx_hash').text(data.tx_hash);
+            $('.asset_id').text(data.asset_id);
+            $('.bit_amount').text(data.bit_amount);
+            $('.tx_time').text(data.tx_time);
+            $('.bit_address').text(data.bit_address);
             if(data.qa_flag == '0'){
                 $('.qa_flag').attr('name', 'enablePending');
             }
