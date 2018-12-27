5433300a0eac368a357b792b29ca97fbbd27647f
Edwin <1045416962@qq.com>
2018-12-27 17:06:26 +0800

    h5

--- h5/twoUp/js/test2.js
+++ h5/twoUp/js/test2.js
@@ -2,7 +2,7 @@ $(function () {
     // var url = "test.json";
     var url_path = window.location.hostname;
     console.log(url_path);
-    var url = url_path + "/api/crontab/get_scale_us_data.php";
+    var url = "https://" + url_path + "/api/crontab/get_scale_us_data.php";
     console.log(url);
     var letter_arr = [], one_list = [], two_list = [], type_one = "1", type_two = "2";
     var text_timer = "";
