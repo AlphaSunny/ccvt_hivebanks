e868b5ee9fced6064ffd593cf56bf8d3a61d365a
songxingwei <you@example.com>
2018-12-28 16:11:33 +0800

    1

--- api/crontab/2.php
+++ api/crontab/2.php
@@ -1,9 +1,23 @@
 <?php
+//$redis = new Redis();
+//
+//$redis->connect('18.219.17.238', 6379); //连接Redis
+//$redis->auth('Windwin2018'); //密码验证
+//$redis->select(0);//选择数据库2
+//$redis->set( "testKey" , "Hello Redis"); //设置测试key
+//echo $redis->get("testKey");//输出value
+
+
+
+$ip = "18.219.17.238";
+$port = 6379;
 $redis = new Redis();
-print_r($redis);
-$redis->connect('18.219.17.238', 6379); //连接Redis
-$redis->auth('Windwin2018'); //密码验证
-$redis->select(0);//选择数据库2
-$redis->set( "testKey" , "Hello Redis"); //设置测试key
-echo $redis->get("testKey");//输出value
+
+$redis->pconnect($ip, $port, 1);
+$key = "test";
+$value = "this is test";
+
+$redis->set($key, $value);
+$d = $redis->get($key);
+var_dump($d);
 ?>
