74b4ed10ab15fb2a9569ce5163a272f20a9ea999
songxingwei <you@example.com>
2018-12-28 11:02:50 +0800

    1

--- api/crontab/group_upgrade.php
+++ api/crontab/group_upgrade.php
@@ -9,13 +9,14 @@ error_reporting(E_ALL | E_STRICT);
 
 $db = new DB_COM();
 //群
-$sql = "select id,name from bot_group as gr where gr.scale=1 and (select count(*) from us_bind where bind_name='group' and bind_info=gr.id)>=10 
+$sql = "select id,name,(select count(*) from us_bind where bind_name='group' and bind_info=gr.id) as bind_count,(select count(*) from us_base where scale=1 and us_id in (select us_id from us_bind where bind_name='group' and bind_info=gr.id)) as one_scale_user_count 
+from bot_group as gr where gr.scale=1 and (select count(*) from us_bind where bind_name='group' and bind_info=gr.id)>=10 
 and (select count(*) from us_base where scale=1 and us_id in (select us_id from us_bind where bind_name='group' and bind_info=gr.id))>=2;";
 $db->query($sql);
 $group_rows = $db->fetchAll();
-foreach ($group_rows as $k=>$v){
-   
-}
+//foreach ($group_rows as $k=>$v){
+//    scale_upgrade($v['id'],1,2,$v['bind_count'],$v['one_scale_user_count']);
+//}
 
 print_r(json_encode($group_rows));die;
 
@@ -23,7 +24,7 @@ print_r(json_encode($group_rows));die;
 
 echo "OK";
 
-function scale_upgrade($us_id,$before_scale,$after_scale,$scale){
+function scale_upgrade($group_id,$before_scale,$after_scale,$bind_count,$one_scale_user_count){
         $db = new DB_COM();
         $pInTrans = $db->StartTrans();  //开启事务
         //升级记录表
