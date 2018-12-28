80301a92c77980d1338d0572a57c37bd11148412
songxingwei <you@example.com>
2018-12-28 16:07:22 +0800

    1

--- api/crontab/2.php
+++ api/crontab/2.php
@@ -2,7 +2,7 @@
 $redis = new Redis();
 $redis->connect('18.219.17.238', 6379); //连接Redis
 $redis->auth('mypasswords123sdfeak'); //密码验证
-$redis->select(2);//选择数据库2
+$redis->select(0);//选择数据库2
 $redis->set( "testKey" , "Hello Redis"); //设置测试key
 echo $redis->get("testKey");//输出value
 ?>
