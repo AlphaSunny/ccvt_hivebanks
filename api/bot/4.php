cc7073c8eaedfc0cbd9891f8b4d1924fccca2053
songxingwei <you@example.com>
2019-01-02 15:28:11 +0800

    1

--- api/bot/4.php
+++ api/bot/4.php
@@ -1,11 +1,14 @@
 <?php
-
+require_once '../inc/common.php';
+php_begin();
 
 //$args = array("content");
 //chk_empty_args('POST', $args);
 //$data = array();
 echo 222;die;
 
+$content  = get_arg_str('POST', 'content', 999999999);
+print_r($content);
 
 
 
