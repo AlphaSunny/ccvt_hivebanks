0111cd5d78a64594ae7f57cb218d5b16f331888e
songxingwei <you@example.com>
2019-01-02 16:00:19 +0800

    1

--- api/bot/4.php
+++ api/bot/4.php
@@ -6,8 +6,8 @@ $args = array("content");
 chk_empty_args('POST', $args);
 $data = array();
 
-$content  = get_arg_str('POST', 'content', 999999999);
-print_r($content);
+$content  = $_POST['content'];
+
 print_r(json_decode($content));
 
 
