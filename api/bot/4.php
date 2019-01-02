4048c1669a423df8292310326c21b64eda37c981
songxingwei <you@example.com>
2019-01-02 15:19:59 +0800

    1

---
+++ api/bot/4.php
@@ -0,0 +1,16 @@
+<?php
+
+php_begin();
+
+$args = array("content");
+chk_empty_args('POST', $args);
+$data = array();
+
+
+$content  = get_arg_str('POST', 'content', 999999999);
+print_r($content);
+
+
+
+
+?>
