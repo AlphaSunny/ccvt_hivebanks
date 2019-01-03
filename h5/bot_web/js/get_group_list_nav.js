b4890d3bcf65f344b56eca144a0475a2703f4524
Edwin <1045416962@qq.com>
2019-01-03 17:31:52 +0800

    h5

--- h5/bot_web/js/get_group_list_nav.js
+++ h5/bot_web/js/get_group_list_nav.js
@@ -22,8 +22,8 @@ $(function () {
                 // group_id = data[0].id;
                 $.each(data, function (i, val) {
                     li += "<li>" +
-                        // "<a class='app-menu__item to_group_link' id='" + data[i].id + "' is_admin_del='" + data[i].is_admin_del + "' is_del='" + data[i].is_del + "' is_flirt='" + data[i].is_flirt + "' is_give_ccvt='" + data[i].is_give_ccvt + "' bind_account_notice='" + data[i].bind_account_notice + "' is_welcome='" + data[i].is_welcome + "' send_address='" + data[i].send_address + "' welcome='" + data[i].welcome + "' group_name='" + data[i].name + "' href='javascript:;'>" +
-                        "<a class='app-menu__item to_group_link' id='" + data[i].id + "' href='javascript:;'>" +
+                        "<a class='app-menu__item to_group_link' id='" + data[i].id + "' is_admin_del='" + data[i].is_admin_del + "' is_del='" + data[i].is_del + "' is_flirt='" + data[i].is_flirt + "' is_give_ccvt='" + data[i].is_give_ccvt + "' bind_account_notice='" + data[i].bind_account_notice + "' is_welcome='" + data[i].is_welcome + "' send_address='" + data[i].send_address + "' welcome='" + data[i].welcome + "' group_name='" + data[i].name + "' href='javascript:;'>" +
+                        // "<a class='app-menu__item to_group_link' id='" + data[i].id + "' href='javascript:;'>" +
                         "<i class='app-menu__icon fa fa-circle-o'></i>" +
                         "<span class='app-menu__label'>" + data[i].name + "</span>" +
                         "</a>" +
