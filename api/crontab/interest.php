0228ebfe8c3fa30d06a92c58e2e9dd2151847536
范助国 <ahino@fanzhuguodeMacBook-Pro.local>
2019-01-03 12:20:16 +0800

    test

--- api/crontab/interest.php
+++ api/crontab/interest.php
@@ -75,7 +75,7 @@ function ba_cut($amount){
 }
 function us_add($amount,$us_id){
     $db = new DB_COM();
-    $sql = "update us_base set base_amount = base_amount+ $amount  *".UNIT." where us_id='{$us_id}'";
+    $sql = "update us_base set base_amount = base_amount+ $amount * 0.01 *".UNIT." where us_id='{$us_id}'";
     $db->query($sql);
     if($db->affectedRows())
         return true;
