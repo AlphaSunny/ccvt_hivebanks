f92504b84e75fda46bb36c9236f1509c0b4fe8a5
范助国 <fanzhuguo@fanzhuguodeMacBook-Pro.local>
2019-01-07 13:17:11 +0800

    test

--- api/crontab/dynamic_tuning.php
+++ api/crontab/dynamic_tuning.php
@@ -62,7 +62,7 @@ function lock_cut()
             die('lock_cut----'.$v);
 
         if(!ba_add($v['amount']))
-            die('ba_add'.$v['phone']);die('1');
+            die('ba_add'.$v['phone']);
         if(!cut_log_com_base($v['us_id'],$v['amount']))
             die('log_com_base'.$v['phone']);die('2');
         if(!cut_log_com_transfer($v['us_id'],$v['amount']))
