685baf0aac300b4dce635b76653c8ea26c8e0ba4
范助国 <fanzhuguo@fanzhuguodeMacBook-Pro.local>
2019-01-07 12:00:31 +0800

    test

--- api/crontab/dynamic_tuning.php
+++ api/crontab/dynamic_tuning.php
@@ -27,14 +27,13 @@ function dynamic_tuning()
     staff_cut();
 
     //增加当月员工金额
-    staff_add();
+//    staff_add();
 }
 
 function staff_cut()
 {
     if(lock_cut())
         staff_add();
-
 }
 
 //========================
@@ -95,21 +94,6 @@ function ba_add($amount)
     return false;
 }
 
-function ba_cut($amount)
-{
-    $db = new DB_COM();
-    $sql = "update ba_base set base_amount = base_amount - $amount*".UNIT." where  ba_id= '6C69520E-E454-127B-F474-452E65A3EE75'";
-    $db->query($sql);
-    $res = $db->affectedRows();
-    if($res)
-    {
-        return true;
-    }
-    die('ba_cut');
-    return false;
-}
-
-
 function cut_log_com_base($us_id,$amount){
     $db = new DB_COM();
     $data = array();
@@ -123,7 +107,7 @@ function cut_log_com_base($us_id,$amount){
     $data['prvs_hash'] = $data['prvs_hash'] == 0 ? $data['hash_id'] : $data['prvs_hash'];
     $data['debit_id'] = $us_id;
     $data['credit_id']= BA_ID;
-    $data['tx_type'] = 'big_us_lock';
+    $data['tx_type'] = 'gone_staff';
     $data["tx_amount"] = $amount*UNIT;
     $data["credit_balance"] = get_ba_account(BA_ID)+($amount*UNIT);
     $data["utime"] = time();
@@ -165,9 +149,9 @@ function cut_log_com_transfer($us_id,$amount){
     $data['credit_balance'] = get_ba_account(BA_ID)+($amount*UNIT);
     $data['tx_hash'] = hash('md5', BA_ID . FLAG . get_ip() . mt() . date('Y-m-d H:i:s'));
     $data['flag'] = FLAG;
-    $data['transfer_type'] = 'ba-us';
+    $data['transfer_type'] = 'us-ba';
     $data['transfer_state'] = 2;
-    $data['tx_detail'] = '分配计划';
+    $data['tx_detail'] = '离职回收';
     $data['give_or_receive'] = 1;
     $data['ctime'] = time();
     $data['utime'] = date('Y-m-d H:i:s',time());
@@ -182,9 +166,9 @@ function cut_log_com_transfer($us_id,$amount){
     $dat['credit_balance'] = get_us_account($us_id)-$dat['tx_amount'];
     $dat['tx_hash'] = hash('md5', $us_id . FLAG . get_ip() . mt() . date('Y-m-d H:i:s'));
     $dat['flag'] = FLAG;
-    $dat['transfer_type'] = 'ba-us';
+    $dat['transfer_type'] = 'us-ba';
     $dat['transfer_state'] = 1;
-    $dat['tx_detail'] = '分配计划';
+    $dat['tx_detail'] = '离职回收';
     $dat['give_or_receive'] = 1;
     $dat['ctime'] = time();
     $dat['utime'] = date('Y-m-d H:i:s',time());
@@ -200,7 +184,7 @@ function cut_log_com_transfer($us_id,$amount){
 //===============================
 //===== =======   ======   ======
 //==== = ====== === ==== === ====
-//=== === ===== ==== === ==== ====
+//=== === ===== ==== === ==== ===
 //==       ==== ==== === ==== ===
 //= ======= === ==== === == =====
 // ========= ==    =====   ======
@@ -211,7 +195,6 @@ function cut_log_com_transfer($us_id,$amount){
 
 function staff_add()
 {
-
     lock_add();
     ba_cut();
     log_com_base();
@@ -220,9 +203,163 @@ function staff_add()
 }
 
 function lock_add(){
+    $db = new DB_COM();
+    //获取利息用户
+    $data = us_get();
+    $flag = 1;
+    //发放利息===BA扣除利息====US增加利息====利息记录表====COMBASE记录表====COMTRANSFER记录表
+    foreach ($data as $k=>$v)
+    {
+        $phone = $v['phone'];
+        $amount = $v['amount'];
+        $sql = "select us_id from us_bind  where SUBSTR(bind_info,4,100) = {$phone}";
+        $db->query($sql);
+        $res = $db->fetchRow();
+        $us_id = $res['us_id'];
+
+        if(!(ba_cut($amount)&&add_log_com_base($us_id,$amount)&&add_log_transfer($us_id,$amount)&&us_add($amount,$us_id)&&log_lock($phone,$amount)))
+            die('failed'.$flag);
+        $flag++;
+    }
+    die('success'.$flag);
+}
+
+function us_get(){
+
+    return [
+        ['phone'=>18137802080,'amount'=>1100],//edwin
+        ['phone'=>18321709102,'amount'=>800],//bruce
+        ['phone'=>15248165523,'amount'=>700],//lilian
+        ['phone'=>13816129726,'amount'=>200],//lvy
+        ['phone'=>15601607210,'amount'=>100],//alan
+        ['phone'=>15221563385,'amount'=>1100],//sunny
+        ['phone'=>15801075991,'amount'=>1100],//gavin
+        ['phone'=>15988394267,'amount'=>600],//karen
+        ['phone'=>13901602243,'amount'=>900],//chan
+    ];
 
 }
 
+function ba_cut($amount){
+    $db = new DB_COM();
+    $sql = "update ba_base set base_amount = base_amount - $amount *".UNIT." where  ba_id= '6C69520E-E454-127B-F474-452E65A3EE75'";
+    echo '1';
+    $db->query($sql);
+    $res = $db->affectedRows();
+    if($res)
+    {
+        return true;
+    }
+    die('ba_cut');
+    return false;
+}
+function us_add($amount,$us_id){
+    $db = new DB_COM();
+    $sql = "update us_base set lock_amount = lock_amount+ $amount *".UNIT." where us_id='{$us_id}'";
+    $db->query($sql);
+    if($db->affectedRows())
+        return true;
+    die('us_add');
+    return false;
+}
+
+
+function log_lock($phone,$amount){
+    $db = new DB_COM();
+    $sql = "update big_account_lock set amount = amount+$amount  where phone = $phone";
+    $db->query($sql);
+    if($db->affectedRows())
+        return true;
+    die('log_info');
+    return false;
+}
+
+function add_log_com_base($us_id,$amount){
+    $db = new DB_COM();
+    $data = array();
+    $us_type = 'big_us_lock';
+    $ctime = date('Y-m-d H:i:s');
+
+    $data['hash_id'] = hash('md5', BA_ID . $us_type . get_ip() . mt() . rand(1000, 9999) . $ctime);
+    $data['tx_id'] = hash('md5', BA_ID  . 'phone' . get_ip() . mt() . date('Y-m-d H:i:s'));;
+    $data['prvs_hash'] = get_recharge_pre_hash(BA_ID);
+
+    $data['prvs_hash'] = $data['prvs_hash'] == 0 ? $data['hash_id'] : $data['prvs_hash'];
+    $data['debit_id'] = $us_id;
+    $data['credit_id']= BA_ID;
+    $data['tx_type'] = 'dynamic_tuning';
+    $data["tx_amount"] = $amount*UNIT;
+    $data["credit_balance"] = get_ba_account(BA_ID)-($amount*UNIT);
+    $data["utime"] = time();
+    $data["ctime"] = $ctime;
+    $sql = $db->sqlInsert("com_base_balance", $data);
+
+    $uata = array();
+    $uata['hash_id'] = hash('md5', $us_id . $us_type . get_ip() . mt() . rand(1000, 9999) . $ctime);
+    $uata['tx_id'] = hash('md5', BA_ID  . 'phone' . get_ip() . mt() . date('Y-m-d H:i:s'));;
+    $uata['prvs_hash'] = get_recharge_pre_hash($us_id);
+    $uata['prvs_hash'] = $uata['prvs_hash'] == 0 ? $uata['hash_id'] : $uata['prvs_hash'];
+
+    $uata['credit_id'] = $us_id;
+    $uata['debit_id']= BA_ID;
+    $uata['tx_type'] = 'dynamic_tuning';
+    $uata["tx_amount"] = $amount*UNIT;
+    $uata["credit_balance"] = get_us_account($us_id)+($amount*UNIT);
+    $uata["utime"] = time();
+    $uata["ctime"] = $ctime;
+    $uql = $db->sqlInsert("com_base_balance", $uata);
+
+    if($db->query($sql)&&$db->query($uql))
+        return true;
+    die('com_base');
+    return false;
+
+
+}
+
+function add_log_transfer($us_id,$amount){
+
+    $db = new DB_COM();
+    $data['hash_id'] = hash('md5', BA_ID . FLAG . get_ip() . mt() . rand(1000, 9999) . date('Y-m-d H:i:s'));
+    $prvs_hash = get_transfer_pre_hash(BA_ID);
+    $data['prvs_hash'] = $prvs_hash == 0 ? $data['hash_id'] : $prvs_hash;
+    $data['credit_id'] = BA_ID;
+    $data['debit_id'] = $us_id;
+    $data['tx_amount'] = $amount*UNIT;
+    $data['credit_balance'] = get_ba_account(BA_ID)-($amount*UNIT);
+    $data['tx_hash'] = hash('md5', BA_ID . FLAG . get_ip() . mt() . date('Y-m-d H:i:s'));
+    $data['flag'] = FLAG;
+    $data['transfer_type'] = 'ba-us';
+    $data['transfer_state'] = 1;
+    $data['tx_detail'] = '员工动态调整';
+    $data['give_or_receive'] = 1;
+    $data['ctime'] = time();
+    $data['utime'] = date('Y-m-d H:i:s',time());
+    $sql = $db->sqlInsert("com_transfer_request", $data);
+
+    $dat['hash_id'] = hash('md5', $us_id . FLAG . get_ip() . mt() . rand(1000, 9999) . date('Y-m-d H:i:s'));
+    $prvs_hash = get_transfer_pre_hash($us_id);
+    $dat['prvs_hash'] = $prvs_hash == 0 ? $data['hash_id'] : $prvs_hash;
+    $dat['credit_id'] = $us_id;
+    $dat['debit_id'] = BA_ID;
+    $dat['tx_amount'] = $amount*UNIT;
+    $dat['credit_balance'] = get_us_account($us_id)+$dat['tx_amount'];
+    $dat['tx_hash'] = hash('md5', $us_id . FLAG . get_ip() . mt() . date('Y-m-d H:i:s'));
+    $dat['flag'] = FLAG;
+    $dat['transfer_type'] = 'ba-us';
+    $dat['transfer_state'] = 1;
+    $dat['tx_detail'] = '员工动态调整';
+    $dat['give_or_receive'] = 2;
+    $dat['ctime'] = time();
+    $dat['utime'] = date('Y-m-d H:i:s',time());
+    $uql = $db->sqlInsert("com_transfer_request", $dat);
+
+    if($db->query($sql)&&$db->query($uql))
+        return true;
+    die('com_transfer');
+    return false;
+
+}
 
 
 
