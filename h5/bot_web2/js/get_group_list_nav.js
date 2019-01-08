a635220fbc31ed6a2f9183417337727dff23148f
Edwin <1045416962@qq.com>
2019-01-08 18:03:51 +0800

    h5

--- h5/bot_web2/js/get_group_list_nav.js
+++ h5/bot_web2/js/get_group_list_nav.js
@@ -1,5 +1,7 @@
 $(function () {
     var token = GetCookie("robot_token");
+    var is_audit = "1";
+
     function GetGroupListNav() {
         GetGroupList(token, is_audit, function (response) {
             if (response.errcode == "0") {
