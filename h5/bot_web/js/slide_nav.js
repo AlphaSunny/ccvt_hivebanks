592071a73209bdfed7250cf81252a61aa9fdf6c4
Edwin <1045416962@qq.com>
2019-01-08 15:41:47 +0800

    h5

--- h5/bot_web/js/slide_nav.js
+++ h5/bot_web/js/slide_nav.js
@@ -1,7 +1,7 @@
 var url = getRootPath(), li = "";
 $.ajax({
     type: "GET",
-    url: url + "/h5/bot_web/json/slide_nav2.json",
+    url: url + "/h5/bot_web/json/slide_nav.json",
     dataType: "json",
     success: function (res) {
         $.each(res, function (i, val) {
