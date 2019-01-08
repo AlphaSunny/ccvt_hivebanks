70d57af29e31312290bca266d8937f0c3e7e0e4f
songxingwei <you@example.com>
2019-01-08 16:54:55 +0800

    1

--- api/bot/db/bot_bind.php
+++ api/bot/db/bot_bind.php
@@ -1084,4 +1084,289 @@ function get_exclusive_code($wechat){
     return $code;
 }
 
+//======================================
+// 函数: 判断点赞人和被点赞人是否有账号
+//======================================
+function check_wechat($give_wechat,$recive_wechat,$num,$status){
+    $db = new DB_COM();
+    $unit = get_la_base_unit();
+    $data = array();
+    $sql = "select * from us_base WHERE wechat='{$give_wechat}' limit 1";
+    $db->query($sql);
+    $row = $db->fetchRow();
+    $data['give_status'] = $row ? 1 : 2;
+    //判断是否打开开关  1:开  2:关
+    if ($row){
+        $sql = "select bind_info from us_bind WHERE bind_name='point_tread_switch' AND us_id='{$row['us_id']}'";
+        $db->query($sql);
+        $switch = $db->fetchRow();
+        $switch_status = $switch ? $switch['bind_info'] : 2;
+    }else{
+        $switch_status = 2;
+    }
+    $data['switch_status'] = $switch_status;
+    //判断设置金额
+    if ($row){
+        $sql = "select bind_info from us_bind WHERE bind_name='point_tread_num' AND us_id='{$row['us_id']}'";
+        $db->query($sql);
+        $point_tread_num = $db->fetchRow();
+        $point_tread_num_status = $point_tread_num['bind_info'] >= $num ? 1 : 2;
+    }else{
+        $point_tread_num_status = 2;
+    }
+    $data['point_tread_num_status'] = $point_tread_num_status;
+    //判断余额
+    $data['if_balance'] = $row['base_amount']/$unit > $num ? 1 : 2;
+
+    //判断是否达到上限
+    $state = $status == "赞" ? 1 : 2;
+    if ($state==1){
+        $max = get_praise_pointon_maxnum()['max_give_like'];
+    }elseif ($state==2){
+        $max = get_praise_pointon_maxnum()['max_give_no_like'];
+    }
+    $start = strtotime(date('Y-m-d 00:00:00'));
+    $end  = strtotime(date('Y-m-d 23:59:59'));
+    $sql = "SELECT sum(tx_amount)/'{$unit}' as give_all FROM us_glory_integral_change_log WHERE credit_id='{$row['us_id']}' AND state='{$state}' AND ctime BETWEEN '{$start}' AND '{$end}'";
+    $db -> query($sql);
+    $give_all= $db -> getField($sql,'give_all');
+    if ($give_all == $max || ($num+$give_all) > $max){
+        $is_ceiling = 2;
+    }else{
+        $is_ceiling = 1;
+    }
+    $data['is_ceiling'] = $is_ceiling;
+
+    $sql = "select * from us_base WHERE wechat='{$recive_wechat}'";
+    $db->query($sql);
+    $row = $db->fetchRow();
+    $data['recive_status'] = $row ? 1 : 2;
+
+    $data['us_id'] = $row['us_id'];
+    $data['give_us_id'] = $row['us_id'];
+
+    return $data;
+}
+
+//======================================
+// 函数: 点赞点踩
+// 参数:
+//
+// 返回: row           最新信息数组
+//======================================
+function give_like_us($data)
+{
+    $db = new DB_COM();
+    $pInTrans = $db->StartTrans();  //开启事务
+    $unit = get_la_base_unit();
+
+    //积分变动记录表
+    $d['hash_id'] = hash('md5', $data['us_id'] . 'give_like_us' . get_ip() . time() . rand(1000, 9999) . date('Y-m-d H:i:s'));
+    $d['credit_id'] = $data['us_id'];
+    $d['debit_id'] = $data['give_us_id'];
+    $d['tx_amount'] = $data['give_num']*$unit;
+    $d['ctime'] = time();
+    $d['utime'] = date('Y-m-d H:i:s');
+    $d['state'] = $data['state'];
+    $d['tx_detail'] = $data['state'] ==1 ? "点赞" : "点踩";
+    $d['entrance'] = 2;
+    $sql = $db->sqlInsert("us_glory_integral_change_log", $d);
+    $id = $db->query($sql);
+    if (!$id){
+        $db->Rollback($pInTrans);
+        return 0;
+    }
+
+    //用户减钱
+
+    $sql = "update us_base set base_amount=base_amount-'{$data['give_num']}'*'{$unit}' WHERE us_id='{$data['us_id']}'";
+    $db->query($sql);
+    if (!$db->affectedRows()){
+        $db->Rollback($pInTrans);
+        return 0;
+    }
+
+    //la加钱
+    $sql = "update la_base set base_amount=base_amount+'{$data['give_num']}'*'{$unit}' limit 1";
+    $db->query($sql);
+    if (!$db->affectedRows()){
+        $db->Rollback($pInTrans);
+        return 0;
+    }
+
+    //增加荣耀积分(减少荣耀积分)
+    $sql = "select * from us_asset WHERE asset_id='GLOP' AND us_id='{$data['give_us_id']}'";
+    $db->query($sql);
+    $asset_us = $db->fetchRow();
+    if ($asset_us){
+        $sql = "update us_asset set";
+        if ($data['state']==1){
+            $sql .= " base_amount=base_amount+'{$data['give_num']}'*'{$unit}'";
+        }elseif ($data['state']==2){
+            $sql .= " base_amount=base_amount-'{$data['give_num']}'*'{$unit}'";
+        }
+        $sql .= " WHERE asset_id='GLOP' AND us_id='{$data['give_us_id']}'";
+        $db->query($sql);
+        if (!$db->affectedRows()){
+            $db->Rollback($pInTrans);
+            return 0;
+        }
+    }else{
+        $sql = "select * from us_base WHERE us_id='{$data['give_us_id']}'";
+        $db->query($sql);
+        $us_base = $db->fetchRow();
+        $asset['asset_id'] = 'GLOP';
+        $asset['us_id'] = $data['give_us_id'];
+        $asset['us_nm'] = $us_base['us_nm'];
+        $asset['us_account'] = $us_base['us_account'];
+        $asset['base_amount'] = $data['give_num']*$unit;
+        $asset['lock_amount'] = 0;
+        $asset['utime'] = time();
+        $asset['ctime'] = date('Y-m-d H:i:s');
+        $sql = $db->sqlInsert("us_asset", $asset);
+        $id = $db->query($sql);
+        if (!$id){
+            $db->Rollback($pInTrans);
+            return 0;
+        }
+    }
+
+
+    /******************************转账记录表***************************************************/
+    $la_id = get_la_id();
+    //赠送者
+    $flag = $data['state'] == 1 ? 5 : 6;
+    $transfer['hash_id'] = hash('md5', $data['us_id'] . $flag . get_ip() . time() . rand(1000, 9999) . date('Y-m-d H:i:s'));
+    $prvs_hash = get_pre_hash($data['us_id']);
+    $transfer['prvs_hash'] = $prvs_hash == 0 ? $transfer['hash_id'] : $prvs_hash;
+    $transfer['credit_id'] = $data['us_id'];
+    $transfer['debit_id'] = $la_id;
+    $transfer['tx_amount'] = $data['give_num']*$unit;
+    $transfer['credit_balance'] = get_us_account($transfer['credit_id'])-$transfer['tx_amount'];
+    $transfer['tx_hash'] = hash('md5', $data['us_id'] . $flag . get_ip() . time() . date('Y-m-d H:i:s'));
+    $transfer['flag'] = $flag;
+    $transfer['transfer_type'] = 'us-la';
+    $transfer['transfer_state'] = 1;
+    $transfer['tx_detail'] = $data['state']==1 ? '点赞消耗' : "踩人消耗";
+    $transfer['give_or_receive'] = 1;
+    $transfer['ctime'] = time();
+    $transfer['utime'] = date('Y-m-d H:i:s');
+    $sql = $db->sqlInsert("com_transfer_request", $transfer);
+    $id = $db->query($sql);
+    if (!$id){
+        $db->Rollback($pInTrans);
+        return 0;
+    }
+
+    //接收者(la)
+    $dat['hash_id'] = hash('md5', $la_id . $flag . get_ip() . time() . rand(1000, 9999) . date('Y-m-d H:i:s'));
+    $prvs_hash = get_pre_hash($la_id);
+    $dat['prvs_hash'] = $prvs_hash == 0 ? $dat['hash_id'] : $prvs_hash;
+    $dat['credit_id'] = $la_id;
+    $dat['debit_id'] = $data['us_id'];
+    $dat['tx_amount'] = $data['give_num']*$unit;
+    $dat['credit_balance'] = get_la_base_amount($la_id)+$dat['tx_amount'];
+    $dat['tx_hash'] = hash('md5', $la_id . $flag . get_ip() . time() . date('Y-m-d H:i:s'));
+    $dat['flag'] = $flag;
+    $dat['transfer_type'] = 'us-la';
+    $dat['transfer_state'] = 1;
+    $dat['tx_detail'] = $data['state']==1 ? '点赞消耗' : "踩人消耗";
+    $dat['give_or_receive'] = 2;
+    $dat['ctime'] = time();
+    $dat['utime'] = date('Y-m-d H:i:s');
+    $sql = $db->sqlInsert("com_transfer_request", $dat);
+    $id = $db->query($sql);
+    if (!$id){
+        $db->Rollback($pInTrans);
+        return 0;
+    }
+
+    /***********************资金变动记录表***********************************/
+
+    //us添加基准资产变动记录
+    $us_type = 'us_get_balance';
+    $ctime = date('Y-m-d H:i:s');
+    $com_balance_us['hash_id'] = hash('md5', $data['us_id'] . $us_type . get_ip() . time() . rand(1000, 9999) . $ctime);
+    $com_balance_us['tx_id'] = $transfer['tx_hash'];
+    $prvs_hash = get_recharge_pre_hash($data['us_id']);
+    $com_balance_us['prvs_hash'] = $prvs_hash==0 ? $com_balance_us['hash_id'] : $prvs_hash;
+    $com_balance_us["credit_id"] = $data['us_id'];
+    $com_balance_us["debit_id"] = $la_id;
+    $com_balance_us["tx_type"] = 'give_like';
+    $com_balance_us["tx_amount"] = $data['give_num']*$unit;
+    $com_balance_us["credit_balance"] = get_us_account($data['us_id'])-$com_balance_us["tx_amount"];
+    $com_balance_us["utime"] = time();
+    $com_balance_us["ctime"] = date('Y-m-d H:i:s');
+
+    $sql = $db->sqlInsert("com_base_balance", $com_balance_us);
+    if (!$db->query($sql)) {
+        $db->Rollback($pInTrans);
+        return 0;
+    }
+
+    //la添加基准资产变动记录
+    $us_type = 'la_get_balance';
+    $com_balance_ba['hash_id'] = hash('md5', $la_id. $us_type . get_ip() . time() . rand(1000, 9999) . $ctime);
+    $com_balance_ba['tx_id'] = $dat['tx_hash'];
+    $prvs_hash = get_recharge_pre_hash($la_id);
+    $com_balance_ba['prvs_hash'] = $prvs_hash == 0 ? $com_balance_us['hash_id'] : $prvs_hash;
+    $com_balance_ba["credit_id"] = $la_id;
+    $com_balance_ba["debit_id"] = $data['us_id'];
+    $com_balance_ba["tx_type"] = 'give_like';
+    $com_balance_ba["tx_amount"] = $data['give_num']*$unit;
+    $com_balance_ba["credit_balance"] = get_la_base_amount($la_id)+$com_balance_ba["tx_amount"];
+    $com_balance_ba["utime"] = time();
+    $com_balance_ba["ctime"] = $ctime;
+
+    $sql = $db->sqlInsert("com_base_balance", $com_balance_ba);
+    if (!$db->query($sql)) {
+        $db->Rollback($pInTrans);
+        return 0;
+    }
+    
+    $db->Commit($pInTrans);
+    return true;
+}
+//======================================
+// 函数: 获取上传交易hash
+//======================================
+function get_pre_hash($credit_id){
+    $db = new DB_COM();
+    $sql = "SELECT hash_id FROM com_transfer_request WHERE credit_id = '{$credit_id}' ORDER BY  ctime DESC LIMIT 1";
+    $hash_id = $db->getField($sql, 'hash_id');
+    if($hash_id == null)
+        return 0;
+    return $hash_id;
+}
+
+
+//获取la id
+function get_la_id(){
+    $db = new DB_COM();
+    $sql = "select id from la_base limit 1";
+    $db->query($sql);
+    $id = $db->getField($sql,'id');
+    return $id;
+}
+
+//获取la余额
+function get_la_base_amount($la_id){
+    $db = new DB_COM();
+    $sql = "select base_amount from la_base WHERE id='{$la_id}'";
+    $db->query($sql);
+    $amount = $db->getField($sql,'base_amount');
+    return $amount;
+}
+
+
+
+//获取us可用余额
+function get_us_base_true_amount($us_id){
+    $db = new DB_COM();
+    $sql = "select base_amount from us_base WHERE us_id='{$us_id}'";
+    $db->query($sql);
+    $amount = $db->getField($sql,'base_amount');
+    return $amount;
+}
+
 ?>
---
+++ api/bot/give_like_or_nolike.php
@@ -0,0 +1,63 @@
+<?php
+require_once '../inc/common.php';
+require_once 'db/bot_bind.php';
+header("cache-control:no-cache,must-revalidate");
+header("Content-Type:application/json;charset=utf-8");
+
+/*
+========================== @机器人点赞或点踩 ==========================
+
+*/
+
+php_begin();
+
+$args = array('give_wechat','recive_wechat','status','num');
+chk_empty_args('GET', $args);
+
+//点赞人微信昵称
+$give_wechat = get_arg_str('GET','give_wechat');
+//被点赞人微信昵称
+$recive_wechat = get_arg_str('GET','recive_wechat');
+//点赞还是点踩
+$status = get_arg_str('GET','status');
+//数量
+$num = get_arg_str('GET','num');
+if ($num<0){
+    exit_error('109',"@".$give_wechat.",金额错误");
+}
+//查看点赞微信或被点赞人微信是否存在,是否打开,是否大于设置金额
+$is_hive = check_wechat($give_wechat,$recive_wechat,$num,$status);
+if ($is_hive['give_status']!=1){
+    exit_error('109',"@".$give_wechat.",您的微信昵称未绑定");
+}elseif ($is_hive['recive_status']!=1){
+    exit_error('109',"@".$give_wechat.",您".$status."的".$recive_wechat."未找到");
+}elseif ($is_hive['switch_status']!=1){
+    exit_error('109',"@".$give_wechat.",您未开启群内点赞功能,可以去账户中心开启奥");
+}elseif ($is_hive['point_tread_num_status']!=1){
+    exit_error('109',"@".$give_wechat.",金额错误");
+}elseif ($is_hive['if_balance']!=1){
+    exit_error('109',"@".$give_wechat.",余额不足");
+}elseif ($is_hive['is_ceiling']!=1){
+    exit_error('109',"@".$give_wechat.",今日".$status."已达到上限");
+}
+
+$data['us_id'] = $is_hive['us_id'];
+$data['give_us_id'] = $is_hive['give_us_id'];
+$data['state'] = $status == "赞" ? 1 : 2;
+$data['give_num'] = $num;
+
+//点踩、点踩
+if (!give_like_us($data)){
+    exit_error('109',"@".$give_wechat.",点赞失败");
+}
+
+$rtn_ary = array();
+$rtn_ary['errcode'] = '0';
+$rtn_ary['errmsg'] = "@".$give_wechat.",点".$status."成功,金额".$num;
+$rtn_str = json_encode($rtn_ary);
+php_end($rtn_str);
+
+
+
+
+?>
