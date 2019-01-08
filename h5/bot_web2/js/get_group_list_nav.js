8a92de74c9921a3efba072ff36eae273ddf1897c
Edwin <1045416962@qq.com>
2019-01-08 18:29:39 +0800

    h5

--- h5/bot_web2/js/get_group_list_nav.js
+++ h5/bot_web2/js/get_group_list_nav.js
@@ -7,6 +7,34 @@ $(function () {
             if (response.errcode == "0") {
                 var data = response.rows[0];
                 $(".group_name_input").val(data.name);
+                if (data.is_admin_del == "1") {
+                    $(".is_admin_del").val("1").addClass("active");
+                } else {
+                    $(".is_admin_del").val("2").removeClass("active");
+                }
+                if (data.is_flirt == "1") {
+                    $(".is_flirt").val("1").addClass("active");
+                } else {
+                    $(".is_flirt").val("2").removeClass("active");
+                }
+                if (data.send_address == "1") {
+                    $(".send_address").val("1").addClass("active");
+                } else {
+                    $(".send_address").val("2").removeClass("active");
+                }
+                if (data.bind_account_notice == "1") {
+                    $(".bind_account_notice").val("1").addClass("active");
+                } else {
+                    $(".bind_account_notice").val("2").removeClass("active");
+                }
+                if (data.is_welcome == "1") {
+                    $(".is_welcome").val("1").addClass("active");
+                    $(".welcome_text").text(data.welcome).removeClass("none");
+                } else {
+                    $(".is_welcome").val("2").removeClass("active");
+                    $(".welcome_text").text("").addClass("none");
+                }
+
 
             }
         }, function (response) {
