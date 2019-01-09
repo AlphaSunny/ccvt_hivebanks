21b8cdc5f7ef41d1f4aff4fe65cfbd0ecfbc8081
Edwin <1045416962@qq.com>
2019-01-09 10:49:48 +0800

    h5

--- h5/bot_web/js/login.js
+++ h5/bot_web/js/login.js
@@ -1,11 +1,11 @@
 $(function () {
-    // function GetCookie(name) {
-    //     var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
-    //     if (arr != null) return unescape(arr[2]);
-    //     if (arr == null) {
-    //         return;
-    //     }
-    // }
+    function GetCookie(name) {
+        var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+        if (arr != null) return unescape(arr[2]);
+        if (arr == null) {
+            return;
+        }
+    }
 
     var robot_token = GetCookie("user_token");
     if (robot_token) {
