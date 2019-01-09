5f8fd3cb642dca51d0d6769c709c0f48fb18a400
Edwin <1045416962@qq.com>
2019-01-09 17:54:37 +0800

    h5

--- h5/user/js/security.js
+++ h5/user/js/security.js
@@ -66,14 +66,15 @@ $(function () {
                     if (data[i].bind_name == "point_tread_switch" && data[i].bind_info == "1") {
                         $(".quickTreadSwitch").addClass("active").val("1");
                         $(".open,.quick_tread_num_top").removeClass("none");
-                        $(".close_status").remove();
+                        $(".close_status").addClass("none");
                         $(".open_status").removeClass("none");
                         $('.quickTreadIcon').addClass('greenIcon icon-duihao').removeClass('redIcon icon-gantanhao');
                     }
 
                     if (data[i].bind_name == "point_tread_switch" && data[i].bind_info == "2") {
                         $('.quickTreadIcon').addClass('redIcon icon-gantanhao').removeClass('greenIcon icon-duihao');
-                        $(".open_status").remove();
+                        $(".open_status").addClass("none");
+                        $(".close_status").removeClass("none");
                     }
 
                     if (data[i].bind_name == "point_tread_num" && data[i].bind_flag == "1") {
