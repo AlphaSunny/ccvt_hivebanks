d7786c322a330dbe9a5ca4e6881b442717e4b71f
Edwin <1045416962@qq.com>
2019-01-09 14:36:25 +0800

    h5

--- h5/bot_web/js/common.js
+++ h5/bot_web/js/common.js
@@ -13,8 +13,6 @@ function GetCookie(name) {
     var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
     if (arr != null) return unescape(arr[2]);
     if (arr == null) {
-        alert(name);
-        return;
         window.location.href = "login.html";
         return;
     }
