e7060b9f24d725340c0d4162cc0c53e3935fc771
Edwin <1045416962@qq.com>
2019-01-10 15:41:52 +0800

    h5

--- h5/bot_web2/js/key_word.js
+++ h5/bot_web2/js/key_word.js
@@ -46,6 +46,11 @@ $(function () {
                         "<td class='send_type none'>" + data[i].send_type + "</td>" +
                         "<td class='id none'>" + data[i].id + "</td>" +
                         "<td>" +
+                        "<div class='flex center'>" +
+                        "<span>关</span>" +
+                        "<input id='keyWordSwitch' class='switch keyWordSwitch switch-anim margin-left-2' type='checkbox' value='2'>" +
+                        "<span>开</span>" +
+                        "</div>" +
                         "<button class='btn-success btn-sm editBtn'><i class='fa fa-pencil' aria-hidden='true'></i>编辑</button>" +
                         "<button class='btn-sm btn-danger delBtn margin-left-5'><i class='fa fa-trash' aria-hidden='true'></i>删除</button>" +
                         "</td>" +
