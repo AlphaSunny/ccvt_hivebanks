2e9d0eeb95446fd44a629721a09294b3c5093628
Edwin <1045416962@qq.com>
2019-01-10 17:27:02 +0800

    h5

--- h5/bot_web2/js/message_list.js
+++ h5/bot_web2/js/message_list.js
@@ -2,7 +2,7 @@ $(function () {
     //token
     var token = GetCookie("user_token");
     var group_id = GetCookie("group_id");
-    var status = "7";
+    var status = "4";
     //获取消息列表
     GetNewsRecord(token, group_id, status, function (response) {
         if (!response) {
