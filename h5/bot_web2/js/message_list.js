1e508aa795cc8851cadf8876ff447e5436dad678
Edwin <1045416962@qq.com>
2019-01-10 15:13:38 +0800

    h5

--- h5/bot_web2/js/message_list.js
+++ h5/bot_web2/js/message_list.js
@@ -1,11 +1,12 @@
 $(function () {
     //token
     var token = GetCookie("user_token");
+    var group_id = GetCookie("group_id");
     var status = "1";
     //获取消息列表
-    GetNewsRecord(token, status, function (response) {
+    GetNewsRecord(token, group_id, status, function (response) {
         // if (response.errcode == "0") {
-            console.log(response);
+        console.log(response);
         // }
     }, function (response) {
         ErrorPrompt(response.errmsg);
