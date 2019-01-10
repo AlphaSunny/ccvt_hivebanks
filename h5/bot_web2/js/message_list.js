722b7c98ab73ab71e75db1ad1a8c71abb1bf551c
Edwin <1045416962@qq.com>
2019-01-10 15:12:44 +0800

    h5

--- h5/bot_web2/js/message_list.js
+++ h5/bot_web2/js/message_list.js
@@ -4,9 +4,9 @@ $(function () {
     var status = "1";
     //获取消息列表
     GetNewsRecord(token, status, function (response) {
-        if (response.errcode == "0") {
+        // if (response.errcode == "0") {
             console.log(response);
-        }
+        // }
     }, function (response) {
         ErrorPrompt(response.errmsg);
     })
