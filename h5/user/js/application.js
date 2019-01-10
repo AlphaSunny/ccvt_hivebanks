3849d8dcbee38769d95e2cb0c17cbf7a77a6b6ca
Edwin <1045416962@qq.com>
2019-01-10 11:23:18 +0800

    h5

--- h5/user/js/application.js
+++ h5/user/js/application.js
@@ -1,13 +1,17 @@
 $(function () {
-   //token
-   var token = GetCookie("user_token");
-   
-   GetGroupType(token, function (response) {
-       if(response.errcode == "0"){
-           var data = response.rows;
-           console.log(data);
-       }
-   }, function (response) {
-       ErrorPrompt(response.errmsg);
-   })
+    //token
+    var token = GetCookie("user_token");
+
+    GetGroupType(token, function (response) {
+        if (response.errcode == "0") {
+            var data = response.rows;
+            var option = "";
+            $.each(data, function (i, val) {
+                option += "<option value='" + data[i].id + "'>" + data[i].name + "</option>"
+            });
+            $("#group_type").html(option);
+        }
+    }, function (response) {
+        ErrorPrompt(response.errmsg);
+    })
 });
