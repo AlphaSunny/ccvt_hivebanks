3eba5ea19fe345061f827877cfd3653642709100
songxingwei <you@example.com>
2019-01-10 18:05:30 +0800

    1

--- api/leaderBoard/db/us_asset.php
+++ api/leaderBoard/db/us_asset.php
@@ -170,7 +170,7 @@ function get_ranking(){
 function for_key($key,$v,$arr){
     $res = '';
     $lenth = $key  ;
-    for ($i = $lenth; $i > 0 ;$i--)
+    for ($i = $lenth; $i >= 0  ;$i--)
     {
         if($v<$arr[$i]) {
             $res = $i + 1;
