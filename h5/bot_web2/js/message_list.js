1472be1be9ec6da6dd0f053762207987d22ae687
Edwin <1045416962@qq.com>
2019-01-10 15:16:03 +0800

    h5

--- h5/bot_web2/js/message_list.js
+++ h5/bot_web2/js/message_list.js
@@ -6,6 +6,10 @@ $(function () {
     //获取消息列表
     GetNewsRecord(token, group_id, status, function (response) {
         console.log(response);
+        if (!response) {
+            $(".no_data").text("暂无数据");
+            return;
+        }
     }, function (response) {
         ErrorPrompt(response.errmsg);
     })
