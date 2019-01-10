c66a45628e6e6a98ad4ffffa4922966e482ca250
Edwin <1045416962@qq.com>
2019-01-10 11:26:57 +0800

    h5

--- h5/user/js/application.js
+++ h5/user/js/application.js
@@ -13,5 +13,13 @@ $(function () {
         }
     }, function (response) {
         ErrorPrompt(response.errmsg);
+    });
+
+    //bind group
+    $(".bind_group_btn").click(function () {
+        var group_name = $("#group_name").val();
+        var group_type_id = $("#group_type").val();
+        console.log(group_name);
+        console.log(group_type_id);
     })
 });
