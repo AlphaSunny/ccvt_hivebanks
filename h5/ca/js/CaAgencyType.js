8cfdcda8f66d1af22ae564ad125386d39689bf41
Edwin <1045416962@qq.com>
2019-01-11 12:13:27 +0800

    h5

--- h5/ca/js/CaAgencyType.js
+++ h5/ca/js/CaAgencyType.js
@@ -1,37 +1,38 @@
-$(function(){
+$(function () {
     //get token
     var token = getCookie('ca_token');
     GetCaAccount();
 
     //get bank list
-    GetBankList(token, function (response){
-        if(response.errcode == '0'){
+    GetBankList(token, function (response) {
+        if (response.errcode == '0') {
             var data = response.rows, li = '';
-            if(data.length <= 0){
+            if (data.length <= 0) {
                 $('.noData').show();
                 return;
             }
-            $.each(data, function (i, val){
-                li+='<li class="width-20">' +
-                    '<h4 style="font-weight: 400;font-family: cursive;">'+ data[i].option_value +'</h4>' +
+            $.each(data, function (i, val) {
+                li += '<li class="width-20">' +
+                    '<h4 style="font-weight: 400;font-family: cursive;">' + data[i].option_value + '</h4>' +
                     // '<img src="img/'+ data[i].option_key.toLowerCase() +'.png">' +
-                    '<span><i class="iconfont icon-duihao"></i></span>'+
+                    '<span><i class="iconfont icon-duihao"></i></span>' +
                     '</li>'
             });
             $('.changePay').append(li);
         }
-    }, function (response){
+    }, function (response) {
         LayerFun(response.errcode);
         return;
     });
     //Select agent type
-    $(document).on('click', '.changePay li', function(){
-        $(this).siblings().find('.icon-duihao').hide();
-        $(this).find('.icon-duihao').show();
+    $(document).on('click', '.changePay li', function () {
+        // $(this).siblings().find('.icon-duihao').hide();
+        $(this).find('.icon-duihao').show().siblings().find('.icon-duihao').hide();
+        $(this).addClass("active").siblings("li").removeClass("active");
         $('.next').show();
         var imgHtml = $(this).find('img').attr('src');
         //Select proxy mode next step
-        $('.next').click(function(){
+        $('.next').click(function () {
             window.location.href = 'CaProxyAuthentication.html?' + encodeURIComponent(imgHtml);
         })
     })
