9c1b528427efbc0a32f6d928065ea5576bb4d436
Edwin <1045416962@qq.com>
2019-01-14 14:12:29 +0800

    h5

---
+++ h5/user/js/write_css.js
@@ -0,0 +1,16 @@
+// 通用js和css
+(function () {
+    var strFullPath = window.document.location.href;
+    var strPath = window.document.location.pathname;
+    var pos = strFullPath.indexOf(strPath);
+    var prePath = strFullPath.substring(0, pos);
+    var postPath = strPath.substring(0, strPath.substr(1).indexOf('/') + 1);
+    var appRootPath = prePath + postPath;
+
+    var cssHeader = "<link rel='stylesheet' type='text/css' href='" + appRootPath + "/";
+    var cssHeaderLess = "<link rel='stylesheet/less' type='text/css' href='" + appRootPath + "/";
+    var cssFooter = "'></link>";
+    document.write(cssHeader + "ccvt_new/common/css/bootstrap.min.css" + cssFooter);
+    document.write(cssHeader + "ccvt_new/common/css/jquery.pagination.css" + cssFooter);
+    document.write(cssHeaderLess + "ccvt_new/common/css/common.less" + cssFooter);
+})();
