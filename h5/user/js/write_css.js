4a937e241a9e31a36066001ba2f7e19c1960b375
Edwin <1045416962@qq.com>
2019-01-14 15:01:33 +0800

    h5

--- h5/user/js/write_css.js
+++ h5/user/js/write_css.js
@@ -12,8 +12,8 @@
     var cssFooter = "'></link>";
     document.write(cssHeader + "assets/css/bootstrap.min.css" + cssFooter);
     document.write(cssHeader + "assets/css/jquery.pagination.css" + cssFooter);
-    document.write(cssHeaderLess + "assets/css/style.less" + cssFooter);
-    document.write(cssHeaderLess + "user/css/common_user.less" + cssFooter);
+    // document.write(cssHeaderLess + "assets/css/style.less" + cssFooter);
+    document.write(cssHeaderLess + "user/css/style.less" + cssFooter);
     // document.write(cssHeader + "user/css/common.css" + cssFooter);
     document.write(cssHeader + "user/css/account.css" + cssFooter);
 })();
