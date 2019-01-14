46fb09e23068ac03a033f2c8dcba21493bf7f73c
Edwin <1045416962@qq.com>
2019-01-14 14:49:49 +0800

    h5

--- h5/ca/js/CaWithdrawInfo.js
+++ h5/ca/js/CaWithdrawInfo.js
@@ -5,14 +5,14 @@ $(function () {
     GetWithdrawInfo(token, function (response) {
         if(response.errcode == '0'){
             var data =response.rows;
-            if(data[0].qa_flag == '0'){
+            if(data.qa_flag == '0'){
                 $('.enablePending').attr('name', 'enablePending');
                 execI18n();
             }
-            $('.tx_hash').text(data[0].tx_hash);
-            $('.caWithdrawAmount').text(data[0].lgl_amount);
-            $('.handlingFee').text(data[0].tx_fee);
-            $('.time').text(data[0].tx_time);
+            $('.tx_hash').text(data.tx_hash);
+            $('.caWithdrawAmount').text(data.lgl_amount);
+            $('.handlingFee').text(data.tx_fee);
+            $('.time').text(data.tx_time);
         }
     }, function (response) {
         LayerFun(response.errcode);
