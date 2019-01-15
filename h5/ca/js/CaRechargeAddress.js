3099d6ec10e035d13b26f20d1f0338412bce3827
Edwin <1045416962@qq.com>
2019-01-15 13:36:58 +0800

    h5

--- h5/ca/js/CaRechargeAddress.js
+++ h5/ca/js/CaRechargeAddress.js
@@ -3,17 +3,18 @@ $(function () {
     GetUsAccount();
     var ca_channel = GetQueryString('ca_channel'),
         ca_channel_en = GetQueryString('ca_channel_en'),
-        ca_id = GetQueryString("ca_id"),
-        card_nm = GetQueryString('card_nm'),
         bit_amount = GetQueryString('bit_amount'),
         base_amount = GetQueryString('base_amount'),
-        name = window.location.search,
-        start = decodeURIComponent(name).indexOf('name='),
-        end = decodeURIComponent(name).indexOf('&card_nm=');
+        ca_id = GetQueryString("ca_id");
+    $('.ca_channel').text(ca_channel);
     $('.bit_amount').text(bit_amount);
     $('.base_amount').text(base_amount);
-    $('.card_nm').text(card_nm);
-    $('.name').text(decodeURIComponent(name).substring(start + 5, end));
+        // card_nm = GetQueryString('card_nm'),
+        // name = window.location.search,
+        // start = decodeURIComponent(name).indexOf('name='),
+    // $('.card_nm').text(card_nm);
+    // end = decodeURIComponent(name).indexOf('&card_nm=');
+    // $('.name').text(decodeURIComponent(name).substring(start + 5, end));
     // $('.rechargeTypeImg').attr("src", "img/" + ca_channel.toLowerCase() + ".png");
 
 
@@ -21,7 +22,9 @@ $(function () {
     //ca_recharge_order_info.php
     GetCaRechargeInfo(token, ca_id, ca_channel_en, function (response) {
         if (response.errcode == "0") {
-            console.log(response);
+            var rows = response.rows;
+            $('.card_nm').text(rows.card_nm);
+            $('.name').text(rows.name);
         }
     }, function (response) {
         ErrorPrompt(response.errmsg);
@@ -29,9 +32,9 @@ $(function () {
 
 
     //copy recharge address
-    $('.copy_address').click(function () {
-        new ClipboardJS('.copy_address');
-        LayerFun('copySuccess');
-        return;
-    })
+    // $('.copy_address').click(function () {
+    //     new ClipboardJS('.copy_address');
+    //     LayerFun('copySuccess');
+    //     return;
+    // })
 });
