08d74c208449f45bc79ec93f47be77a695605291
Edwin <1045416962@qq.com>
2019-01-15 12:01:22 +0800

    h5

--- h5/ca/js/CaRechargeAddress.js
+++ h5/ca/js/CaRechargeAddress.js
@@ -2,6 +2,7 @@ $(function () {
     GetUsCookie("user_token");
     GetUsAccount();
     var ca_channel = GetQueryString('ca_channel'),
+        ca_id = GetCookie("ca_id"),
         card_nm = GetQueryString('card_nm'),
         bit_amount = GetQueryString('bit_amount'),
         base_amount = GetQueryString('base_amount'),
@@ -12,7 +13,19 @@ $(function () {
     $('.base_amount').text(base_amount);
     $('.card_nm').text(card_nm);
     $('.name').text(decodeURIComponent(name).substring(start + 5, end));
-    $('.rechargeTypeImg').attr("src", "img/" + ca_channel.toLowerCase() + ".png");
+    // $('.rechargeTypeImg').attr("src", "img/" + ca_channel.toLowerCase() + ".png");
+
+
+    //api
+    //ca_recharge_order_info.php
+    GetCaRechargeInfo(token, ca_id, ca_channel, function (response) {
+        if(response.errcode == "0"){
+            console.log(response);
+        }
+    }, function (response) {
+        ErrorPrompt(response.errmsg);
+    });
+
 
     //copy recharge address
     $('.copy_address').click(function () {
