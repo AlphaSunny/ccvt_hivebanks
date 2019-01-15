3327d392f630b5ea9b24983b6e45ddf700b9a7ff
Edwin <1045416962@qq.com>
2019-01-15 18:11:57 +0800

    h5

--- h5/user/js/common.js
+++ h5/user/js/common.js
@@ -792,9 +792,11 @@ window.onload = function () {
     // $("header").css("background-image", "url(assets/img/banner-1.jpg)");
     if (document.readyState === 'loading') {
         document.body.style.overflow = "hidden";
+        console.log("loading");
     } else if (document.readyState === 'interactive' || document.readyState === 'complete') {
         document.body.style.overflow = "auto";
         var loading = document.querySelector(".loading");
+        console.log(loading);
         loading.parentNode.removeChild(loading);
     }
 };
