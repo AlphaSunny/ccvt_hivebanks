82319b47438dfd8e3932704930cfa5fad870fac1
Edwin <1045416962@qq.com>
2019-01-15 13:45:16 +0800

    h5

--- h5/ca/js/CaRechargeAddress.js
+++ h5/ca/js/CaRechargeAddress.js
@@ -22,10 +22,10 @@ $(function () {
     //ca_recharge_order_info.php
     GetCaRechargeInfo(token, ca_id, ca_channel_en, function (response) {
         if (response.errcode == "0") {
-            var rows = response.rows.lgl_address;
+            var rows = JSON.parse(response.rows.lgl_address);
             console.log(rows);
-            // $('.card_nm').text(rows.lgl_address.card_nm);
-            // $('.name').text(rows.lgl_address.name);
+            $('.card_nm').text(rows.card_nm);
+            $('.name').text(rows.name);
         }
     }, function (response) {
         ErrorPrompt(response.errmsg);
