0492ffdf421d76cf217a21100e6062f3eb0dac9a
songxingwei <you@example.com>
2019-01-16 15:00:11 +0800

    1

--- api/crontab/now_data.php
+++ api/crontab/now_data.php
@@ -32,6 +32,7 @@ $db->query($sql);
 $two_invite_send = $db->fetchAll();
 foreach ($two_invite_send as $k=>$v){
     $sql = "select count(us_id) as count from us_base WHERE invite_code in (select us_nm from us_base WHERE invite_code=(select us_nm from us_base WHERE us_id='{$v['us_id']}') GROUP BY us_nm)";
+    echo $sql;
     $db->query($sql);
     $data = $db->fetchRow();
     if ($data['count']==0){
