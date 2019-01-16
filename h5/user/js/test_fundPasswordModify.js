5181ab722e63fca4d70654a11659f2c7cd8b63c4
Edwin <1045416962@qq.com>
2019-01-16 19:12:59 +0800

    h5

--- h5/user/js/test_fundPasswordModify.js
+++ h5/user/js/test_fundPasswordModify.js
@@ -7,7 +7,7 @@ $(function () {
     GetImgCode();
 
     //Switch graphic verification code
-    $('#phone_imgCode').click(function () {
+    $('.getImgCodeBtn').click(function () {
         GetImgCode();
     });
 
