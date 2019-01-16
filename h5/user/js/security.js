29bce89b61dfe6af3957e063d6658ace641ef3cc
Edwin <1045416962@qq.com>
2019-01-16 18:18:16 +0800

    h5

--- h5/user/js/security.js
+++ h5/user/js/security.js
@@ -2,71 +2,92 @@ $(function () {
     // Get user binding information
     var token = GetCookie('user_token'), cellphone = '';
     GetUsAccount();
-    BindingInformation(token, function (response) {
-        if (response.errcode == '0') {
-            var data = response.rows,
-                security_level = parseInt(response.security_level);
-            $('.levelNum').text(security_level);
 
-            // Security Level
-            $.each(data, function (i, val) {
-                //Whether the phone is bound
-                if (data[i].bind_name == 'cellphone' && data[i].bind_flag == '1') {
-                    cellphone = 'cellphone';
-                    $('.phoneTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
-                    $('.phoneBind').fadeOut('fast');
-                    $('.phoneModify').fadeIn('fast');
-                    $(".phoneInfo").text($(this)[0].bind_info);
-                    $('.phoneIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
-                }
+    function BindingInformationFun() {
+        BindingInformation(token, function (response) {
+            if (response.errcode == '0') {
+                var data = response.rows,
+                    security_level = parseInt(response.security_level);
+                $('.levelNum').text(security_level);
 
-                //Whether the email is bound
-                if (data[i].bind_name == 'email' && data[i].bind_flag == '1') {
-                    $('.emailTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
-                    $('.emailBind').fadeOut('fast');
-                    $('.emailModify').fadeIn('fast');
-                    $(".emailInfo").text($(this)[0].bind_info);
-                    $('.emailIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
-                }
+                // Security Level
+                $.each(data, function (i, val) {
+                    //Whether the phone is bound
+                    if (data[i].bind_name == 'cellphone' && data[i].bind_flag == '1') {
+                        cellphone = 'cellphone';
+                        $('.phoneTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
+                        $('.phoneBind').fadeOut('fast');
+                        $('.phoneModify').fadeIn('fast');
+                        $(".phoneInfo").text($(this)[0].bind_info);
+                        $('.phoneIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
+                    }
 
-                //Whether google is certified
-                if (data[i].bind_name == 'GoogleAuthenticator' && data[i].bind_flag == '1') {
-                    $('.googleTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
-                    $('.googleBind').fadeOut('fast');
-                    // $('.fileModify').fadeIn('fast');
-                    $('.googleIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
-                }
+                    //Whether the email is bound
+                    if (data[i].bind_name == 'email' && data[i].bind_flag == '1') {
+                        $('.emailTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
+                        $('.emailBind').fadeOut('fast');
+                        $('.emailModify').fadeIn('fast');
+                        $(".emailInfo").text($(this)[0].bind_info);
+                        $('.emailIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
+                    }
 
-                //Whether the password hash is bound
-                if (data[i].bind_name == 'pass_hash' && data[i].bind_flag == '1') {
-                    $('.fundPasswordTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
-                    $('.fundPasswordBind').fadeOut('fast');
-                    $('.fundPasswordModify').fadeIn('fast');
-                    $('.fundPasswordIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
-                }
+                    //Whether google is certified
+                    if (data[i].bind_name == 'GoogleAuthenticator' && data[i].bind_flag == '1') {
+                        $('.googleTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
+                        $('.googleBind').fadeOut('fast');
+                        // $('.fileModify').fadeIn('fast');
+                        $('.googleIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
+                    }
 
-                //Whether the password login is bound
-                if (data[i].bind_name == 'password_login' && data[i].bind_flag == '1') {
-                    $('.loginPasswordTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
-                    $('.loginPasswordModify').fadeIn('fast');
-                    $('.loginPasswordIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
-                }
+                    //Whether the password hash is bound
+                    if (data[i].bind_name == 'pass_hash' && data[i].bind_flag == '1') {
+                        $('.fundPasswordTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
+                        $('.fundPasswordBind').fadeOut('fast');
+                        $('.fundPasswordModify').fadeIn('fast');
+                        $('.fundPasswordIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
+                    }
 
-                //Whether identity authentication is bound
-                if (data[i].bind_name == 'idPhoto' && data[i].bind_flag == '1') {
-                    $('.authenticationTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
-                    $('.authenticationBind').fadeOut('fast');
-                    // $('.authenticationModify').fadeIn('fast');
-                    $('.authenticationIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
-                }
-            })
-        }
-    }, function (response) {
-        layer.msg(response.errcode);
-        if (response.errcode == '114') {
-            window.location.href = 'login.html';
-        }
-    });
+                    //Whether the password login is bound
+                    if (data[i].bind_name == 'password_login' && data[i].bind_flag == '1') {
+                        $('.loginPasswordTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
+                        $('.loginPasswordModify').fadeIn('fast');
+                        $('.loginPasswordIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
+                    }
+
+                    //Whether identity authentication is bound
+                    if (data[i].bind_name == 'idPhoto' && data[i].bind_flag == '1') {
+                        $('.authenticationTime').removeClass('i18n').text($(this)[0].ctime).addClass('isTime');
+                        $('.authenticationBind').fadeOut('fast');
+                        // $('.authenticationModify').fadeIn('fast');
+                        $('.authenticationIcon').addClass('greenIcon icon-duihao').removeClass('symbol icon-gantanhao');
+                    }
+
+                    //Whether quick tread is bound
+                    if (data[i].bind_name == "point_tread_num" && data[i].bind_flag == "1") {
+                        $(".quick_top_num").text(data[i].bind_info);
+                    }
+
+                    if (data[i].bind_name == "point_tread_switch" && data[i].bind_info == "1") {
+                        $(".quickTreadSwitch").addClass("active").val("1");
+                        $(".close_status").addClass("none");
+                        $(".open_status,.quick_tread_num_top").removeClass("none");
+                    }
+
+                    if (data[i].bind_name == "point_tread_switch" && data[i].bind_info == "2") {
+                        $(".quickTreadSwitch").removeClass("active").val("2");
+                        $(".open_status").addClass("none");
+                    }
+                })
+            }
+        }, function (response) {
+            layer.msg(response.errcode);
+            if (response.errcode == '114') {
+                window.location.href = 'login.html';
+            }
+        });
+    }
+
+    BindingInformationFun();
 
     //return fundPasswordBind
     $('.fundPasswordBind').click(function () {
@@ -88,17 +109,62 @@ $(function () {
         }
     });
 
+    //quick tread
+    var point_tread_switch = "", point_tread_num = "";
+    $("#quickTreadSwitch").on("change", function () {
+        var val = $(this).val();
+        if (val == "1") {
+            $(this).removeClass("active").val("2");
+            point_tread_switch = 2;
+            layer.confirm('确定要关闭快捷赞踩?', {
+                btn: ['确定', '取消'] //鎸夐挳
+            }, function () {
+                PointTreadSwitchFun(point_tread_switch, point_tread_num);
+            }, function () {
+                $("#quickTreadSwitch").addClass("active").val("1");
+            })
+        } else {
+            $(this).addClass("active").val("1");
+            point_tread_switch = 1;
+            $("#top_modal").removeClass("none");
+        }
+    });
+
+    //confirm
+    $(".top_confirm_btn").click(function () {
+        point_tread_num = $(".amount_top_input").val();
+        if (point_tread_num.length <= 0) {
+            WarnPrompt("请输入金额上限");
+            return;
+        }
+        PointTreadSwitchFun(point_tread_switch, point_tread_num);
+        $("#top_modal").addClass("none");
+    });
+
+    function PointTreadSwitchFun(point_tread_switch, point_tread_num) {
+        PointTreadSwitch(token, point_tread_switch, point_tread_num, function (response) {
+            if (response.errcode == "0") {
+                SuccessPrompt("提交成功");
+                BindingInformationFun();
+            }
+        }, function (response) {
+            ErrorPrompt(response.errmsg);
+        })
+    }
+
+    //cancel
+    $(".top_cancel_btn").click(function () {
+        $("#top_modal").addClass("none");
+        $("#quickTreadSwitch").removeClass("active").val("2");
+    });
+
     // Login record query
     var login_api_url = 'log_login.php', limit = 10, offset = 0;
 
     function GetLoginCode(token, limit, offset, login_api_url) {
         var tr = '', count = 1;
-        var index = layer.load(1, {
-            shade: [0.1, '#fff']
-        });
-        // $("#loginCode").html("<tr><td colspan='5'><img src='../assets/img/loading.gif' alt=''><span class='i18n' name='tryingToLoad'>loading...</span></td></tr>")
         AllRecord(token, limit, offset, login_api_url, function (response) {
-            layer.close(index);
+            ShowLoading("hide");
             if (response.errcode == '0') {
                 var data = response.rows;
                 if (data == false) {
@@ -134,13 +200,14 @@ $(function () {
                     nextPageText: ">>",
                     callback: function (current) {
                         GetLoginCode(token, limit, (current - 1) * limit, login_api_url);
+                        ShowLoading("show");
                     }
                 });
             }
 
         }, function (response) {
-            execI18n();
-            layer.msg(response.errcode);
+            ShowLoading("show");
+            ErrorPrompt(response.errmsg);
             if (response.errcode == '114') {
                 window.location.href = 'login.html';
             }
