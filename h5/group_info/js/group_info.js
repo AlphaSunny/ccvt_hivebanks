f9cf9e3a7d59626fdb28a869692b399afd6cdc73
Edwin <1045416962@qq.com>
2019-01-17 16:06:01 +0800

    h5

--- h5/group_info/js/group_info.js
+++ h5/group_info/js/group_info.js
@@ -166,7 +166,7 @@ $(function () {
                 if (res.errcode == "0") {
                     var data = res.row, bind_num = "", glory_number = "";
                     var bind_rows = res.bind_rows;
-                    var newArrary = bind_rows;
+                    var newArrary = [];
                     $(".name").text(data.name);
                     $(".bind_count").text(data.bind_count);
                     $(".glory_number").text(data.glory_number);
@@ -197,10 +197,12 @@ $(function () {
 
 
                     //chart line
-                    // $.each(bind_rows, function (i, val) {
-                    //     newArrary[i].x = bind_rows[i].date;
-                    //     newArrary[i].y = bind_rows[i].num;
-                    // });
+                    $.each(bind_rows, function (i, val) {
+                        // newArrary[i].x = bind_rows[i].date;
+                        // newArrary[i].y = bind_rows[i].num;
+
+                        newArrary.push({x: bind_rows[i].date, y: bind_rows[i].num})
+                    });
                     // config.data.datasets.data = newArrary;
                     ChartLine(newArrary);
 
