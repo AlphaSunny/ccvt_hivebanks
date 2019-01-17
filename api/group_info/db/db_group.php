b6bafda906e0c14f1e108d94ca73f2bf9d03e8f2
songxingwei <you@example.com>
2019-01-17 14:37:11 +0800

    1

--- api/group_info/db/db_group.php
+++ api/group_info/db/db_group.php
@@ -8,7 +8,20 @@
 function get_group_list()
 {
     $db = new DB_COM();
-    $sql = "SELECT id,name,scale,(select count(*) from us_bind where bind_name='group' and bind_info=id) as bind_count FROM bot_group  WHERE is_test=1 AND is_audit=2 AND is_admin_del=1 ORDER BY scale DESC,bind_count desc;";
+    $sql = "SELECT id,name FROM bot_groupt  WHERE is_test=1 AND is_audit=2 AND is_admin_del=1 ORDER BY scale DESC,bind_count desc";
+    $db -> query($sql);
+    $row = $db -> fetchAll();
+    return $row;
+}
+
+//======================================
+// 函数: 获取信息
+// 返回: row           最新信息数组
+//======================================
+function get_group_info($group_id)
+{
+    $db = new DB_COM();
+    $sql = "SELECT id,name,scale,(select count(*) from us_bind where bind_name='group' and bind_info=id) as bind_count FROM bot_group  WHERE is_test=1 AND is_audit=2 AND is_admin_del=1 ORDER BY scale DESC,bind_count desc";
     $db -> query($sql);
     $row = $db -> fetchAll();
     if ($row){
@@ -16,6 +29,9 @@ function get_group_list()
             $sql = "select count(member_id) as count from bot_group_members WHERE group_id='{$v['id']}'";
             $db->query($sql);
             $row[$k]['group_member_number'] = $db->getField($sql,'count');
+            $sql = "select count(id) as count from bot_memeber_change_record WHERE group_id='{$v['id']}' AND to_days(ctime) = to_days(now())";
+            $db->query($sql);
+            $row[$k]['this_day_in'] = $db->getField($sql,'count');
             $row[$k]['glory_number'] = glory_number($v['id']);
             $sql = "select scale from bot_group_level_rules ORDER BY scale desc limit 1";
             $db->query($sql);
