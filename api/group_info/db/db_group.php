9da3b8cded3db3ef63964b04014cc5a8a8548505
songxingwei <you@example.com>
2019-01-17 16:20:41 +0800

    1

--- api/group_info/db/db_group.php
+++ api/group_info/db/db_group.php
@@ -47,6 +47,7 @@ function get_group_info($group_id)
         }
     }
     $row['row'] = $row;
+    
     $weeks = get_weeks();
     foreach ($weeks as $k=>$v){
         $bind_rows[$k-1]['date'] = $v;
@@ -54,14 +55,6 @@ function get_group_info($group_id)
         $db->query($sql);
         $bind_rows[$k-1]['num'] = $db->getField($sql,'num');
     }
-    print_r($bind_rows);
-    die;
-    //七天内绑定变化
-    $sql = "select count(us_id) as num,DATE_FORMAT(FROM_UNIXTIME(UNIX_TIMESTAMP(ctime)), '%Y-%m-%d') as date from us_bind where bind_name='group' AND bind_info='{$group_id}' AND 
-DATE_FORMAT(FROM_UNIXTIME(UNIX_TIMESTAMP(ctime)), '%Y-%m-%d') IN '({$weeks})'";
-    echo $sql;die;
-    $db->query($sql);
-    $bind_rows = $db->fetchAll();
     $row['bind_rows'] = $bind_rows;
     return $row;
 
