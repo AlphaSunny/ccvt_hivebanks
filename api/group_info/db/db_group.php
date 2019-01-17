83a5fbf4a7a00c829f414d96c22146012501ffc0
songxingwei <you@example.com>
2019-01-17 16:10:04 +0800

    1

--- api/group_info/db/db_group.php
+++ api/group_info/db/db_group.php
@@ -47,12 +47,13 @@ function get_group_info($group_id)
         }
     }
     $row['row'] = $row;
+    $weeks = implode(get_weeks(),',');
+    echo $weeks;
     //七天内绑定变化
     $sql = "select count(us_id) as num,DATE_FORMAT(FROM_UNIXTIME(UNIX_TIMESTAMP(ctime)), '%Y-%m-%d') as date from us_bind where bind_name='group' AND bind_info='{$group_id}' AND DATE_FORMAT(FROM_UNIXTIME(UNIX_TIMESTAMP(ctime)), '%Y-%m-%d') between date_sub(curdate(),interval 7 day) and date_sub(curdate(),interval 1 day) group by DATE_FORMAT(FROM_UNIXTIME(UNIX_TIMESTAMP(ctime)), '%Y-%m-%d')";
     $db->query($sql);
     $bind_rows = $db->fetchAll();
     $row['bind_rows'] = $bind_rows;
-    print_r(get_weeks());
     return $row;
 
 }
