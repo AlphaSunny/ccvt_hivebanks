39b612697d8b071b2a7d60209ff1d750b4b8d7dc
songxingwei <you@example.com>
2019-01-17 16:07:56 +0800

    1

--- api/group_info/db/db_group.php
+++ api/group_info/db/db_group.php
@@ -64,7 +64,7 @@ function get_weeks($time = '', $format='Y-m-d'){
     $time = $time != '' ? $time : time();
     //组合数据
     $date = [];
-    for ($i=1; $i<=7; $i++){
+    for ($i=1; $i<7; $i++){
         $date[$i] = date($format ,strtotime( '+' . $i-7 .' days', $time));
     }
     return $date;
