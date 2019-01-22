988d0840bb31c2813b063862ba92b5d9da2bba41
范助国 <fanzhuguo@fanzhuguodeMacBook-Pro.local>
2019-01-22 14:33:51 +0800

    test

--- api/inc/transaction_order/db/com_base_balance.php
+++ api/inc/transaction_order/db/com_base_balance.php
@@ -10,19 +10,19 @@
 function  get_recharge_pre_hash($ba_id)
 {
     $db = new DB_COM();
-    $sql = "SELECT hash_id FROM com_base_balance WHERE credit_id = '{$ba_id}' and tx_type = 'ba_in' ORDER BY  ctime DESC LIMIT 1";
+    $sql = "SELECT hash_id FROM com_base_balance WHERE credit_id = '{$ba_id}'  ORDER BY  ctime DESC LIMIT 1";
     $hash_id = $db->getField($sql, 'hash_id');
     if($hash_id == null)
-        return 0;
+        return hash('md5',$ba_id);//FZG  (return 0;)
     return $hash_id;
 }
 
 function  get_withdraw_pre_hash($ba_id)
 {
     $db = new DB_COM();
-    $sql = "SELECT hash_id FROM com_base_balance WHERE credit_id = '{$ba_id}' and tx_type = 'ba_out' ORDER BY  ctime DESC LIMIT 1";
+    $sql = "SELECT hash_id FROM com_base_balance WHERE credit_id = '{$ba_id}'  ORDER BY  ctime DESC LIMIT 1";
     $hash_id = $db->getField($sql, 'hash_id');
     if($hash_id == null)
-        return 0;
+        return hash('md5',$ba_id);//FZG  (return 0;)
     return $hash_id;
 }
