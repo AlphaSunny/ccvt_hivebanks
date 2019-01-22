fea2b94560e412fa39a0c42c26ac10ce74f07cf3
songxingwei <you@example.com>
2019-01-22 11:31:21 +0800

    1

--- api/user/log_balance.php
+++ api/user/log_balance.php
@@ -40,11 +40,7 @@ $rows = get_log_balance($us_id,$offset,$limit);
 $new_row = array();
 
 foreach ($rows as $row){
-    if ($row['tx_type']=='give_like' || $row['tx_type']=='ccvt_inte' || $row['tx_type']=='us_us_transfer_out'){
-        $row["tx_amount"] = -($row["tx_amount"] / get_la_base_unit());
-    }else{
-        $row["tx_amount"] = $row["tx_amount"] / get_la_base_unit();
-    }
+    $row["tx_amount"] = $row["tx_amount"] / get_la_base_unit();
     $row["credit_balance"] = $row["credit_balance"] / get_la_base_unit();
     $new_row[] = $row;
 }
