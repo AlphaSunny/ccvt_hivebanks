d4c58676256b90faf0d86f81a136744d8fb62ba3
juguohong <1045416962@qq.com>
2019-01-23 17:05:46 +0800

    h5

--- h5/glory/threeUp/js/test2.js
+++ h5/glory/threeUp/js/test2.js
@@ -141,9 +141,9 @@ $(function () {
     //3生成列表
     function ListThree() {
         $.each(three_list, function (j, val) {
-            if (three_list <= max || j < item_three) {
+            if (three_list.length <= max || j < item_three) {
                 $(".level_three_ul_box ul:nth-child(1)").append("<li class='wow slideInLeft' data-wow-delay='800ms'><svg class='icon'><use xlink:href='#icon-lv3'></use></svg>" + three_list[j].wechat + "</li>");
-            } else if (three_list > max && j >= item_three && j < item_three * 2) {
+            } else if (three_list.length > max && j >= item_three && j < item_three * 2) {
                 $(".level_three_ul_box ul:nth-child(2)").append("<li class='wow slideInLeft' data-wow-delay='800ms'><svg class='icon'><use xlink:href='#icon-lv3'></use></svg>" + three_list[j].wechat + "</li>");
             } else {
                 $(".level_three_ul_box ul:nth-child(3)").append("<li class='wow slideInLeft' data-wow-delay='800ms'><svg class='icon'><use xlink:href='#icon-lv3'></use></svg>" + three_list[j].wechat + "</li>");
