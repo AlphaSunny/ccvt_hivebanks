c01d0a224ebe26aa7dff2b13ac547325dcfefb7c
songxingwei <you@example.com>
2019-01-23 18:49:26 +0800

    1

--- api/crontab/get_scale_us_data.php
+++ api/crontab/get_scale_us_data.php
@@ -25,33 +25,38 @@ error_reporting(E_ALL | E_STRICT);
 $db = new DB_COM();
 $unit = get_la_base_unit();
 
-$sql = "select after_scale from us_scale_changes WHERE ctime>'2018-12-2 00:00:00' group BY after_scale ORDER by after_scale DESC ";
+$times = '2018-12-2 00:00:00';
+
+$sql = "select after_scale from us_scale_changes WHERE ctime>'{$times}' group BY after_scale ORDER by after_scale DESC ";
 $db->query($sql);
 $scales = $db->fetchAll();
-print_r($scales);
-die;
+$all_list = [];
+foreach ($scales as $k=>$v){
+    $sql = "select us.wechat from us_scale_changes as sc INNER JOIN us_base as us on sc.us_id=us.us_id WHERE sc.after_scale='{$v['after_scale']}'  AND us.wechat!='' AND sc.ctime>'{$times}' ORDER BY sc.scale DESC";
+    $db->query($sql);
+    $list = $db->fetchAll();
+    $all_list = array_merge($all_list,$list);
+    $rtn_ary[$v['after_scale']."_list"] = $list;
+}
 
 
-//1
-$sql = "select us.wechat from us_scale_changes as sc INNER JOIN us_base as us on sc.us_id=us.us_id WHERE sc.after_scale=1  AND us.wechat!='' AND sc.ctime>'2018-12-2 00:00:00' ORDER BY sc.scale DESC";
-$db->query($sql);
-$one_list = $db->fetchAll();
+////1
+//$sql = "select us.wechat from us_scale_changes as sc INNER JOIN us_base as us on sc.us_id=us.us_id WHERE sc.after_scale=1  AND us.wechat!='' AND sc.ctime>'2018-12-2 00:00:00' ORDER BY sc.scale DESC";
+//$db->query($sql);
+//$one_list = $db->fetchAll();
+//
+////2
+//$sql = "select us.wechat from us_scale_changes as sc INNER JOIN us_base as us on sc.us_id=us.us_id WHERE sc.after_scale=2  AND us.wechat!='' AND sc.ctime>'2018-12-2 00:00:00' ORDER BY sc.scale DESC";
+//$db->query($sql);
+//$two_list = $db->fetchAll();
 
-//2
-$sql = "select us.wechat from us_scale_changes as sc INNER JOIN us_base as us on sc.us_id=us.us_id WHERE sc.after_scale=2  AND us.wechat!='' AND sc.ctime>'2018-12-2 00:00:00' ORDER BY sc.scale DESC";
-$db->query($sql);
-$two_list = $db->fetchAll();
-
-//all
-$all_list = array_merge($one_list,$two_list);
+////all
+//$all_list = array_merge($one_list,$two_list);
 
 // 返回数据做成
 $rtn_ary = array();
 $rtn_ary['errcode'] = '0';
 $rtn_ary['errmsg'] = '';
-$rtn_ary['one_list'] =$one_list;
-$rtn_ary['two_list'] =$two_list;
-$rtn_ary['all_list'] = $all_list;
 $rtn_str = json_encode($rtn_ary);
 php_end($rtn_str);
 
