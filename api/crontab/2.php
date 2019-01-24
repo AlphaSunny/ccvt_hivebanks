17468aab9a026ef747ad95b0ad17851b3831078f
songxingwei <you@example.com>
2019-01-24 16:07:24 +0800

    1

--- api/crontab/2.php
+++ api/crontab/2.php
@@ -1,23 +1,7 @@
 <?php
-//$redis = new Redis();
-//
-//$redis->connect('18.219.17.238', 6379); //连接Redis
-//$redis->auth('Windwin2018'); //密码验证
-//$redis->select(0);//选择数据库2
-//$redis->set( "testKey" , "Hello Redis"); //设置测试key
-//echo $redis->get("testKey");//输出value
-
-ini_set('default_socket_timeout', -1);
-$ip = "18.219.17.238";
-$port = '6379';
 $redis = new Redis();
-
-$redis->pconnect($ip, $port, 1);
+$redis->connect('18.219.114.141', 6379); //连接Redis
 $redis->auth('Windwin2018'); //密码验证
-$key = "test";
-$value = "this is test";
-
-$redis->set($key, $value);
-$d = $redis->get($key);
-var_dump($d);
+$redis->set( "testKey" , "Hello Redis"); //设置测试key
+echo $redis->get("testKey");//输出value
 ?>
