1763c045703377ec3d3db6b17013ca51d98b59f5
juguohong <1045416962@qq.com>
2019-01-24 14:51:22 +0800

    h5

--- h5/user/js/verify_withdraw.js
+++ h5/user/js/verify_withdraw.js
@@ -1,21 +1,21 @@
 $(function () {
-    function verifyWithdraw(token) {
+    let token = GetCookie('user_token');
+    function verifyWithdraw() {
         let base_amount = "";
         UserInformation(token, function (response) {
             if (response.errcode == "0") {
                 let data = response.rows;
                 base_amount = data.base_amount;
+                return base_amount;
             }
         }, function (response) {
             ErrorPrompt(response);
         });
-        return base_amount;
     }
 
 //withdraw
     $('.nav_ba_withdraw').click(function () {
-        let token = GetCookie('user_token');
-        let base_amount = verifyWithdraw(token);
+        let base_amount = verifyWithdraw();
         if (base_amount <= 0) {
             // $('#noBalanceModal').modal('show');
             WarnPrompt("账户余额不足，无法进行提现");
@@ -24,8 +24,7 @@ $(function () {
         window.location.href = "bit_withdraw.html";
     });
     $('.nav_ca_withdraw').click(function () {
-        let token = GetCookie('user_token');
-        let base_amount = verifyWithdraw(token);
+        let base_amount = verifyWithdraw();
         console.log(base_amount);
         if (base_amount <= 0) {
             // $('#noBalanceModal').modal('show');
