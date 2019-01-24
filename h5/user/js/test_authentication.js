a49660610abcb9c9a1f624ff235ecf55cf31bc4c
juguohong <1045416962@qq.com>
2019-01-24 10:44:09 +0800

    h5

--- h5/user/js/test_authentication.js
+++ h5/user/js/test_authentication.js
@@ -1,31 +1,31 @@
 $(function () {
     //token
-    var token = GetCookie('user_token');
-    var id = getCookie('us_id');
-    var url = getRootPath();
+    let token = GetCookie('user_token');
+    let id = getCookie('us_id');
+    let url = getRootPath();
     GetUsAccount();
 
     //Get binding information, whether to bind
-    var name = '', idNum = '', idPhoto = '';
+    let name = '', idNum = '', idPhoto = '';
 
     function GetBindInfo() {
         BindingInformation(token, function (response) {
             if (response.errcode == '0') {
-                var data = response.rows;
+                let data = response.rows;
                 //count_error==0review 1rejection
                 $.each(data, function (i, val) {
 
                     //bind name
                     if (data[i].bind_name == 'name' && data[i].bind_flag == '1') {//Name binding succeeded
                         name = data[i].bind_name;
-                        var info = data[i].bind_info;
+                        let info = data[i].bind_info;
                         $(".nameNotBind,.nameUnderReview,.nameBindBtn,#nameBindModal").remove();
                         $(".nameBindInfo").text(info).removeClass("none");
                         $(".nameAlreadyBind").removeClass("none");
                         return;
                     } else if (data[i].bind_name == 'name' && data[i].count_error == '0') {//Name review
                         name = data[i].bind_name;
-                        var info = data[i].bind_info;
+                        let info = data[i].bind_info;
                         $(".nameNotBind,.nameBindBtn,.nameAlreadyBind,#nameBindModal").remove();
                         $(".nameBindInfo").text(info).removeClass("none");
                         $(".nameUnderReview").removeClass("none");
@@ -39,14 +39,14 @@ $(function () {
                     //Bind ID number
                     if (data[i].bind_name == 'idNum' && data[i].bind_flag == '1') {//ID card number binding success
                         idNum = data[i].bind_name;
-                        var idNum_info = data[i].bind_info;
+                        let idNum_info = data[i].bind_info;
                         $(".idCardNotBind,.idCardUnderReview,.idCardBindBtn,#idCardBindModal").remove();
                         $(".idCardBindInfo").text(idNum_info).removeClass("none");
                         $(".idCardAlreadyBind").removeClass("none");
                         return;
                     } else if (data[i].bind_name == 'idNum' && data[i].count_error == '0') {//ID card number review
                         idNum = data[i].bind_name;
-                        var idNum_info = data[i].bind_info;
+                        let idNum_info = data[i].bind_info;
                         $(".idCardNotBind,.idCardBindBtn,.idCardAlreadyBind,#idCardBindModal").remove();
                         $(".idCardBindInfo").text(idNum_info).removeClass("none");
                         $(".idCardUnderReview").removeClass("none");
@@ -60,22 +60,22 @@ $(function () {
                     //Upload ID card
                     if (data[i].bind_name == 'idPhoto' && data[i].bind_flag == '1') {//Successful ID card upload
                         idPhoto = data[i].bind_name;
-                        var idPhotoInfo = data[i].bind_info;
+                        let idPhotoInfo = data[i].bind_info;
                         $(".idPhotoNotBind,.idPhotoUnderReview,.idPhotoBindBtn,.idPhotoBindInfo,#idPhotoBindModal").remove();
                         $(".idCardBindInfo").removeClass("none");
                         $(".idPhotoAlreadyBind").removeClass("none");
-                        var idPhoto1 = idPhotoInfo.split(",")[0];
-                        var idPhoto2 = idPhotoInfo.split(",")[1];
+                        let idPhoto1 = idPhotoInfo.split(",")[0];
+                        let idPhoto2 = idPhotoInfo.split(",")[1];
                         $(".idPhoto1").attr("src",idPhoto1);
                         $(".idPhoto2").attr("src",idPhoto2);
                         return;
                     } else if (data[i].bind_name == 'idPhoto' && data[i].count_error == '0') {//Upload ID card review
                         idPhoto = data[i].bind_name;
-                        var idPhotoInfo = data[i].bind_info;
+                        let idPhotoInfo = data[i].bind_info;
                         $(".idPhotoNotBind,.idPhotoBindBtn,.idPhotoAlreadyBind,#idPhotoBindModal").remove();
                         $(".idPhotoUnderReview,.idPhotoBindInfo").removeClass("none");
-                        var idPhoto1 = idPhotoInfo.split(",")[0];
-                        var idPhoto2 = idPhotoInfo.split(",")[1];
+                        let idPhoto1 = idPhotoInfo.split(",")[0];
+                        let idPhoto2 = idPhotoInfo.split(",")[1];
                         $(".idPhoto1").attr("src",idPhoto1);
                         $(".idPhoto2").attr("src",idPhoto2);
                         return;
@@ -105,7 +105,7 @@ $(function () {
     });
     //bind name
     $('.nameBindEnable').click(function () {
-        var text_type = '3',
+        let text_type = '3',
             text = $('#nameBindInput').val(),
             text_hash = hex_sha1(text);
 
@@ -114,7 +114,7 @@ $(function () {
             WarnPrompt("请输入姓名");
             return;
         }
-        var $this = $(this), btnText = $(this).text();
+        let $this = $(this), btnText = $(this).text();
         if (DisableClick($this)) return;
         ShowLoading("show");
         TextBind(token, text_type, text, text_hash, function (response) {
@@ -148,7 +148,7 @@ $(function () {
 
     //Bind ID number
     $('.idNumBindEnable').click(function () {
-        var text_type = '2',
+        let text_type = '2',
             text = $('#idCardBindInput').val(),
             text_hash = hex_sha1(text);
 
@@ -157,7 +157,7 @@ $(function () {
             WarnPrompt("请输入身份证号码");
             return;
         }
-        var $this = $(this), btnText = $(this).text();
+        let $this = $(this), btnText = $(this).text();
         if (DisableClick($this)) return;
         ShowLoading("show");
         TextBind(token, text_type, text, text_hash, function (response) {
@@ -199,7 +199,7 @@ $(function () {
 
     //Return images information
     function UpLoadImg(formData) {
-        var src = '';
+        let src = '';
         $.ajax({
             url: url + '/api/plugin/upload_file.php',
             type: 'POST',
@@ -209,8 +209,7 @@ $(function () {
             contentType: false,
             processData: false,
             success: function (response) {
-                ShowLoading("hide");
-                var data = JSON.parse(response);
+                let data = JSON.parse(response);
                 if (data.errcode == '0') {
                     src = data.url;
                 }
@@ -223,7 +222,7 @@ $(function () {
     }
 
     //get key_code
-    var key_code = "";
+    let key_code = "";
     GetKeyCode(token, function (response) {
         if (response.errcode == '0') {
             key_code = response.key_code;
@@ -237,28 +236,29 @@ $(function () {
      *Get the selection file
      * ID card upload verification
      */
-    var src1 = '', src2 = '';
+    let src1 = '', src2 = '';
     $('#file0').on('change', function () {
-        var objUrl = getObjectURL(this.files[0]);
+        ShowLoading("show");
+        let objUrl = getObjectURL(this.files[0]);
         if (objUrl) {
             // show img
             $("#idPositive").attr("src", objUrl);
         }
+        ShowLoading("hide");
 
-        var formData = new FormData($("#form0")[0]);
+        let formData = new FormData($("#form0")[0]);
         formData.append("file", this.files[0]);
         formData.append("key_code", key_code);
         src1 = UpLoadImg(formData);
-        ShowLoading("show");
     });
     //Upload back
     $('#file1').on('change', function () {
-        var objUrl = getObjectURL(this.files[0]);
+        let objUrl = getObjectURL(this.files[0]);
         if (objUrl) {
             // show img
             $("#idNegative").attr("src", objUrl);
         }
-        var formData = new FormData($("#form1")[0]);
+        let formData = new FormData($("#form1")[0]);
         formData.append("file", this.files[0]);
         formData.append("key_code", key_code);
         src2 = UpLoadImg(formData);
@@ -266,10 +266,10 @@ $(function () {
 
     // ID card upload verification
     $('#submit').click(function () {
-        var file_type = 'idPhoto',
+        let file_type = 'idPhoto',
             file_url = src1 + ',' + src2;
         //bind file
-        var $this = $(this), btnText = $(this).text();
+        let $this = $(this), btnText = $(this).text();
         if (DisableClick($this)) return;
         ShowLoading("show");
         FileBind(token, file_type, file_url, function (response) {
@@ -290,7 +290,7 @@ $(function () {
 
     //Display when selecting a picture
     function getObjectURL(file) {
-        var url = null;
+        let url = null;
         if (window.createObjectURL != undefined) { // basic
             url = window.createObjectURL(file);
         } else if (window.URL != undefined) { // mozilla(firefox)
