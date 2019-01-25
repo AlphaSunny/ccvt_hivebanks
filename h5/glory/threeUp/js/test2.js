8437f45c842f7964980a1c7ba367f79b1ab1cead
juguohong <1045416962@qq.com>
2019-01-25 18:34:18 +0800

    h5

--- h5/glory/threeUp/js/test2.js
+++ h5/glory/threeUp/js/test2.js
@@ -22,7 +22,7 @@ $(function () {
     //判断当前时间
     function timeIsOk() {
         let curr_time = new Date();
-        let end_time = new Date("2019-1-25 16:31");//setTime
+        let end_time = new Date("2019-1-25 20:01");//setTime
         let num = parseInt((end_time - curr_time) / 1000);
 
         console.log(num);
