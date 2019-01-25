d88b8e89fffbd3d1c220109fb6c0552a6b230522
juguohong <1045416962@qq.com>
2019-01-25 14:21:58 +0800

    h5

---
+++ h5/group_info/js/common.js
@@ -0,0 +1,55 @@
+// Get URL parameters
+function GetQueryString(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+
+//CallApi
+function CallApi(api_url, post_data, suc_func, error_func) {
+    let api_site = config_api_url + '/api/group_info/';
+    post_data = post_data || {};
+    suc_func = suc_func || function () {
+    };
+    error_func = error_func || function () {
+    };
+    $.ajax({
+        url: api_site + api_url,
+        dataType: "jsonp",
+        data: post_data,
+        success: function (response) {
+            // API return failed
+            if (response.errcode != 0) {
+                error_func(response);
+            } else {
+                // Successfully process data
+                suc_func(response);
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            // API error exception
+            let response = {"errcode": -1, "errmsg": '系统异常，请稍候再试'};
+            // Exception handling
+            error_func(response);
+        }
+    });
+}
+
+function _GetGroupList(limit, offset, search_name, scale, type_id, suc_func, error_func) {
+    let api_url = "group_list.php";
+    let post_data = {
+        "limit": limit,
+        "offset": offset,
+        "search_name": search_name,
+        "scale": scale,
+        "type_id": type_id
+    };
+    CallApi(api_url, post_data, suc_func, error_func);
+}
+
+function GetGroupSearch(suc_func, error_func) {
+    let api_url = "group_search.php";
+    let post_data = {};
+    CallApi(api_url, post_data, suc_func, error_func);
+}
--- h5/group_info/js/group_list.js
+++ h5/group_info/js/group_list.js
@@ -23,63 +23,76 @@ $(function () {
     });
 
     //获取群列表
-    let limit = 10, offset = 0;
+    let limit = 10, offset = 0, search_name = "", scale = "", type_id = "";
 
-    function getGroupListFun(limit, offset) {
+    function getGroupListFun(limit, offset, search_name, scale, type_id) {
         let count = "";
-        $.ajax({
-            type: "GET",
-            url: getRootPath() + "/api/group_info/group_list.php?limit=" + limit + "&offset=" + offset,
-            dataType: "jsonp",
-            success: function (res) {
-                if (res.errcode == "0") {
-                    let data = res.rows;
-                    let total = res.total;
-                    let totalPage = Math.ceil(total / limit);
-                    if (totalPage <= 1) {
-                        count = 1;
-                    } else if (1 < totalPage && totalPage <= 6) {
-                        count = totalPage;
-                    } else {
-                        count = 6;
-                    }
-                    let tr = "";
-                    console.log(data);
-                    $.each(data, function (i, val) {
-                        tr += "<tr>" +
-                            "<td class='id_name' id=" + data[i].id + " title=" + data[i].name + ">" + data[i].name + "</td>" +
-                            "<td>" + data[i].scale + "</td>" +
-                            "<td>暂无数据</td>" +
-                            "<td>暂无数据</td>" +
-                            "<td><a href='javascript:;' class='to_group_info'>查看</a></td>" +
-                            "</tr>";
-                    });
-                    $("#group_list").html(tr);
-                    $("#pagination").pagination({
-                        currentPage: (limit + offset) / limit,
-                        totalPage: totalPage,
-                        isShow: false,
-                        count: count,
-                        prevPageText: "<<",
-                        nextPageText: ">>",
-                        callback: function (current) {
-                            getGroupListFun(limit, (current - 1) * limit);
-                            ShowLoading("show");
-                        }
-                    });
+        _GetGroupList(limit, offset, search_name, scale, type_id, function (response) {
+            if (response.errcode == "0") {
+                let data = response.rows;
+                let total = response.total;
+                let totalPage = Math.ceil(total / limit);
+                if (totalPage <= 1) {
+                    count = 1;
+                } else if (1 < totalPage && totalPage <= 6) {
+                    count = totalPage;
+                } else {
+                    count = 6;
                 }
-            },
-            error: function (res) {
-                ErrorPrompt(res.errmsg);
+                let tr = "";
+                console.log(data);
+                $.each(data, function (i, val) {
+                    tr += "<tr>" +
+                        "<td class='id_name' id=" + data[i].id + " title=" + data[i].name + ">" + data[i].name + "</td>" +
+                        "<td>" + data[i].scale + "</td>" +
+                        "<td>暂无数据</td>" +
+                        "<td>暂无数据</td>" +
+                        "<td><a href='javascript:;' class='to_group_info'>查看</a></td>" +
+                        "</tr>";
+                });
+                $("#group_list").html(tr);
+                $("#pagination").pagination({
+                    currentPage: (limit + offset) / limit,
+                    totalPage: totalPage,
+                    isShow: false,
+                    count: count,
+                    prevPageText: "<<",
+                    nextPageText: ">>",
+                    callback: function (current) {
+                        getGroupListFun(limit, (current - 1) * limit);
+                        ShowLoading("show");
+                    }
+                });
             }
+        }, function (response) {
+            ErrorPrompt(response.errmsg);
         });
     }
 
-    getGroupListFun(limit, offset);
+    getGroupListFun(limit, offset, search_name, scale, type_id);
 
     //前往群详情
     $(document).on("click", ".to_group_info", function () {
         let id = $(this).parents("tr").find(".id_name").attr("id");
         window.location.href = "group_info.html?id=" + id;
-    })
+    });
+
+    //获取筛选列表
+    GetGroupSearch(function (response) {
+        let option_scale = "<option vlaue='0'>等级筛选</option>";
+        let option_type = "<option vlaue='0'>类型筛选</option>";
+        let scale_list = response.scale_list;
+        let type_list = response.type_list;
+        $.each(scale_list, function (i, val) {
+            option_scale += "<option vlaue=" + data[i].scale + ">" + data[i].scale + "</option>";
+        });
+        $("#level").html(option_scale);
+
+        $.each(type_list, function (i, val) {
+            option_scale += "<option vlaue=" + data[i].id + ">" + data[i].name + "</option>";
+        });
+        $("#type").html(option_type);
+    }, function (response) {
+        ErrorPrompt(response.errmsg);
+    });
 });
