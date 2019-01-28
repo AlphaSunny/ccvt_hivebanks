cc12da0420636ed0f12e9dc3c3ffda1d42f3e7ed
范助国 <fanzhuguo@fanzhuguodeMacBook-Pro.local>
2019-01-28 15:53:59 +0800

    test

--- api/crontab/dynamic_tuning.php
+++ api/crontab/dynamic_tuning.php
@@ -427,7 +427,7 @@ function add_log_transfer($us_id,$amount){
 function  get_recharge_pre_hash($ba_id)
 {
     $db = new DB_COM();
-    $sql = "SELECT hash_id FROM com_base_balance WHERE credit_id = '{$ba_id}' and tx_type = 'ba_send' ORDER BY  ctime DESC LIMIT 1";
+    $sql = "SELECT hash_id FROM com_base_balance WHERE credit_id = '{$ba_id}'  ORDER BY  ctime DESC LIMIT 1";
     $hash_id = $db->getField($sql, 'hash_id');
     if($hash_id == null)
         return 0;
