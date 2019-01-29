6a569cfddeec4c220b65d044012ffa34c96f1892
juguohong <1045416962@qq.com>
2019-01-29 15:38:20 +0800

    h5

--- h5/user/language/language.js
+++ h5/user/language/language.js
@@ -1,7 +1,7 @@
 /**
  * Cookie operation
  */
-var getCookie = function(name, value, options) {
+var getCookie = function (name, value, options) {
     if (typeof value != 'undefined') { // name and value given, set cookie
         options = options || {};
         if (value === null) {
@@ -63,13 +63,13 @@ var i18nLanguage = "";
 /*
 Set the language types supported by the website
  */
-var webLanguage = ['en','zh-CN', 'ja'];
+var webLanguage = ['en', 'zh-CN', 'ja'];
 
 /**
  * Execute page i18n method
  * @return
- */ 
-var execI18n = function(){
+ */
+var execI18n = function () {
     /*
     Get the resource file name
      */
@@ -77,92 +77,98 @@ var execI18n = function(){
     if (optionEle.length < 1) {
         // console.log("未找到页面名称元素，请在页面写入\n <meta id=\"i18n_pagename\" content=\"页面名(对应语言包的语言文件名)\">");
         return false;
-    };
+    }
+    ;
     var sourceName = optionEle.attr('content');
     sourceName = sourceName.split('-');
-        /*
-        First get the language type selected before the user's browser device
-         */
-        if (getCookie("userLanguage")) {
-            i18nLanguage = getCookie("userLanguage");
-        } else {
-            // Get the browser language
-            // var navLanguage = getNavLanguage();
-            // if (navLanguage) {
-            //     // Determine if it is in the web support language array
-            //     var charSize = $.inArray(navLanguage, webLanguage);
-            //     if (charSize > -1) {
-            //         i18nLanguage = navLanguage;
-            //         // Save to cache
-            //         getCookie("userLanguage",navLanguage);
-            //     };
-            // } else{
-            //     console.log("not navigator");
-            //     return false;
-            // }
+    /*
+    First get the language type selected before the user's browser device
+     */
+    if (getCookie("userLanguage")) {
+        i18nLanguage = getCookie("userLanguage");
+    } else {
+        // Get the browser language
+        // var navLanguage = getNavLanguage();
+        // if (navLanguage) {
+        //     // Determine if it is in the web support language array
+        //     var charSize = $.inArray(navLanguage, webLanguage);
+        //     if (charSize > -1) {
+        //         i18nLanguage = navLanguage;
+        //         // Save to cache
+        //         getCookie("userLanguage",navLanguage);
+        //     };
+        // } else{
+        //     console.log("not navigator");
+        //     return false;
+        // }
+    }
+    /* Need to introduce i18n file*/
+    if ($.i18n == undefined) {
+        // console.log("请引入i18n js 文件")
+        return false;
+    }
+    ;
+
+    /*
+    Here you need to translate i18n
+     */
+    jQuery.i18n.properties({
+        name: sourceName, //Resource file name
+        path: 'language/i18n/' + i18nLanguage + '/', //Resource file path
+        mode: 'map', //Use the value in the resource file as a Map
+        language: i18nLanguage,
+        checkAvailableLanguages: true,
+        async: true,
+        callback: function () {//Set the display content after loading successfully
+            var insertEle = $(".i18n");
+            // console.log(".i18n 写入中...");
+            insertEle.each(function () {
+                // Get content write based on the name of the i18n element
+                $(this).html($.i18n.prop($(this).attr('name')));
+            });
+            // console.log("写入完毕");
+
+            // console.log(".i18n-input 写入中...");
+            var insertInputEle = $(".i18n-input");
+            insertInputEle.each(function () {
+                var selectAttr = $(this).attr('selectattr');
+                if (!selectAttr) {
+                    selectAttr = "value";
+                }
+                ;
+                $(this).attr(selectAttr, $.i18n.prop($(this).attr('selectname')));
+            });
+            // console.log("写入完毕");
         }
-        /* Need to introduce i18n file*/
-        if ($.i18n == undefined) {
-            // console.log("请引入i18n js 文件")
-            return false;
-        };
-
-        /*
-        Here you need to translate i18n
-         */
-        jQuery.i18n.properties({
-            name : sourceName, //Resource file name
-            path : 'language/i18n/' + i18nLanguage +'/', //Resource file path
-            mode : 'map', //Use the value in the resource file as a Map
-            language : i18nLanguage,
-            checkAvailableLanguages:true,
-            async:true,
-            callback : function() {//Set the display content after loading successfully
-                var insertEle = $(".i18n");
-                // console.log(".i18n 写入中...");
-                insertEle.each(function() {
-                    // Get content write based on the name of the i18n element
-                    $(this).html($.i18n.prop($(this).attr('name')));
-                });
-                // console.log("写入完毕");
-
-                // console.log(".i18n-input 写入中...");
-                var insertInputEle = $(".i18n-input");
-                insertInputEle.each(function() {
-                    var selectAttr = $(this).attr('selectattr');
-                    if (!selectAttr) {
-                        selectAttr = "value";
-                    };
-                    $(this).attr(selectAttr, $.i18n.prop($(this).attr('selectname')));
-                });
-                // console.log("写入完毕");
-            }
-        });
+    });
 };
 
 /*Page execution load execution*/
-$(function(){
+$(function () {
 
     /*Perform I18n translation*/
     execI18n();
 
     /*Set the language selection to the value in the cache by default*/
     // $("#language option[value="+i18nLanguage+"]").prop("selected",true);
-
+    var get_language_icon = $.cookie("language_icon");
     var languageList = $(".toggle_language_li");
     $.each(languageList, function (i, val) {
-        if(i18nLanguage == $(this).find(".toggleLanguage").attr("title")){
+        if (i18nLanguage == $(this).find(".toggleLanguage").attr("title")) {
             $(".curr_language").text($(this).text());
-        };
+        }
+        if (get_language_icon == $(this).find("use").attr("xlink:href")) {
+            $(".current_icon").find("use").attr("xlink:href", get_language_icon);
+        }
     });
 
     /* Choose a language */
-    $("#language").on('change', function() {
+    $("#language").on('change', function () {
         var language = $(this).children('option:selected').val();
         // console.log("language"+language);
-        getCookie("userLanguage",language,{
+        getCookie("userLanguage", language, {
             expires: 30,
-            path:'/'
+            path: '/'
         });
         location.reload();
     });
@@ -171,15 +177,14 @@ $(function(){
     $(".toggle_language_li").on('click', function () {
         var language = $(this).find(".toggleLanguage").attr("title");
         var language_icon = $(this).find("use").attr("xlink:href");
-        $(".current_icon").find("use").attr("xlink:href",language_icon);
+        $(".current_icon").find("use").attr("xlink:href", language_icon);
         $(".curr_language").text($(this).find(".toggleLanguage").text());
+        $.cookie("language_icon", language_icon, {path: "/"});
         // $("#language").text($(this).find(".toggleLanguage").text());
         getCookie("userLanguage", language, {
             expires: 30,
             path: '/'
         });
-        // location.reload();
-        execI18n();
-        $(".footer_box iframe").contentWindow.execI18n();
+        location.reload();
     });
 });
