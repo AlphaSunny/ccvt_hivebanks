6ae1ef1f3056c93e64a3a12f6b6e6248765b127f
juguohong <1045416962@qq.com>
2019-01-30 13:24:57 +0800

    h5

--- h5/domain/js/domain_list.js
+++ h5/domain/js/domain_list.js
@@ -48,13 +48,14 @@ $(function () {
                     $("#group_list").html(tr);
                     return;
                 }
-
                 $.each(data, function (i, val) {
+                    let scale = "<svg class='icon icon_grade' aria-hidden='true'><use xlink:href='#icon-v" + data[i].scale + "'></use></svg>";
                     tr += "<tr>" +
-                        "<td class='id_name' id=" + data[i].id + " title=" + data[i].name + "><span>" + data[i].name + "</span><span>("+ data[i].type_name +")</span></td>" +
-                        "<td>" + data[i].scale + "</td>" +
+                        "<td class='id_name' id=" + data[i].id + " title=" + data[i].name + "><span>"+ scale +"</span><span>" + data[i].name + "</span></td>" +
+                        // "<td class='id_name' id=" + data[i].id + " title=" + data[i].name + "><span>" + data[i].name + "</span><span>("+ data[i].type_name +")</span></td>" +
+                        // "<td>" + data[i].scale + "</td>" +
                         "<td>🌟" + data[i].glory_number + "</td>" +
-                        // "<td>" + data[i].type_name + "</td>" +
+                        "<td>" + data[i].type_name + "</td>" +
                         "<td><a href='javascript:;' class='to_group_info'>查看</a></td>" +
                         "</tr>";
                 });
