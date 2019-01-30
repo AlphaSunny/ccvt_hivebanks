ecbb42678aec5779699280e373301136bd9d1cb1
juguohong <1045416962@qq.com>
2019-01-30 11:31:33 +0800

    h5

--- h5/domain/js/domain_list.js
+++ h5/domain/js/domain_list.js
@@ -25,7 +25,7 @@ $(function () {
     });
 
     //获取群列表
-    let limit = 10, offset = 0, search_name = "", scale = "", type_id = "";
+    let limit = 50, offset = 0, search_name = "", scale = "", type_id = "";
 
     function getGroupListFun(limit, offset, search_name, scale, type_id) {
         let count = "";
