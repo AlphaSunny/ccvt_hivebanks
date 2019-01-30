c3bc3d72c9a201c9f74746a574b0d432b7ed7515
juguohong <1045416962@qq.com>
2019-01-30 14:39:27 +0800

    h5

--- h5/domain/js/domain_info.js
+++ h5/domain/js/domain_info.js
@@ -151,3 +151,17 @@ $(function () {
         window.location.href = "../honor/chat_person.html?group_id=" + group_id + "&group_name=" + encodeURI(encodeURI(group_name)) + "&domain=1";
     })
 });
+
+/**
+ * Initialization page loading loading
+ */
+window.onload = function () {
+    // $("header").css("background-image", "url(assets/img/banner-1.jpg)");
+    if (document.readyState === 'loading') {
+        document.body.style.overflow = "hidden";
+    } else if (document.readyState === 'interactive' || document.readyState === 'complete') {
+        document.body.style.overflow = "auto";
+        let loading = document.querySelector(".loading");
+        loading.parentNode.removeChild(loading);
+    }
+};
