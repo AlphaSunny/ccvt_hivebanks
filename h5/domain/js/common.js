0f4c574f3939489f60670e9f31afca1c0b5a1fa7
juguohong <1045416962@qq.com>
2019-01-30 13:37:33 +0800

    h5

--- h5/domain/js/common.js
+++ h5/domain/js/common.js
@@ -106,13 +106,13 @@ function ShowLoading(type) {
 /**
  * Initialization page loading loading
  */
-window.onload = function () {
-    // $("header").css("background-image", "url(assets/img/banner-1.jpg)");
-    if (document.readyState === 'loading') {
-        document.body.style.overflow = "hidden";
-    } else if (document.readyState === 'interactive' || document.readyState === 'complete') {
-        document.body.style.overflow = "auto";
-        let loading = document.querySelector(".loading");
-        loading.parentNode.removeChild(loading);
-    }
-};
+// window.onload = function () {
+//     // $("header").css("background-image", "url(assets/img/banner-1.jpg)");
+//     if (document.readyState === 'loading') {
+//         document.body.style.overflow = "hidden";
+//     } else if (document.readyState === 'interactive' || document.readyState === 'complete') {
+//         document.body.style.overflow = "auto";
+//         let loading = document.querySelector(".loading");
+//         loading.parentNode.removeChild(loading);
+//     }
+// };
