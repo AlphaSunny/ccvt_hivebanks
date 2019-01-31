216b0a01ed45442d0355d9c46bb99d9a610a9d50
juguohong <1045416962@qq.com>
2019-01-31 18:08:30 +0800

    h5

--- h5/ca/js/ca_withdraw.js
+++ h5/ca/js/ca_withdraw.js
@@ -20,9 +20,9 @@ $(function () {
             }
 
             $.each(data, function (i, val) {
-                li += "<a class='bankItem list_box_item flex align-items-center justify-content-space-between'>" +
+                li += "<li class='bankItem list_box_item flex align-items-center justify-content-space-between'>" +
                     "<span class='i18n ca_channel' name='" + data[i].ca_channel + "'></span><span> > </span>" +
-                    "</a>"
+                    "</li>"
             });
             $('.ca_channel_ul').html(li);
             execI18n();
