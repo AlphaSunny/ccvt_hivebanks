a4aaf976a0cfbf6d6c8f3411b4ab881f97071327
juguohong <1045416962@qq.com>
2019-02-12 13:54:14 +0800

    h5

--- h5/ca/js/ca_withdraw_info.js
+++ h5/ca/js/ca_withdraw_info.js
@@ -3,15 +3,16 @@ $(function () {
     let token = GetCookie('user_token');
     GetUsAccount();
     GetWithdrawInfo(token, function (response) {
-        if(response.errcode == '0'){
-            let data =response.rows;
-            if(data.qa_flag == '0'){
+        if (response.errcode == '0') {
+            let data = response.rows;
+            if (data.qa_flag == '0') {
                 $('.enablePending').attr('name', 'enablePending');
                 execI18n();
             }
             $('.tx_hash').text(data.tx_hash);
             $('.caWithdrawAmount').text(data.lgl_amount);
             $('.handlingFee').text(data.tx_fee);
+            $('.id_card').text(data.id_card.replace(/(.{4})/g, "$1-"));
             $('.time').text(ToHour(data.tx_time));
         }
     }, function (response) {
