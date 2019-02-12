da28685166c1eaf82da722824f0f6a8d69e4b80f
juguohong <1045416962@qq.com>
2019-02-12 14:50:40 +0800

    h5

--- h5/user/js/index.js
+++ h5/user/js/index.js
@@ -1,8 +1,4 @@
 $(function () {
-    let str = "1234567890129867123";
-    let str2 = str.replace(/(.{4})/g,"$1-").replace(/[-]$/,"");
-    console.log(str2);
-
     let login_us = GetQueryString('user');
     let login_ba = GetQueryString('ba');
     let login_ca = GetQueryString('ca');
