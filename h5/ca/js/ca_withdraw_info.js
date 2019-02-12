1b9cdbce03b2d04048cdb57fefd0f9cbcc8967b7
juguohong <1045416962@qq.com>
2019-02-12 13:39:41 +0800

    h5

--- h5/ca/js/ca_withdraw_info.js
+++ h5/ca/js/ca_withdraw_info.js
@@ -14,7 +14,7 @@ $(function () {
             $('.handlingFee').text(data.tx_fee);
             $('.time').text(data.tx_time);
             console.log(data.tx_time);
-            console.log(new Date(toNumber(data.tx_time))/1000);
+            console.log(new Date(Number(data.tx_time))/1000);
         }
     }, function (response) {
         LayerFun(response.errcode);
