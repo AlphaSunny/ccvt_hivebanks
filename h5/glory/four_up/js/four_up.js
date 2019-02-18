04726cd6af80692ccebd4c3f2004619b4f8d1e38
juguohong <1045416962@qq.com>
2019-02-18 13:23:11 +0800

    h5

--- h5/glory/four_up/js/four_up.js
+++ h5/glory/four_up/js/four_up.js
@@ -1,7 +1,7 @@
 $(function () {
     let url_path = window.location.hostname;
     let url = "https://" + url_path + "/api/crontab/get_scale_us_data.php";
-    let all_list = [];
+    let all_list = [],item_list_arr = [];
     $.ajax(
         {
             type: "GET",
@@ -15,9 +15,10 @@ $(function () {
                 }
                 let item_list = res;
                 $.each(item_list, function (i, val) {
-                    console.log(i);
-                    // console.log(val);
                     console.log(i.indexOf("list_"));
+                    if (i.indexOf("list_") > -1) {
+                        console.log(item_list[i]);
+                    }
                 })
             }
         }
