c882c90f7ff2a7799da43e495bc1a4152519cfe9
juguohong <1045416962@qq.com>
2019-02-18 14:43:21 +0800

    h5

--- h5/glory/four_up/js/four_up.js
+++ h5/glory/four_up/js/four_up.js
@@ -22,8 +22,8 @@ $(function () {
                 });
                 for (let i = 0; i < item_list_arr.length; i++) {
                     let div = $("<div class='up_item'><h2><span>" + level_list[i].split("list_")[1] + "</span>级用户</h2><ul class='item_ul'></ul></div>");
+                    let li = "";
                     for (let j = 0; j < item_list_arr[i].length; j++) {
-                        let li = "";
                         li += "<li>" +
                             "<svg class='icon'><use xlink:href='#icon-lv"+ level_list[i].split("list_")[1] +"'></use></svg>" +
                             "<span title=" + item_list_arr[i][j].wechat + ">" + item_list_arr[i][j].wechat + "</span>" +
