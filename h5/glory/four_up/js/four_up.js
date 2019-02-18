472d4f6fb89ab875d36aa8bb3a67826f0c962b05
juguohong <1045416962@qq.com>
2019-02-18 13:26:57 +0800

    h5

--- h5/glory/four_up/js/four_up.js
+++ h5/glory/four_up/js/four_up.js
@@ -17,6 +17,7 @@ $(function () {
                 $.each(item_list, function (i, val) {
                     console.log(i.indexOf("list_"));
                     if (i.indexOf("list_") > -1) {
+                        console.log(i);
                         console.log(item_list[i]);
                         item_list_arr.push(item_list[i]);
                     }
