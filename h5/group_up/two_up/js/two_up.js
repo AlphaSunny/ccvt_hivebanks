7b449fbe3773b5504952110485abbef743d6ff01
juguohong <1045416962@qq.com>
2019-02-18 17:33:26 +0800

    h5

--- h5/group_up/two_up/js/two_up.js
+++ h5/group_up/two_up/js/two_up.js
@@ -1,14 +1,16 @@
 $(function () {
-   $.ajax({
-       type:"GET",
-       dataType:"json",
-       url:"",
-       success:function (res) {
-           let data = res.all_list;
-           if(data == ""){
-               ErrorPrompt("暂无数据");
-               return;
-           }
-       }
-   })
+    let url_path = window.location.hostname;
+    let url = "https://" + url_path + "/api/crontab/get_scale_us_data.php";
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: url,
+        success: function (res) {
+            let data = res.all_list;
+            if (data == "") {
+                ErrorPrompt("暂无数据");
+                return;
+            }
+        }
+    })
 });
