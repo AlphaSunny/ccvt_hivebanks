80d320031636ed63dcf49c6fad2ebe13db20127f
juguohong <1045416962@qq.com>
2019-02-22 15:45:19 +0800

    h5

--- h5/user/js/login.js
+++ h5/user/js/login.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     function GetLoginCookie(name) {
         let arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
         if (arr != null) {
+            alert(arr);
             return unescape(arr[2]);
         }else{
             DelCookie("user_token");
