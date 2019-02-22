39268abd3f3bbc1012a6a99e93259c2d7541645c
juguohong <1045416962@qq.com>
2019-02-22 18:43:31 +0800

    h5

--- h5/user/js/common.js
+++ h5/user/js/common.js
@@ -14,7 +14,6 @@ function GetCookie(name) {
     let arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
     if (arr != null) return unescape(arr[2]);
     if (arr == null && name == "user_token") {
-        alert(name);
         window.location.href = 'login.html';
     }
 }
