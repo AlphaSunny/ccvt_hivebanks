eabf883f07ad67f2cee48489be3fb40e9137de89
juguohong <1045416962@qq.com>
2019-02-22 14:32:27 +0800

    h5

--- h5/ba/js/common.js
+++ h5/ba/js/common.js
@@ -23,14 +23,6 @@ function GetCookie(name) {
     }
 }
 
-// Delete cookie function
-function DelCookie(name) {
-    var exp = new Date();
-    exp.setTime(exp.getTime() - 1);
-    var cookieVal = GetCookie(name);
-    if (cookieVal != null) document.cookie = name + "=" + cookieVal + ";expires=" + exp.toGMTString() + ';path=/';
-}
-
 // Take the us_cookies function
 function GetUsCookie(name) {
     var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
@@ -43,6 +35,14 @@ function GetUsCookie(name) {
         DelCookie('ba_token');
         window.location.href = 'BaLogin.html';
     }
+}
+
+// Delete cookie function
+function DelCookie(name) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cookieVal = GetCookie(name);
+    if (cookieVal != null) document.cookie = name + "=" + cookieVal + ";expires=" + exp.toGMTString() + ';path=/';
 }
 
 // Get URL parameters
