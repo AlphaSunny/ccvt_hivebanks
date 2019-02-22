beb412d3a7a511bc49e6fe965f457049fb18d4b7
juguohong <1045416962@qq.com>
2019-02-22 18:25:02 +0800

    h5

--- h5/user/js/common.js
+++ h5/user/js/common.js
@@ -14,6 +14,7 @@ function GetCookie(name) {
     let arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
     if (arr != null) return unescape(arr[2]);
     if (arr == null && name == "user_token") {
+        alert(name);
         window.location.href = 'login.html';
     }
 }
