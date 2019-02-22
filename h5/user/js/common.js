f01a53e975fc357656c1a2a0f6738c7fe7301b37
juguohong <1045416962@qq.com>
2019-02-22 16:13:13 +0800

    h5

--- h5/user/js/common.js
+++ h5/user/js/common.js
@@ -13,10 +13,10 @@ function SetCookie(name, value) {
 function GetCookie(name) {
     let arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
     if (arr != null) return unescape(arr[2]);
-    if (arr == null && name == "user_token") {
-        alert("commom文件中不存在user_token" + "com:" + name);
-        //     window.location.href = 'login.html';
-    }
+    // if (arr == null && name == "user_token") {
+    //     alert("commom文件中不存在user_token" + "com:" + name);
+    //     //     window.location.href = 'login.html';
+    // }
 }
 
 // Delete cookie function
