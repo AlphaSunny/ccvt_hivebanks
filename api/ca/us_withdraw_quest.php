90bfb24a2887f18391d73e41c8ac7fea90e6996c
yangguang <epsilonsunny@gmail.com>
2019-02-22 16:06:49 +0800

    on

--- api/ca/us_withdraw_quest.php
+++ api/ca/us_withdraw_quest.php
@@ -48,7 +48,7 @@ if (($rate_row["min_amount"] > $base_amount * get_la_base_unit() || $base_amount
     exit_error('123',"提现金额必须要在ca允许的金额以内");
 
 $us_row = get_us_base_info($us_id);
-die($us_row["base_amount"]);
+//die($us_row["base_amount"]);
 // TODO: 加上500的手续费，在数据库未设置前写死
 if ($us_row["base_amount"] / get_la_base_unit() < $base_amount + 500)
     exit_error("126","用户余额不足");
