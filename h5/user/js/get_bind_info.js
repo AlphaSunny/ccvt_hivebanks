2560ea72725251f22528ba0f75392cb368be01a1
juguohong <1045416962@qq.com>
2019-02-23 13:00:45 +0800

    h5

--- h5/user/js/get_bind_info.js
+++ h5/user/js/get_bind_info.js
@@ -1,15 +1,16 @@
 function GetVerifyBindingInformation(token, type) {
+    let info = "";
     BindingInformation(token, function (response) {
         let data = response.rows;
         $.each(data, function (i, val) {
             if (data[i].bind_name == type && data[i].bind_flag == '1') {
                 // return true;
-                let info = data[i].bind_info;
-                console.log(info);
-                return info;
+                info = data[i].bind_info;
             }
         })
     }, function (response) {
         ErrorPrompt(response.errmsg)
     });
+    console.log(info);
+    return info;
 }
