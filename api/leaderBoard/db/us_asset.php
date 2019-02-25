3e01cc2abf3e9b580f581f2c24d34c2d57c8cfa8
songxingwei <you@example.com>
2019-02-25 12:05:36 +0800

    1

--- api/leaderBoard/db/us_asset.php
+++ api/leaderBoard/db/us_asset.php
@@ -145,14 +145,14 @@ function get_chat_list($data,$offset,$limit)
         $s_time = strtotime(date('Y-m-d 00:00:00'), time());
         $e_time = strtotime(date('Y-m-d 23:59:59'), time());
         $unit = get_la_base_unit();
-        $sql = "select sum(tx_amount)/'{$unit}' as all_am from us_glory_integral_change_log WHERE credit_id='{$v['us_id']}' AND state=1 AND ctime BETWEEN '{$s_time}' AND '{$e_time}'";
+        $sql = "select sum(tx_amount)/'{$unit}' as all_am from us_glory_integral_change_log WHERE debit_id='{$v['us_id']}' AND state=1 AND ctime BETWEEN '{$s_time}' AND '{$e_time}'";
         $db->query($sql);
         $all_zan = $db->getField($sql, 'all_am');
         if (!$all_zan) {
             $all_zan = 0;
         }
         $rows[$k]['all_praise'] = $all_zan;
-        $sql = "select sum(tx_amount)/'{$unit}' as all_am from us_glory_integral_change_log WHERE credit_id='{$v['us_id']}' AND state=2 AND ctime BETWEEN '{$s_time}' AND '{$e_time}'";
+        $sql = "select sum(tx_amount)/'{$unit}' as all_am from us_glory_integral_change_log WHERE debit_id='{$v['us_id']}' AND state=2 AND ctime BETWEEN '{$s_time}' AND '{$e_time}'";
         $db->query($sql);
         $all_cai = $db->getField($sql, 'all_am');
         if (!$all_cai) {
