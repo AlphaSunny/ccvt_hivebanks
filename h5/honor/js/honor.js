82548c76d9d63a5e656de2e2b985375b17be8df6
juguohong <1045416962@qq.com>
2019-02-25 11:39:00 +0800

    h5

--- h5/honor/js/honor.js
+++ h5/honor/js/honor.js
@@ -96,15 +96,16 @@ $(function () {
                         "<span class='none us_id'>" + data[i].us_id + "</span>" +
                         "<svg class='icon zan_icon' aria-hidden='true'><use xlink:href='#icon-zan'></use></svg>" +
                         "<span class='zan_num'>" + data[i].all_praise + "</span>&nbsp;|&nbsp;" +
-                        "<svg class='icon cai_icon' aria-hidden='true'><use xlink:href='#icon-cai'></use></svg>";
-                    if (data[i].us_id == '8D5664EC-2722-B70B-7DF7-80EFE8118CFD') {
-                        tr += "<span class='cai_num'>0</span>";
-                    } else {
-                        tr += "<span class='cai_num'>" + data[i].all_point_on + "</span>";
-                    }
-                    tr +=
-                        "</td>" +
-                        "</tr>"
+                        "<svg class='icon cai_icon' aria-hidden='true'><use xlink:href='#icon-cai'></use></svg>" +
+                        "<span class='cai_num'>" + data[i].all_point_on + "</span>";
+                    // if (data[i].us_id == '8D5664EC-2722-B70B-7DF7-80EFE8118CFD') {
+                    //     tr += "<span class='cai_num'>0</span>";
+                    // } else {
+                    //     tr += "<span class='cai_num'>" + data[i].all_point_on + "</span>";
+                    // }
+                    // tr +=
+                    //     "</td>" +
+                    //     "</tr>"
                 });
                 $("#leaderBoardBody").html(tr);
 
