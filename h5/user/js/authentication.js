b7813cd3688c8f6f857c20993f0f8cd950c13a40
juguohong <1045416962@qq.com>
2019-02-27 17:12:10 +0800

    h5

--- h5/user/js/authentication.js
+++ h5/user/js/authentication.js
@@ -261,17 +261,4 @@ $(function () {
             ErrorPrompt(response.errmsg);
         })
     });
-
-    //Display when selecting a picture
-    // function getObjectURL(file) {
-    //     let url = null;
-    //     if (window.createObjectURL != undefined) { // basic
-    //         url = window.createObjectURL(file);
-    //     } else if (window.URL != undefined) { // mozilla(firefox)
-    //         url = window.URL.createObjectURL(file);
-    //     } else if (window.webkitURL != undefined) { // webkit or chrome
-    //         url = window.webkitURL.createObjectURL(file);
-    //     }
-    //     return url;
-    // }
 });
