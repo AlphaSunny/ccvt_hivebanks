aec3e5127e1f354ecb94028d791f2bfe908c6185
juguohong <1045416962@qq.com>
2019-03-11 11:38:07 +0800

    h5

--- h5/la/assets/js/userList.js
+++ h5/la/assets/js/userList.js
@@ -11,44 +11,41 @@ $(function () {
                 GetDataEmpty('userList', '4');
             }
 
-            $("#userListTable").DataTable({
-                destroy: true,
-                deferRender: true,
-                lengthMenu: [ 10, 20, 50, 70, 100 ],
-                searching:false,//是否显示搜索框
-                info:false,//是否显示表左下角文字
-                language: {
-                    paginate: {
-                        url: "dataTables.german.lang",
-                        first:"<<",
-                        previous: "<",
-                        next: ">",
-                        last:">>",
-                        loadingRecords:"Please wait - loading..",
-                    }
-                },
-                data:data,
-                columns:[
-                    {"data":"us_id", className:"us_id jump"},
-                    {"data":"us_level"},
-                    {"data":"security_level"},
-                    {"data":"ctime"},
-                ],
-            });
-
-            // $.each(data, function (i, val) {
-            //     tr+='<tr>' +
-            //         '<td><a href="javascript:;" class="us_id">'+ data[i].us_id +'</a></td>' +
-            //         // '<td>'+ data[i].base_amount +'&nbsp;<span class="base_type">BTC</span></td>' +
-            //         // '<td>'+ data[i].lock_amount +'&nbsp;<span class="base_type">BTC</span></td>' +
-            //         '<td>'+ data[i].us_level +'</td>' +
-            //         '<td>'+ data[i].security_level +'</td>' +
-            //         // '<td>'+ data[i].utime +'</td>' +
-            //         '<td>'+ data[i].ctime +'</td>' +
-            //         // '<td><a href="#!">编辑</a></td>' +
-            //         '</tr>'
+            // $("#userListTable").DataTable({
+            //     destroy: true,
+            //     deferRender: true,
+            //     lengthMenu: [ 10, 20, 50, 70, 100 ],
+            //     searching:false,//是否显示搜索框
+            //     info:false,//是否显示表左下角文字
+            //     language: {
+            //         paginate: {
+            //             url: "dataTables.german.lang",
+            //             first:"<<",
+            //             previous: "<",
+            //             next: ">",
+            //             last:">>",
+            //             loadingRecords:"Please wait - loading..",
+            //         }
+            //     },
+            //     data:data,
+            //     columns:[
+            //         {"data":"us_id", className:"us_id jump"},
+            //         {"data":"us_level"},
+            //         {"data":"security_level"},
+            //         {"data":"ctime"},
+            //     ],
             // });
-            // $('#userList').html(tr);
+
+            $.each(data, function (i, val) {
+                tr+='<tr>' +
+                    '<td><a href="javascript:;" class="us_id">'+ data[i].us_account +'</a></td>' +
+                    '<td class="none">'+ data[i].us_id +'</td>' +
+                    '<td>'+ data[i].us_level +'</td>' +
+                    '<td>'+ data[i].security_level +'</td>' +
+                    '<td>'+ data[i].ctime +'</td>' +
+                    '</tr>'
+            });
+            $('#userList').html(tr);
         }
     }, function (response) {
         GetDataFail('userList', '4');
