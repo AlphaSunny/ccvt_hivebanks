b806c51a5a359f5c7c178a61b44f8a01dc1db336
juguohong <1045416962@qq.com>
2019-03-11 12:53:42 +0800

    h5

--- h5/la/assets/js/userList.js
+++ h5/la/assets/js/userList.js
@@ -1,60 +1,61 @@
 $(function () {
     //get token
-    var token = GetCookie('la_token');
+    let token = GetCookie('la_token');
 
     //get user list
-    var api_url = 'user_list.php', limit = 10, offset = 0, n = 0;
-    GetUserList(token, api_url, limit, offset, function (response) {
-        if(response.errcode == '0'){
-            var data = response.rows, tr = '';
-            if(data == false){
-                GetDataEmpty('userList', '4');
+    let api_url = 'user_list.php', limit = 10, offset = 0, count = "", tr = "";
+
+    function GetUserListFun(token, limit, offset) {
+        GetUserList(token, api_url, limit, offset, function (response) {
+            if (response.errcode == '0') {
+                let data = response.rows;
+                let total = response.total;
+                let totalPage = Math.ceil(total / limit);
+                if (totalPage <= 1) {
+                    count = 1;
+                } else if (1 < totalPage && totalPage <= 6) {
+                    count = totalPage;
+                } else {
+                    count = 6;
+                }
+                if (data == false) {
+                    GetDataEmpty('userList', '4');
+                }
+                $.each(data, function (i, val) {
+                    tr += '<tr>' +
+                        '<td><a href="javascript:;" class="us_id">' + data[i].us_account + '</a></td>' +
+                        '<td class="none us_id">' + data[i].us_id + '</td>' +
+                        '<td>' + data[i].us_level + '</td>' +
+                        '<td>' + data[i].security_level + '</td>' +
+                        '<td>' + data[i].ctime + '</td>' +
+                        '</tr>'
+                });
+                $('#userList').html(tr);
+                $("#pagination").pagination({
+                    currentPage: (limit + offset) / limit,
+                    totalPage: totalPage,
+                    isShow: false,
+                    count: count,
+                    prevPageText: "<<",
+                    nextPageText: ">>",
+                    callback: function (current) {
+                        GetUserListFun(token, limit, (current - 1) * limit);
+                        ShowLoading("show");
+                    }
+                });
             }
+        }, function (response) {
+            GetDataFail('userList', '4');
+            LayerFun(response.errcode);
+        });
+    }
 
-            // $("#userListTable").DataTable({
-            //     destroy: true,
-            //     deferRender: true,
-            //     lengthMenu: [ 10, 20, 50, 70, 100 ],
-            //     searching:false,//是否显示搜索框
-            //     info:false,//是否显示表左下角文字
-            //     language: {
-            //         paginate: {
-            //             url: "dataTables.german.lang",
-            //             first:"<<",
-            //             previous: "<",
-            //             next: ">",
-            //             last:">>",
-            //             loadingRecords:"Please wait - loading..",
-            //         }
-            //     },
-            //     data:data,
-            //     columns:[
-            //         {"data":"us_id", className:"us_id jump"},
-            //         {"data":"us_level"},
-            //         {"data":"security_level"},
-            //         {"data":"ctime"},
-            //     ],
-            // });
+    GetUserListFun(token, limit, offset);
 
-            $.each(data, function (i, val) {
-                tr+='<tr>' +
-                    '<td><a href="javascript:;" class="us_id">'+ data[i].us_account +'</a></td>' +
-                    '<td class="none">'+ data[i].us_id +'</td>' +
-                    '<td>'+ data[i].us_level +'</td>' +
-                    '<td>'+ data[i].security_level +'</td>' +
-                    '<td>'+ data[i].ctime +'</td>' +
-                    '</tr>'
-            });
-            $('#userList').html(tr);
-        }
-    }, function (response) {
-        GetDataFail('userList', '4');
-        LayerFun(response.errcode);
-    });
 
     //Jump user details
     $(document).on('click', '.us_id', function () {
-        var us_id = $(this).text();
+        let us_id = $(this).siblings(".us_id").text();
         window.location.href = 'userInfo.html?us_id=' + us_id;
     })
 });
