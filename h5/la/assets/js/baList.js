042f18953e91ebdf4245344e5df40f45f14326fb
juguohong <1045416962@qq.com>
2019-03-11 13:49:38 +0800

    h5

--- h5/la/assets/js/baList.js
+++ h5/la/assets/js/baList.js
@@ -1,67 +1,65 @@
 $(function () {
     //Get token
-    var token = GetCookie('la_token');
+    let token = GetCookie('la_token');
 
     //get ba list
-    var api_url = 'ba_list.php', limit = 10, offset = 0, n = 0;
-    GetUserList(token, api_url, limit, offset, function (response) {
-        if (response.errcode == '0') {
-            var data = response.rows, tr = '';
-            if (data == false) {
-                GetDataEmpty('baList', '5');
-                return;
-            }
+    let api_url = 'ba_list.php', limit = 10, offset = 0;
+    function GetUserListFun (limit, offset){
+        let totalPage = "", count = "", tr = "";
+        GetUserList(token, api_url, limit, offset, function (response) {
+            if (response.errcode == '0') {
+                let data = response.rows;
+                if (data == false) {
+                    GetDataEmpty('baList', '5');
+                    return;
+                }
 
-            $("#baListTable").DataTable({
-                destroy: true,
-                deferRender: true,
-                lengthMenu: [10, 20, 50, 70, 100],
-                searching: false,//是否显示搜索框
-                info: false,//是否显示表左下角文字
-                language: {
-                    paginate: {
-                        url: "dataTables.german.lang",
-                        first: "<<",
-                        previous: "<",
-                        next: ">",
-                        last: ">>",
-                        loadingRecords: "Please wait - loading..",
+                let total = response.total;
+                totalPage = Math.ceil(total / limit);
+                if (totalPage <= 1) {
+                    count = 1;
+                } else if (1 < totalPage && totalPage <= 6) {
+                    count = totalPage;
+                } else {
+                    count = 6;
+                }
+
+                $.each(data, function (i, val) {
+                    tr += '<tr class="baListItem">' +
+                        '<td><a href="javascript:;" class="ba_id">' + data[i].ba_id + '</a></td>' +
+                        '<td><a href="javascript:;" class="ba_type">' + data[i].ba_type + '</a></td>' +
+                        '<td>' + data[i].ba_level + '</td>' +
+                        '<td>' + data[i].security_level + '</td>' +
+                        '<td>' + data[i].ctime + '</td>' +
+                        '</tr>'
+                });
+
+                $('#baList').html(tr);
+
+                $("#pagination").pagination({
+                    currentPage: (limit + offset) / limit,
+                    totalPage: totalPage,
+                    isShow: false,
+                    count: count,
+                    prevPageText: "<<",
+                    nextPageText: ">>",
+                    callback: function (current) {
+                        GetUserListFun(token, limit, (current - 1) * limit);
+                        ShowLoading("show");
                     }
-                },
-                data: data,
-                columns: [
-                    {
-                        "data": "ba_id",
-                        className: "ba_id jump",
-                    },
-                    {"data": "ba_type",className:"ba_type"},
-                    {"data": "ba_level"},
-                    {"data": "security_level"},
-                    {"data": "ctime"}
-                ],
-            });
-
-            // $.each(data, function (i, val) {
-            //     tr += '<tr class="baListItem">' +
-            //         '<td><a href="javascript:;" class="ba_id">' + data[i].ba_id + '</a></td>' +
-            //         '<td><a href="javascript:;" class="ba_type">' + data[i].ba_type + '</a></td>' +
-            //         '<td>' + data[i].ba_level + '</td>' +
-            //         '<td>' + data[i].security_level + '</td>' +
-            //         '<td>' + data[i].ctime + '</td>' +
-            //         '</tr>'
-            // });
-            //
-            // $('#baList').html(tr);
-        }
-    }, function (response) {
-        GetDataFail('baList', '5');
-        LayerFun(response.errcode);
-        return;
-    });
+                });
+            }
+        }, function (response) {
+            GetDataFail('baList', '5');
+            LayerFun(response.errcode);
+            return;
+        });
+    }
+    GetUserListFun(limit, offset);
 
     //Jump user details
     $(document).on('click', '.ba_id', function () {
-        var ba_id = $(this).text(), ba_type = $(this).parents('tr').find('.ba_type').text();
+        let ba_id = $(this).text(), ba_type = $(this).parents('tr').find('.ba_type').text();
         window.location.href = 'baInfo.html?ba_id=' + ba_id + '&ba_type=' + ba_type;
     })
 });
