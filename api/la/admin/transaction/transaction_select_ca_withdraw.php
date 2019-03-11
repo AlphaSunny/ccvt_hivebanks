c281691f4c7e6ee429e21c868c2c35ad3e4107af
songxingwei <you@example.com>
2019-03-11 18:10:53 +0800

    1

--- api/la/admin/transaction/transaction_select_ca_withdraw.php
+++ api/la/admin/transaction/transaction_select_ca_withdraw.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once "../../../inc/common.php";
-require_once  "db/us_ca_recharge_request.php";
 require_once  "db/us_ca_withdraw_request.php";
 require_once "db/la_admin.php";
+require_once "../../db/la_func_common.php";
 
 header("cache-control:no-cache,must-revalidate");
 header("Content-Type:application/json;charset=utf-8");
@@ -93,24 +93,8 @@ chk_empty_args('GET', $args);
 
 // 用户token
 $token = get_arg_str('GET', 'token', 128);
-
-$key = Config::TOKEN_KEY;
-// 获取token并解密
-$des = new Des();
-$decryption_code = $des -> decrypt($token, $key);
-$now_time = time();
-$code_conf =  explode(',',$decryption_code);
-// 获取token中的需求信息
-$user = $code_conf[0];
-$timestamp = $code_conf[1];
-if($timestamp < $now_time){
-    exit_error('114','Token timeout please retrieve!');
-}
-//判断la是否存在
-$row = get_la_by_user($user);
-if(!$row){
-    exit_error('120','用户不存在');
-}
+//la用户检查
+la_user_check($token);
 $from_time     =   get_arg_str('GET', 'from_time');
 $to_time       =   get_arg_str('GET', 'to_time');
 $qa_id         =   get_arg_str('GET', 'qa_id ');
@@ -128,12 +112,16 @@ $tx_time       =   get_arg_str('GET', 'tx_time');
 $qa_flag       =   get_arg_str('GET', 'qa_flag');
 $tx_hash       =   get_arg_str('GET', 'tx_hash');
 
-$recharge_rows_ca = get_us_ca_recharge_log_balance_limt($from_time,
+// 取得分页参数
+list($limit, $offset) = get_paging_arg('GET');
+
+// 记录数组总数
+$total = get_us_ca_withdraw_log_balance_limt_total($from_time,
     $to_time,
     $ca_id,
     $qa_id,
     $us_id,
-    $ca_account_id,
+    $us_account_id,
     $lgl_amount,
     $base_amount,
     $tx_type,
@@ -153,25 +141,20 @@ $withdraw_rows_ca = get_us_ca_withdraw_log_balance_limt($from_time,
     $tx_detail,
     $tx_fee,
     $qa_flag,
-    $tx_hash);
+    $tx_hash,
+    $offset,
+    $limit);
 
-$row_recharge = array();
-$row_withdraw = array();
-$rows =array();
-foreach ($recharge_rows_ca as $recharge_row_ca){
-    $recharge_row_ca['tx_time'] = date("Y-m-d H:i:s",$recharge_row_ca['tx_time']);
-    $row_recharge[]=$recharge_row_ca;
+
+
+foreach ($withdraw_rows_ca as $k=>$v){
+    $withdraw_rows_ca[$k]['tx_time'] = date("Y-m-d H:i:s",$v['tx_time']);
 }
-foreach ($withdraw_rows_ca as $withdraw_row_ca){
-    $withdraw_row_ca['tx_time'] = date("Y-m-d H:i:s",$withdraw_row_ca['tx_time']);
-    $row_withdraw[]=$withdraw_row_ca;
-}
-$rows['recharge'] = $row_recharge;
-$rows['withdraw'] = $row_withdraw;
 
 $rtn_ary = array();
 $rtn_ary['errcode'] = '0';
 $rtn_ary['errmsg'] = '';
-$rtn_ary['rows'] = $rows;
+$rtn_ary['total'] = $total;
+$rtn_ary['rows'] = $withdraw_rows_ca;
 $rtn_str = json_encode($rtn_ary);
 php_end($rtn_str);
