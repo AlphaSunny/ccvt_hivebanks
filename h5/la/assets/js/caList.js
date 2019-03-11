79275a68b1c8282c75e3dd4db1a11f8da4bd0eae
juguohong <1045416962@qq.com>
2019-03-11 16:34:30 +0800

    h5

--- h5/la/assets/js/caList.js
+++ h5/la/assets/js/caList.js
@@ -1,54 +1,59 @@
 $(function () {
     //Get token
-    var token = GetCookie('la_token');
+    let token = GetCookie('la_token');
 
     //Get the list of users
-    var api_url = 'ca_list.php', limit = 10, offset = 0, n = 0;
-    GetUserList(token, api_url, limit, offset, function (response) {
-        if (response.errcode == '0') {
-            var data = response.rows, tr = '';
-            $("#caListTable").DataTable({
-                destroy: true,
-                deferRender: true,
-                lengthMenu: [ 10, 20, 50, 70, 100 ],
-                searching:false,//是否显示搜索框
-                info:false,//是否显示表左下角文字
-                language: {
-                    paginate: {
-                        url: "dataTables.german.lang",
-                        first:"<<",
-                        previous: "<",
-                        next: ">",
-                        last:">>",
-                        loadingRecords:"Please wait - loading..",
+    let api_url = 'ca_list.php', limit = 10, offset = 0;
+
+    function GetUserListFun(limit, offset) {
+        let tr = "",totalPage = "",count = "";
+        GetUserList(token, api_url, limit, offset, function (response) {
+            ShowLoading("hide");
+            if (response.errcode == '0') {
+                let data = response.rows;
+                let total = response.total;
+                totalPage = Math.ceil(total / limit);
+                if (totalPage <= 1) {
+                    count = 1;
+                } else if (1 < totalPage && totalPage <= 6) {
+                    count = totalPage;
+                } else {
+                    count = 6;
+                }
+                $.each(data, function (i, val) {
+                    tr += '<tr>' +
+                        '<td><a href="javascript:;" class="ca_account">' + data[i].ca_account + '</a><span class="ca_id">' + data[i].ca_id + '</span></td>' +
+                        '<td>' + data[i].ca_level + '</td>' +
+                        '<td>' + data[i].security_level + '</td>' +
+                        '<td>' + data[i].ctime + '</td>' +
+                        '</tr>'
+                });
+                $('#caList').html(tr);
+                $("#pagination").pagination({
+                    currentPage: (limit + offset) / limit,
+                    totalPage: totalPage,
+                    isShow: false,
+                    count: count,
+                    prevPageText: "<<",
+                    nextPageText: ">>",
+                    callback: function (current) {
+                        GetUserListFun(limit, (current - 1) * limit);
+                        ShowLoading("show");
                     }
-                },
-                data:data,
-                columns:[
-                    {"data":"ca_id", className:"ca_id jump"},
-                    {"data":"ca_level"},
-                    {"data":"security_level"},
-                    {"data":"ctime"}
-                ],
-            });
-            // $.each(data, function (i, val) {
-            //     tr += '<tr>' +
-            //         '<td><a href="javascript:;" class="ca_id">' + data[i].ca_id + '</a></td>' +
-            //         '<td>' + data[i].ca_level + '</td>' +
-            //         '<td>' + data[i].security_level + '</td>' +
-            //         '<td>' + data[i].ctime + '</td>' +
-            //         '</tr>'
-            // });
-            // $('#caList').html(tr);
-        }
-    }, function (response) {
-        LayerFun(response.errcode);
-        return;
-    });
+                });
+            }
+        }, function (response) {
+            ShowLoading("hide");
+            LayerFun(response.errcode);
+            return;
+        });
+    }
+
+    GetUserListFun(limit, offset);
 
     //Jump user details
-    $(document).on('click', '.ca_id', function () {
-        var ca_id = $(this).text();
+    $(document).on('click', '.ca_account', function () {
+        let ca_id = $(this).siblings(".ca_id").text();
         window.location.href = 'caInfo.html?ca_id=' + ca_id;
     })
 });
