97e22a9aacca75cfabcb739f66f6969cb0f727c2
juguohong <1045416962@qq.com>
2019-03-14 18:53:47 +0800

    h5

--- h5/la/assets/js/voucher.js
+++ h5/la/assets/js/voucher.js
@@ -27,7 +27,7 @@ $(function () {
                 }
 
                 $.each(data, function (i, val) {
-                    console.log(data);
+                    console.log(val);
                     //是否有效
                     if (data[i].is_effective == "1") {
                         is_effective = "有效";
