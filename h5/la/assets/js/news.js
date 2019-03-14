5c8c2f58a0a6f7072a99192a96a065b72f6d1791
juguohong <1045416962@qq.com>
2019-03-14 10:13:44 +0800

    h5

--- h5/la/assets/js/news.js
+++ h5/la/assets/js/news.js
@@ -5,7 +5,7 @@ $(function () {
     let limit = 50, offset = 0;
 
     function GetNewsListFun(limit, offset) {
-        let total = "", totalPage = "", count = "", tr = "";
+        let total = "", totalPage = "", count = "", tr = "",overdue_time = "";
         GetNewsList(token, limit, offset, function (response) {
             if (response.errcode == "0") {
                 ShowLoading("hide");
@@ -26,11 +26,16 @@ $(function () {
                 }
 
                 $.each(data, function (i, val) {
+                    if(data[i].overdue_time == "0"){
+                        overdue_time = "";
+                    }else{
+                        overdue_time = data[i].overdue_time;
+                    }
                     tr += "<tr class='newsItem'>" +
                         "<td><a href='newsDetail.html?news_id=" + data[i].news_id + "' class='newsTitleClick'>" + data[i].title + "</a></td>" +
                         "<td class='text-center'><span>" + data[i].author + "</span></td>" +
                         "<td class='text-center'><span>" + data[i].utime + "</span></td>" +
-                        // "<td class='text-center'><span>" + data[i].utime + "</span></td>" +
+                        "<td class='text-center'><span>" + overdue_time + "</span></td>" +
                         "<td class='text-center'>" +
                         "<span class='news_id none'>" + data[i].news_id + "</span>" +
                         "<button class='btn btn-success modifyNewsBtn i18n' name='modify'>modify</button>" +
