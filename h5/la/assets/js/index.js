1ce0233edaded5c5a2b4542645d08b7a80f16720
juguohong <1045416962@qq.com>
2019-03-15 11:04:41 +0800

    h5

--- h5/la/assets/js/index.js
+++ h5/la/assets/js/index.js
@@ -84,6 +84,8 @@ $(function () {
         let ca_scale = (sum_ca_base_amount / sum_total_base_amount).toFixed(3) * 100;
         let la_scale = (sum_la_base_amount / sum_total_base_amount).toFixed(3) * 100;
 
+        console.log((sum_us_base_amount / sum_total_base_amount).toFixed(3) * 100);
+
         $('#user_percent').attr('data-percent', user_scale);
         $('#ba_percent').attr('data-percent', ba_scale);
         $('#ca_percent').attr('data-percent', ca_scale);
