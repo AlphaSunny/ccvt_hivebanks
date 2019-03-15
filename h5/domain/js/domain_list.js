62939cc1a00f7e9c7799d1f86fd7cf92ab1989d8
juguohong <1045416962@qq.com>
2019-03-15 10:25:45 +0800

    h5

--- h5/domain/js/domain_list.js
+++ h5/domain/js/domain_list.js
@@ -57,6 +57,7 @@ $(function () {
                         "<td>🌟" + data[i].glory_number + "</td>" +
                         "<td>" + data[i].type_name + "</td>" +
                         "<td data-toggle='tooltip' data-placement='bottom' data-original-title='24小时内CCVT发放数量'>"+ data[i].send_amount +"</td>" +
+                        "<td data-toggle='tooltip' data-placement='bottom' data-original-title='24小时内的聊天数量'>"+ data[i].all_num +"</td>" +
                         "</tr>";
                 });
                 $("#group_list").html(tr);
