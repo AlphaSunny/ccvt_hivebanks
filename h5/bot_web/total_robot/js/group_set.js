73484e6581ec187beea961c9257c0a3071f615d1
juguohong <1045416962@qq.com>
2019-03-18 17:25:40 +0800

    h5

--- h5/bot_web/total_robot/js/group_set.js
+++ h5/bot_web/total_robot/js/group_set.js
@@ -8,6 +8,20 @@ $(function () {
         $(".input_box").addClass("none");
     });
 
+    //编辑欢迎语
+    $(".cancel_edit_welcome_text").click(function () {
+        $(".welcome_text_box").addClass("none");
+    });
+
+    //编辑群介绍
+    $(".edit_group_introduction").click(function () {
+        $(".group_introduction_box").removeClass("none");
+    });
+
+    $(".cancel_edit_group_introduction_btn").click(function () {
+        $(".group_introduction_box").addClass("none");
+    });
+
     //监听开关按钮状态
     $("#runSwitch").on("change", function () {
         if ($(this).val() == "1") {
