6a249365b997d87a6cd5765ebedc6cda50612978
juguohong <1045416962@qq.com>
2019-03-18 17:22:56 +0800

    h5

--- h5/bot_web/total_robot/js/group_set.js
+++ h5/bot_web/total_robot/js/group_set.js
@@ -1,4 +1,13 @@
 $(function () {
+    //编辑群名字
+    $(".edit_group_name").click(function () {
+        $(".input_box").removeClass("none");
+    });
+
+    $(".cancel_edit_group_name_btn").click(function () {
+        $(".input_box").addClass("none");
+    });
+
     //监听开关按钮状态
     $("#runSwitch").on("change", function () {
         if ($(this).val() == "1") {
