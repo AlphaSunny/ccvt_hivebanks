68f2fd47add6a43cb383c1a3b1f6479d6f9c9d5b
juguohong <1045416962@qq.com>
2019-03-18 17:56:45 +0800

    h5

--- h5/bot_web/total_robot/js/common.js
+++ h5/bot_web/total_robot/js/common.js
@@ -176,6 +176,16 @@ function ReviewGroup(token, review_group_id, is_audit, why, suc_func, error_func
     CallRobotApi(api_url, post_data, suc_func, error_func);
 }
 
+//获取群信息状态
+function GetGroupInfo(token, group_id, suc_func, error_func) {
+    var api_url = 'group_info.php',
+        post_data = {
+            'token': token,
+            'group_id': group_id
+        };
+    CallRobotApi(api_url, post_data, suc_func, error_func);
+}
+
 //编辑群信息
 function EditGroup(token, group_name, del, flirt, group_id, send_address, bind_account_notice, is_welcome, welcome, ranking_change_switch, src, group_introduction, news_notice, suc_func, error_func) {
     let api_url = "group_edit.php",
--- h5/bot_web/total_robot/js/group_set.js
+++ h5/bot_web/total_robot/js/group_set.js
@@ -1,5 +1,18 @@
 $(function () {
     let token = GetCookie("user_token");
+    let group_id = GetQueryString("group_id");
+
+    //获取群状态信息
+    function GetGroupInfoFun() {
+        GetGroupInfo(token, group_id, function (response) {
+            console.log(response);
+        }, function (response) {
+            ErrorPrompt(response.errmsg);
+        })
+    }
+
+    GetGroupInfoFun();
+
     //编辑群名字
     $(".edit_group_name").click(function () {
         $(".input_box").removeClass("none");
@@ -23,12 +36,12 @@ $(function () {
         $(".group_introduction_box").addClass("none");
     });
 
-    let group_name = "", del = "", flirt = "", send_address = "", bind_account_notice = "",news_notice = "",
-        is_welcome = "", welcome = "", ranking_change_switch = "", src = "", group_introduction = "";
-    let group_id = GetCookie("group_id");
+    // let group_name = "", del = "", flirt = "", send_address = "", bind_account_notice = "", news_notice = "",
+    //     is_welcome = "", welcome = "", ranking_change_switch = "", src = "", group_introduction = "";
+    // let group_id = GetCookie("group_id");
 
     function EditGroupFun() {
-        EditGroup(token, group_name, del, flirt, group_id, send_address, bind_account_notice, is_welcome, welcome, ranking_change_switch, src, group_introduction,news_notice, function (response) {
+        EditGroup(token, group_name, del, flirt, group_id, send_address, bind_account_notice, is_welcome, welcome, ranking_change_switch, src, group_introduction, news_notice, function (response) {
             if (response.errcode == "0") {
                 SuccessPrompt("设置成功");
                 $(".welcome_text_box,.input_box,.group_introduction_box").addClass("none");
