5bb8ec6d6f1800553bef50f94b952853cb517409
songxingwei <you@example.com>
2019-03-18 11:42:02 +0800

    1

--- api/bot/db/bot_bind.php
+++ api/bot/db/bot_bind.php
@@ -564,7 +564,7 @@ function get_news()
     $db->query($sql);
     $row = $db->fetchRow();
     if ($row['overdue_time']<$time){
-        $sql = "select news_id,title from la_news WHERE category=1 AND status=1 AND overdue_time=0 ORDER BY rand() limit 1";
+        $sql = "select news_id,title from la_news WHERE category=1 AND status=1 AND overdue_time>'{$time}' ORDER BY rand() limit 1";
         $db->query($sql);
         $row = $db->fetchRow();
     }
