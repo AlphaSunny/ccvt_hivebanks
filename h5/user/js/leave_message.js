ffc5bb5a8815a7745595f9ae969d4009f321b1d4
juguohong <1045416962@qq.com>
2019-03-18 16:01:15 +0800

    h5

--- h5/user/js/leave_message.js
+++ h5/user/js/leave_message.js
@@ -5,5 +5,9 @@ $(function () {
         let length = $(this).val().length;
         console.log(length);
         $(".input_num").text(length);
+        if (length > 140) {
+            WarnPrompt("最多输入140个字符");
+            return;
+        }
     });
 });
