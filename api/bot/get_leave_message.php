e6ecdc3d4cc0add900132557d630f7cd676e66d7
songxingwei <you@example.com>
2019-03-19 18:25:33 +0800

    1

---
+++ api/bot/get_leave_message.php
@@ -0,0 +1,30 @@
+<?php
+require_once '../inc/common.php';
+require_once 'db/bot_bind.php';
+header("cache-control:no-cache,must-revalidate");
+header("Content-Type:application/json;charset=utf-8");
+
+/*
+========================== 获取前十留言 ==========================
+
+*/
+
+php_begin();
+
+//获取最新文章
+$rows = get_leave_message();
+$content = '';
+if ($rows){
+    $content = $rows['title'].$data['api_url']."/h5/newsInfo.html?news_id=".$rows['news_id'];
+}
+$rtn_ary = array();
+$rtn_ary['errcode'] = '0';
+$rtn_ary['errmsg'] = '';
+$rtn_ary['content'] = $content;
+$rtn_str = json_encode($rtn_ary);
+php_end($rtn_str);
+
+
+
+
+?>
