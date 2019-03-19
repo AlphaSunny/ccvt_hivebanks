5aaf6124943c98f881655cff4984de6c74f2c5b7
songxingwei <you@example.com>
2019-03-19 18:35:24 +0800

    1

--- api/bot/get_leave_message.php
+++ api/bot/get_leave_message.php
@@ -13,14 +13,14 @@ php_begin();
 
 //获取最新文章
 $rows = get_leave_message();
-$content = '';
-if ($rows){
-    $content = $rows['title'].$data['api_url']."/h5/newsInfo.html?news_id=".$rows['news_id'];
-}
+//$content = '';
+//if ($rows){
+//    $content = $rows['title'].$data['api_url']."/h5/newsInfo.html?news_id=".$rows['news_id'];
+//}
 $rtn_ary = array();
 $rtn_ary['errcode'] = '0';
 $rtn_ary['errmsg'] = '';
-$rtn_ary['content'] = $content;
+$rtn_ary['content'] = $rows;
 $rtn_str = json_encode($rtn_ary);
 php_end($rtn_str);
 
