855f7e42fb829920654943e90eb7418cbb878b87
songxingwei <you@example.com>
2019-03-20 14:25:01 +0800

    1

--- api/bot/get_leave_message.php
+++ api/bot/get_leave_message.php
@@ -13,10 +13,10 @@ php_begin();
 
 //获取最新文章
 $rows = get_leave_message();
-$content = "TOP10:"." \r";
+$content = "TOP10:"." \n";
 if ($rows){
     foreach ($rows as $k=>$v){
-        $content = $content.($k+1)."、".$v['wechat'].":".($v['leave_message'] == null ? "未设置" : $v['leave_message'])." \r";
+        $content = $content.($k+1)."、".$v['wechat'].":".($v['leave_message'] == null ? "未设置" : $v['leave_message'])." \n";
     }
 }
 $rtn_ary = array();
