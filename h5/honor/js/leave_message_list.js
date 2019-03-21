a4762ca4925e461f0f95186baf29a1afbc48de34
juguohong <1045416962@qq.com>
2019-03-21 14:23:10 +0800

    h5

--- h5/honor/js/leave_message_list.js
+++ h5/honor/js/leave_message_list.js
@@ -1,11 +1,41 @@
 $(function () {
     LeaveMessageList(function (response) {
-        let li = "", name = "", text = "",new_arr = [];
+        let li = "", name = "", text = "", new_arr = [
+            {
+                scale: 3,
+                wechat: "3范德萨发发",
+                leave_message: "范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发"
+            },
+            {
+                scale: 2,
+                wechat: "2范德萨发发",
+                leave_message: "范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发"
+            },
+            {
+                scale: 1,
+                wechat: "1范德萨发发",
+                leave_message: "范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发"
+            },
+            {
+                scale: 4,
+                wechat: "4范德萨发发",
+                leave_message: "范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发范德萨发发"
+            },
+        ];
         let data = response.rows;
         console.log(data);
-        $.each(data, function (i, val) {
-            console.log(val.leave_message);
+        $.each(data, function (index, item) {
+            if (item.leave_message) {
+                new_arr.push(item);
+            }
         });
+        $.each(new_arr, function (index, item) {
+            li += "<li>" +
+                "<svg class='icon icon_grade' aria-hidden='true'><use xlink:href='#icon-v" + item.scale + "'></use></svg>" +
+                "<span class='name'>" + item.wechat + "</span>:&nbsp;&nbsp;" +
+                "<span class='message'>" + item.leave_message + "</span>" +
+                "</li>";
+        })
     }, function (response) {
         layer.msg("留言获取失败");
     })
