089694e4055cd5357cc5f75953d6af1768816935
juguohong <1045416962@qq.com>
2019-03-21 14:09:42 +0800

    h5

--- h5/honor/js/leave_message_list.js
+++ h5/honor/js/leave_message_list.js
@@ -1,6 +1,15 @@
 $(function () {
     LeaveMessageList(function (response) {
         console.log(response);
+        let li = "", name = "", text = "",new_arr = [];
+        let data = response.rows;
+        $.each(data, function (i, val) {
+            if (data[i].leave_message) {
+                new_arr.push(data[i]);
+                console.log(i);
+            }
+            console.log(new_arr);
+        });
     }, function (response) {
         layer.msg("留言获取失败");
     })
