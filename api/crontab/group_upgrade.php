ad1b883467754b6c670861e8e6d8a20a31aea496
songxingwei <you@example.com>
2019-03-22 14:02:11 +0800

    1

--- api/crontab/group_upgrade.php
+++ api/crontab/group_upgrade.php
@@ -28,7 +28,7 @@ if ($scale_list){
                 //echo $b['name']."&nbsp;&nbsp;当前绑定数:".$b['bind_count']."&nbsp;&nbsp;下级所需绑定数:".$next_group_level['bind_number']."&nbsp;&nbsp;当前星数:".$all_glory_number."&nbsp;&nbsp;下级所需星数:".$next_group_level['glory_number']."<br />";
                 if (($b['bind_count']>=$next_group_level['bind_number']) && ($all_glory_number>=$next_group_level['glory_number'])){
                     echo $b['name']."&nbsp;&nbsp;&nbsp;".$v['scale']."--".($v['scale']+1).'<br />';
-                    scale_upgrade($b['id'],$v['scale'],$v['scale']+1,$b['bind_count'],$all_glory_number);
+                    //scale_upgrade($b['id'],$v['scale'],$v['scale']+1,$b['bind_count'],$all_glory_number);
                 }
             }
         }
