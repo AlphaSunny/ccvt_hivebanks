4043887689a1f818ad55e874385bf66424a2f6c1
songxingwei <you@example.com>
2019-03-25 11:41:54 +0800

    1

--- api/bot/db/bot_bind.php
+++ api/bot/db/bot_bind.php
@@ -1747,10 +1747,11 @@ function random_reward($group_id){
         }else{
             //判断群前一个小时聊天
             $bot_start_time = time()-(60*60);
-//            $sql = "select wechat from bot_message WHERE group_id='{$group_id}' and bot_create_time BETWEEN '{$bot_start_time}' AND '{$time}' and wechat in (select wechat from us_base where 1) group by wechat";
-            $sql ="select a.wechat from bot_message as a left join us_base as b on a.wechat=b.wechat WHERE group_id='{$group_id}' and a.wechat!='AI大白' AND bot_create_time BETWEEN '{$bot_start_time}' AND '{$time}'  and a.wechat in (select wechat from us_base where 1) and b.us_id in (select us_id from us_bind where bind_name='group' and bind_info='{$group_id}') group by a.wechat,b.us_id";
+            $sql = "select a.wechat from bot_message as a left join us_base as b on a.wechat=b.wechat WHERE group_id='81' and a.wechat!='AI大白'  and a.wechat in (select wechat from us_base where 1) and b.us_id in (select us_id from us_bind where bind_name='group' and bind_info='81') group by a.wechat order  by count(a.bot_message_id) asc;";
+            //$sql ="select a.wechat from bot_message as a left join us_base as b on a.wechat=b.wechat WHERE group_id='{$group_id}' and a.wechat!='AI大白' AND bot_create_time BETWEEN '{$bot_start_time}' AND '{$time}'  and a.wechat in (select wechat from us_base where 1) and b.us_id in (select us_id from us_bind where bind_name='group' and bind_info='{$group_id}') group by a.wechat,b.us_id";
             $db->query($sql);
             $array = $db->fetchAll();
+            print_r($array);die();
             $wechat_array = array_map(function($val){return $val['wechat'];}, $array);
             if (count($wechat_array)>0){
                 $rand_num = array_rand($wechat_array,1);
