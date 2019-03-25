1684befeb9d056acad3209f3963bfe986b7458db
juguohong <1045416962@qq.com>
2019-03-25 17:11:16 +0800

    h5

--- h5/la/assets/js/userList.js
+++ h5/la/assets/js/userList.js
@@ -28,6 +28,7 @@ $(function () {
                     tr += '<tr>' +
                         '<td class="text-left"><a href="javascript:;" class="to_us">' + data[i].us_account + '</a><span class="none us_id">' + data[i].us_id + '</span></td>' +
                         '<td>' + data[i].base_amount + '</td>' +
+                        '<td>' + data[i].us_nm + '</td>' +
                         '<td>' + data[i].us_level + '</td>' +
                         '<td>' + data[i].security_level + '</td>' +
                         '<td>' + data[i].ctime + '</td>' +
